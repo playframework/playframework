b242900c3f616430a2de0dd849912f79f86f8d8a
Peter Hausel <peter_kovac11@hotmail.com>
2011-10-17 18:36:02 -0400

    fixing specs, also renaming them using xxSpec conventional format

---
+++ framework/templates/src/test/scala/TemplateCompilerSpec.scala
@@ -0,0 +1,131 @@
+package play.templates.test
+
+import org.specs2.mutable._
+import play.templates._
+
+import java.io._
+
+object TemplateCompilerSpec extends Specification {
+  import Helper._
+
+  "The template compiler" should {
+    "successfully compile" in {
+      val helper = new CompilerHelper
+      helper.compile[((String, List[String]) => (Int) => Html)]("real.scala.html", "html.real")("World", List("A", "B"))(4).toString.trim must beLike {
+        case html =>
+          {
+            if (html.contains("<h1>Hello World</h1>") &&
+              html.contains("You have 2 items") &&
+              html.contains("EA") &&
+              html.contains("EB")) ok else ko
+          }
+
+          helper.compile[(() => Html)]("static.scala.html", "html.static")().toString.trim must be_==(
+            "<h1>It works</h1>")
+
+          helper.compile[((String) => Html)]("hello.scala.html", "html.hello")("World").toString.trim must be_==(
+            "<h1>Hello World!</h1>")
+      }
+    }
+    "fail compilation for error.scala.html" in {
+      val helper = new CompilerHelper
+      helper.compile[(() => Html)]("error.scala.html", "html.error") must throwA[CompilationError].like {
+        case CompilationError(_, 2, 12) => ok
+        case _ => ko
+      }
+    }
+
+  }
+
+}
+
+object Helper {
+
+  case class Html(text: String) extends Appendable[Html] {
+    val buffer = new StringBuilder(text)
+    def +(other: Html) = {
+      buffer.append(other.buffer)
+      this
+    }
+    override def toString = buffer.toString
+  }
+
+  object HtmlFormat extends Format[Html] {
+    def raw(text: String) = Html(text)
+    def escape(text: String) = Html(text.replace("<", "&lt;"))
+  }
+
+  case class CompilationError(message: String, line: Int, column: Int) extends RuntimeException(message)
+
+  class CompilerHelper {
+    import scala.tools.nsc.interactive.{ Response, Global }
+    import scala.tools.nsc.io.AbstractFile
+    import scala.tools.nsc.util.{ SourceFile, Position, BatchSourceFile }
+    import scala.tools.nsc.Settings
+    import scala.tools.nsc.reporters.ConsoleReporter
+
+    import java.net._
+
+    val templateCompiler = ScalaTemplateCompiler
+    val sourceDir = new File("templates/src/test/templates")
+    val generatedDir = new File("templates/target/test/generated-templates")
+    val generatedClasses = new File("templates/target/test/generated-classes")
+
+    try {
+      scalax.file.Path(generatedDir).deleteRecursively()
+      scalax.file.Path(generatedClasses).deleteRecursively()
+      scalax.file.Path(generatedClasses).createDirectory()
+    } catch { case _ => }
+
+    val classloader = new URLClassLoader(Array(generatedClasses.toURI.toURL), Class.forName("play.templates.ScalaTemplateCompiler").getClassLoader)
+
+    val compiler = {
+
+      def additionalClassPathEntry: Option[String] = Some(
+        Class.forName("play.templates.ScalaTemplateCompiler").getClassLoader.asInstanceOf[URLClassLoader].getURLs.map(_.getFile).mkString(":"))
+
+      val settings = new Settings
+      val scalaObjectSource = Class.forName("scala.ScalaObject").getProtectionDomain.getCodeSource
+
+      // is null in Eclipse/OSGI but luckily we don't need it there
+      if (scalaObjectSource != null) {
+        val compilerPath = Class.forName("scala.tools.nsc.Interpreter").getProtectionDomain.getCodeSource.getLocation
+        val libPath = scalaObjectSource.getLocation
+        val pathList = List(compilerPath, libPath)
+        val origBootclasspath = settings.bootclasspath.value
+        settings.bootclasspath.value = ((origBootclasspath :: pathList) ::: additionalClassPathEntry.toList) mkString File.pathSeparator
+        settings.outdir.value = generatedClasses.getAbsolutePath
+      }
+
+      val compiler = new Global(settings, new ConsoleReporter(settings) {
+        override def printMessage(pos: Position, msg: String) = {
+          throw CompilationError(msg, pos.line, pos.offset.get)
+        }
+      })
+
+      new compiler.Run
+
+      compiler
+    }
+
+    def compile[T](templateName: String, className: String): T = {
+      val templateFile = new File(sourceDir, templateName)
+      val Some(generated) = templateCompiler.compile(templateFile, sourceDir, generatedDir, "play.templates.test.Helper.Html", "play.templates.test.Helper.HtmlFormat")
+
+      val mapper = GeneratedSource(generated)
+
+      val run = new compiler.Run
+
+      try {
+        run.compile(List(generated.getAbsolutePath))
+      } catch {
+        case CompilationError(msg, line, column) => throw CompilationError(
+          msg, mapper.mapLine(line), mapper.mapPosition(column))
+      }
+
+      val t = classloader.loadClass(className + "$").getDeclaredField("MODULE$").get(null)
+
+      t.getClass.getDeclaredMethod("f").invoke(t).asInstanceOf[T]
+    }
+  }
+}
---
+++ framework/templates/src/test/scala/TemplateUtilsSpec.scala
@@ -0,0 +1,64 @@
+package play.templates.test
+
+import org.specs2.mutable._
+
+import play.templates._
+
+object TemplateUtilsSpec extends Specification {
+
+  "Templates" should {
+
+    "provide a HASH util" in {
+      Hash("itShouldWork".getBytes) must be_==("31c0c4e0e142fe9b605fff44528fedb3dd8ae254")
+    }
+
+    "provide a Format API" in {
+
+      "HTML for example" in {
+
+        case class Html(text: String) extends Appendable[Html] {
+          val buffer = new StringBuilder(text)
+          def +(other: Html) = {
+            buffer.append(other.buffer)
+            this
+          }
+          override def toString = buffer.toString
+        }
+
+        object HtmlFormat extends Format[Html] {
+          def raw(text: String) = Html(text)
+          def escape(text: String) = Html(text.replace("<", "&lt;"))
+        }
+
+        val html = HtmlFormat.raw("<h1>") + HtmlFormat.escape("Hello <world>") + HtmlFormat.raw("</h1>")
+
+        html.toString must be_==("<h1>Hello &lt;world></h1>")
+
+      }
+
+      "Text for example" in {
+
+        case class Text(text: String) extends Appendable[Text] {
+          val buffer = new StringBuilder(text)
+          def +(other: Text) = {
+            buffer.append(other.buffer)
+            this
+          }
+          override def toString = buffer.toString
+        }
+
+        object TextFormat extends Format[Text] {
+          def raw(text: String) = Text(text)
+          def escape(text: String) = Text(text)
+        }
+
+        val text = TextFormat.raw("<h1>") + TextFormat.escape("Hello <world>") + TextFormat.raw("</h1>")
+
+        text.toString must be_==("<h1>Hello <world></h1>")
+
+      }
+
+    }
+  }
+
+}
