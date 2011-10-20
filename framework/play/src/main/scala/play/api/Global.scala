f0adb8574c5512a3d83fd2cb758e0cdbe4cdbb41
Nicolas Martignole <nicolas@martignole.net>
2011-10-20 23:14:21 +0200

    GlobalSettings for Java, Global Plugin for Scala

--- framework/play/src/main/scala/play/api/Global.scala
+++ framework/play/src/main/scala/play/api/Global.scala
@@ -111,3 +111,34 @@ trait GlobalSettings {
  * The default global settings if not defined in the application.
  */
 object DefaultGlobal extends GlobalSettings
+
+/**
+ * Adapter that holds the java GlobalSettings and acts as a scala GlobalSettings for the framework.
+ */
+class JavaGlobalSettingsAdapter(javaGlobalSettings: play.GlobalSettings) extends GlobalSettings {
+  require(javaGlobalSettings != null, "javaGlobalSettings cannot be null")
+
+  override def beforeStart(app: Application) {
+    javaGlobalSettings.beforeStart(new play.Application(app))
+  }
+
+  override def onStart(app: Application) {
+    javaGlobalSettings.onStart(new play.Application(app))
+  }
+
+  override def onStop(app: Application) {
+    javaGlobalSettings.onStop(new play.Application(app))
+  }
+
+  override def onRouteRequest(request: RequestHeader): Option[Action[_]] = {
+    super.onRouteRequest(request)
+  }
+
+  override def onError(request: RequestHeader, ex: Throwable): Result = {
+    Option(javaGlobalSettings.onError(ex)).map(_.getWrappedResult).getOrElse(super.onError(request, ex))
+  }
+
+  override def onActionNotFound(request: RequestHeader): Result = {
+    Option(javaGlobalSettings.onActionNotFound(request.path)).map(_.getWrappedResult).getOrElse(super.onActionNotFound(request))
+  }
+}
