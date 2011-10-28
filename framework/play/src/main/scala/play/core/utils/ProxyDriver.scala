6c8d49a9608092b7e25a17a8a527f83e6f6ef475
Peter Hausel <peter_kovac11@hotmail.com>
2011-10-27 20:50:12 -0400

    fixing name space issue

--- framework/play/src/main/scala/play/core/utils/ProxyDriver.scala
+++ framework/play/src/main/scala/play/core/utils/ProxyDriver.scala
@@ -6,10 +6,10 @@ import java.util._
 class ProxyDriver(proxied: Driver) extends Driver {
 
   def acceptsURL(url: String) = proxied.acceptsURL(url)
-  def connect(user: String, properties: Properties) = proxied.connect(user, properties)
+  def connect(user: String, properties: java.util.Properties) = proxied.connect(user, properties)
   def getMajorVersion() = proxied.getMajorVersion
   def getMinorVersion() = proxied.getMinorVersion
-  def getPropertyInfo(user: String, properties: Properties) = proxied.getPropertyInfo(user, properties)
+  def getPropertyInfo(user: String, properties: java.util.Properties) = proxied.getPropertyInfo(user, properties)
   def jdbcCompliant() = proxied.jdbcCompliant
   def getParentLogger() = null
 
