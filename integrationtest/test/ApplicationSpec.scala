c8cb3a968fd4f4c7eed217d9580508e56507fdd6
Peter Hausel <peter_kovac11@hotmail.com>
2011-11-07 05:10:16 -0500

    extract body from iteratee in spec

--- integrationtest/test/ApplicationSpec.scala
+++ integrationtest/test/ApplicationSpec.scala
@@ -8,8 +8,6 @@ class MyGlobalApplicationMock extends MockApplication {
   def mockConfig: java.util.Map[String,String] = withMockDataSources
 }
 
-object ApplicationSpec extends Specification {
-
 class FakeRequest[AnyContent] extends Request[AnyContent] {
   def uri = "foo"
   def method = "GET"
@@ -26,20 +24,28 @@ class FakeRequest[AnyContent] extends Request[AnyContent] {
       def get(name: String) = Some(Cookie(name="foo",value="yay"))
   }
 }
+
+object ApplicationSpec extends Specification {
+
 val globalMock = new MyGlobalApplicationMock()
 
 "an Application" should {
-"execute index" in {
-  val action = controllers.Application.index()
-  val bodyParser = action.parser
-  val result = action.apply(new FakeRequest)
-  result match {
-    case SimpleResult(ResponseHeader(status, headers), body) => 
-      status.toString must equalTo("200")
-      headers.toString must equalTo("Map(Content-Type -> text/html)")
-      body.toString must contain ("Enumerator")
-    case _  => throw new Exception("it should have matched a valid response")
+  "execute index" in {
+    val action = controllers.Application.index()
+    val bodyParser = action.parser
+    val result = action.apply(new FakeRequest)
+    result match {
+      case SimpleResult(ResponseHeader(status, headers), body) => 
+        status.toString must equalTo("200")
+        headers.toString must equalTo("Map(Content-Type -> text/html)")
+        body.toString must contain ("Enumerator")
+        val later:Promise[String] = (Iteratee.fold[String,String](""){ case (s,e) => s+e } <<: body).map(_.run)
+
+        //clear mock from current scope
+        globalMock.clearMock() 
+        None must equalTo(None)
+      case _  => throw new Exception("it should have matched a valid response")
+    }
   }
  }
-}
 }
