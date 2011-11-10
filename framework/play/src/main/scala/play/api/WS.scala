89dadb9e02d328434a9e8d956e77fe4f86eaa9b0
Erwan Loisant <eloisant@gmail.com>
2011-11-10 12:26:38 +0100

    Add documentation to the WS API

--- framework/play/src/main/scala/play/api/WS.scala
+++ framework/play/src/main/scala/play/api/WS.scala
@@ -5,30 +5,62 @@ import java.util.concurrent.Executor
 import play.api.libs.concurrent._
 import com.ning.http.client._
 
+/**
+ * Asynchronous API to to query web services, as an http client
+ *
+ * Usage example:
+ * WS.url("http://example.com/feed").get()
+ *
+ * The value returned is a Promise of com.ning.http.client.Response,
+ * and you should use Play's asynchronous mechanisms to use this response.
+ *
+ */
 object WS {
 
   private lazy val client = new AsyncHttpClient()
 
+  /**
+   * Prepare a new request. You can then construct it by chaining calls.
+   * @param url the URL to request
+   */
   def url(url: String) = new WSRequest().setUrl(url)
 
   class WSRequest extends RequestBuilderBase[WSRequest](classOf[WSRequest], "GET") {
 
-    def get(): HttpPromise[Response] = promise(WS.client.executeRequest(this.setMethod("GET").build()))
-    def post(): HttpPromise[Response] = promise(WS.client.executeRequest(this.setMethod("POST").build()))
-    def put(): HttpPromise[Response] = promise(WS.client.executeRequest(this.setMethod("PUT").build()))
-    def delete(): HttpPromise[Response] = promise(WS.client.executeRequest(this.setMethod("DELETE").build()))
-    def head(): HttpPromise[Response] = promise(WS.client.executeRequest(this.setMethod("HEAD").build()))
-    def options(): HttpPromise[Response] = promise(WS.client.executeRequest(this.setMethod("OPTIONS").build()))
+    /**
+     * Perform a GET on the request asynchronously.
+     */
+    def get(): HttpPromise[Response] = execute("GET")
 
-    def promise(listenable: ListenableFuture[Response]) = new HttpPromise[Response](listenable)
+    /**
+     * Perform a POST on the request asynchronously.
+     */
+    def post(): HttpPromise[Response] = execute("POST")
+
+    /**
+     * Perform a PUT on the request asynchronously.
+     */
+    def put(): HttpPromise[Response] = execute("PUT")
+
+    /**
+     * Perform a DELETE on the request asynchronously.
+     */
+    def delete(): HttpPromise[Response] = execute("DELETE")
+
+    /**
+     * Perform a HEAD on the request asynchronously.
+     */
+    def head(): HttpPromise[Response] = execute("HEAD")
+
+    /**
+     * Perform a OPTIONS on the request asynchronously.
+     */
+    def options(): HttpPromise[Response] = execute("OPTION")
+
+    private def execute(method: String) = new HttpPromise[Response](WS.client.executeRequest(this.setMethod("HEAD").build()))
   }
 
   class HttpPromise[A](listenable: ListenableFuture[A]) extends Promise[A] {
-
-    def apply: A = this.value match {
-      case Redeemed(a) => a
-      case Thrown(e) => throw e
-    }
 
     override def onRedeem(k: A => Unit) = {
       listenable.addListener(new Runnable() {
