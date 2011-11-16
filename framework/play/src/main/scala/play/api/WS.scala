d7983071fcdd13d86c9e1f8bba1cc5f61b0a0a40
Erwan Loisant <eloisant@gmail.com>
2011-11-16 22:25:15 +0100

    WS: Pass exceptions thrown in the call to the promise, and simplify

--- framework/play/src/main/scala/play/api/WS.scala
+++ framework/play/src/main/scala/play/api/WS.scala
@@ -30,78 +30,49 @@ object WS {
     /**
      * Perform a GET on the request asynchronously.
      */
-    def get(): HttpPromise[Response] = execute("GET")
+    def get(): Promise[Response] = execute("GET")
 
     /**
      * Perform a POST on the request asynchronously.
      */
-    def post(): HttpPromise[Response] = execute("POST")
+    def post(): Promise[Response] = execute("POST")
 
     /**
      * Perform a PUT on the request asynchronously.
      */
-    def put(): HttpPromise[Response] = execute("PUT")
+    def put(): Promise[Response] = execute("PUT")
 
     /**
      * Perform a DELETE on the request asynchronously.
      */
-    def delete(): HttpPromise[Response] = execute("DELETE")
+    def delete(): Promise[Response] = execute("DELETE")
 
     /**
      * Perform a HEAD on the request asynchronously.
      */
-    def head(): HttpPromise[Response] = execute("HEAD")
+    def head(): Promise[Response] = execute("HEAD")
 
     /**
      * Perform a OPTIONS on the request asynchronously.
      */
-    def options(): HttpPromise[Response] = execute("OPTION")
+    def options(): Promise[Response] = execute("OPTION")
 
-    private def execute(method: String) = new HttpPromise[Response](WS.client.executeRequest(this.setMethod(method).build()))
-  }
-
-  class HttpPromise[A](listenable: ListenableFuture[A]) extends Promise[A] {
-
-    override def onRedeem(k: A => Unit) = {
-      listenable.addListener(new Runnable() {
-        def run() = k(listenable.get())
-      }, new Executor {
-        def execute(r: Runnable) = r.run()
-      })
-    }
-
-    override def extend[B](k: Function1[Promise[A], B]): Promise[B] = {
-      val result = new STMPromise[B]()
-      onRedeem(_ => result.redeem(k(this)))
-      result
-    }
-
-    override def filter(p: A => Boolean): Promise[A] = {
-      val result = new STMPromise[A]()
-      onRedeem(a => if (p(a)) result.redeem(a))
-      result
-    }
-
-    override def value: NotWaiting[A] = try {
-      Redeemed(listenable.get())
-    } catch { case e => Thrown(e) }
-
-    override def map[B](f: A => B): Promise[B] = {
-      val result = new STMPromise[B]()
-      this.onRedeem(a => result.redeem(f(a)))
-      result
-    }
-
-    override def flatMap[B](f: A => Promise[B]) = {
-      throw new RuntimeException("Not implemented")
-      val result = new STMPromise[B]()
-      this.onRedeem(a => {
-        f(a).onRedeem(b => {
-          result.redeem(b)
-        })
+    private def execute(method: String): Promise[Response] = {
+      play.Logger.error("Oh promises...")
+      var result = Promise[Response]()
+      WS.client.executeRequest(this.setMethod(method).build(), new AsyncCompletionHandler[Response]() {
+        override def onCompleted(response: Response) = {
+          result.redeem(response)
+          response
+        }
+        override def onThrowable(t: Throwable) = {
+          result.redeem(throw t)
+        }
       })
       result
     }
+
   }
 
 }
+
