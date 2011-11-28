aaf745a9622314926adba112fcabdd142f5abcdf
Sadek Drobi <github@sadekdrobi.com>
2011-11-28 11:53:18 +0100

    cyclic iterator balancer has simpler semantics

--- framework/play/src/main/scala/play/core/server/Server.scala
+++ framework/play/src/main/scala/play/core/server/Server.scala
@@ -24,13 +24,13 @@ trait Server {
   import akka.actor._
   import akka.actor.Actor._
   import akka.routing.Routing._
-  import akka.routing.SmallestMailboxFirstIterator
+  import akka.routing.CyclicIterator
   import akka.config._
   import akka.config.Supervision._
 
   def newInvoker = { val inv = actorOf[Invoker]; inv.start(); inv }
 
-  val invoker = loadBalancerActor(new SmallestMailboxFirstIterator(List.fill(3)(newInvoker))).start()
+  val invoker = loadBalancerActor(new CyclicIterator(List.fill(3)(newInvoker))).start()
 
   def getHandlerFor(request: RequestHeader): Either[Result, (Handler, Application)] = {
     def sendHandler: Either[Throwable, (Handler, Application)] =
