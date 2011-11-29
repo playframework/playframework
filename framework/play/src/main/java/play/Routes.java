f0ff5377f14fb1bab1f0fd0b4bef5c1693ffcd97
Erwan Loisant <eloisant@gmail.com>
2011-11-29 17:36:59 +0100

    Provide a way to change the Ajax method (not just jQuery)

--- framework/play/src/main/java/play/Routes.java
+++ framework/play/src/main/java/play/Routes.java
@@ -5,11 +5,15 @@ import java.util.*;
 import play.libs.Scala;
 
 public class Routes {
-    
+
     public static String javascriptRouter(String name, play.core.Router.JavascriptReverseRoute... routes) {
+        return javascriptRouter(name, "jQuery.ajax", routes);
+    }
+
+    public static String javascriptRouter(String name, String ajaxMethod, play.core.Router.JavascriptReverseRoute... routes) {
         return play.api.Routes.javascriptRouter(
-            name, Scala.toSeq(routes)
+            name, Scala.Option(ajaxMethod), Scala.toSeq(routes)
         );
     }
-    
+
 }
--- framework/play/src/main/java/play/libs/Scala.java
+++ framework/play/src/main/java/play/libs/Scala.java
@@ -4,14 +4,14 @@ package play.libs;
  * class that contains useful java <-> scala conversion helpers
  */
 public class Scala {
-    
+
     public static <T> T orNull(scala.Option<T> opt) {
         if(opt.isDefined()) {
             return opt.get();
         }
         return null;
     }
-    
+
     public static <K,V> java.util.Map<K,V> asJava(scala.collection.Map<K,V> scalaMap) {
        return scala.collection.JavaConverters.asJavaMapConverter(scalaMap).asJava();
     }
@@ -23,9 +23,13 @@ public class Scala {
     public static <T> scala.collection.Seq<T> toSeq(java.util.List<T> list) {
         return scala.collection.JavaConverters.asScalaBufferConverter(list).asScala().toList();
     }
-    
+
     public static <T> scala.collection.Seq<T> toSeq(T[] array) {
         return toSeq(java.util.Arrays.asList(array));
     }
-    
+
+    public static <T> scala.Option<T> Option(T t) {
+        return scala.Option.apply(t);
+    }
+
 }
