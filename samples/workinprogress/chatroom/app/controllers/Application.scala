9584d89de5b95db037cc540cdc50aaa561b30d60
Peter Hausel <peter_kovac11@hotmail.com>
2011-11-29 11:34:52 -0500

    separate sample apps

---
+++ samples/workinprogress/chatroom/app/controllers/Application.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs._
+
+import play.api.libs.iteratee._
+import play.api.libs.concurrent._
+import play.api.libs.akka._
+
+import actors._
+import actors.ChatRoomActor._
+
+object Application extends Controller {
+  
+  def index = Action {
+    Ok(views.html.index()) 
+  }
+  
+  // -- Comet chat room
+  
+  def chatRoom = Action {
+    Ok(views.html.room())
+  }
+  
+  def stream = Action {
+    AsyncResult {
+      (ChatRoomActor.ref ? Join() map {_.asInstanceOf[Enumerator[String]]}).asPromise.map { chunks =>
+        Ok(Comet(chunks, callback = "parent.message"))
+      }
+    }
+  }
+  
+  def say(message: String) = Action {
+    ChatRoomActor.ref ! Message(message)
+    Ok("Said " + message)
+  }
+
+}
--- samples/workinprogress/websockets/app/controllers/Application.scala
+++ samples/workinprogress/websockets/app/controllers/Application.scala
@@ -6,10 +6,7 @@ import play.api.libs._
 
 import play.api.libs.iteratee._
 import play.api.libs.concurrent._
-import play.api.libs.akka._
 
-import actors._
-import actors.ChatRoomActor._
 
 object Application extends Controller {
   
@@ -36,23 +33,4 @@ object Application extends Controller {
     
   }
   
-  // -- Comet chat room
-  
-  def chatRoom = Action {
-    Ok(views.html.room())
-  }
-  
-  def stream = Action {
-    AsyncResult {
-      (ChatRoomActor.ref ? Join() map {_.asInstanceOf[Enumerator[String]]}).asPromise.map { chunks =>
-        Ok(Comet(chunks, callback = "parent.message"))
-      }
-    }
-  }
-  
-  def say(message: String) = Action {
-    ChatRoomActor.ref ! Message(message)
-    Ok("Said " + message)
-  }
-
 }
