b628eaa234c7ed4f9be13393682a85c3e4ce3184
Peter Hausel <peter_kovac11@hotmail.com>
2011-12-15 10:35:35 -0500

    adding total counts to web based test reporter

--- framework/src/sbt-plugin/src/main/scala/PlayTestListener.scala
+++ framework/src/sbt-plugin/src/main/scala/PlayTestListener.scala
@@ -2,9 +2,42 @@ package sbt
 
 import org.scalatools.testing.{ Event => TEvent, Result => TResult }
 
-private[sbt] class PlayTestListener extends TestReportListener {
+private[sbt] class PlayTestListener extends TestsListener {
+
+  private var skipped, errors, passed, failures = 0
+
+  private def count(event: TEvent) {
+    event.result match {
+      case TResult.Error => errors += 1
+      case TResult.Success => passed += 1
+      case TResult.Failure => failures += 1
+      case TResult.Skipped => skipped += 1
+    }
+  }
+
+  private def playReport(messageName: String, attributes: (String, String)*) {
+    result.append("<li>" + messageName + " " + attributes.map {
+      case (k, v) => k + ": " + tidy(v)
+    }.mkString(" ") + "</li>")
+  }
 
   val result = new collection.mutable.ListBuffer[String]
+
+  def doComplete(finalResult: TestResult.Value) {
+    val totalCount = failures + errors + skipped + passed
+    val postfix = "Total " + totalCount + ", Failed " + failures + ", Errors " + errors + ", Passed " + passed + ", Skipped " + skipped
+    result.append("</ul>")
+    result.append("<p>" + postfix + "</p>")
+  }
+
+  def doInit {
+    result.append("<p>Executing Test suit</p>")
+    result.append("<ul>")
+    failures = 0
+    errors = 0
+    passed = 0
+    skipped = 0
+  }
 
   /** called for each class or equivalent grouping */
   def startGroup(name: String) {
@@ -17,6 +50,7 @@ private[sbt] class PlayTestListener extends TestReportListener {
     event match {
       case e =>
         for (d <- e.detail) {
+          event.detail.foreach(count)
           d match {
             case te: TEvent =>
               te.result match {
@@ -49,9 +83,4 @@ private[sbt] class PlayTestListener extends TestReportListener {
     .replace("[", "|[")
     .replace("]", "|]")
 
-  private def playReport(messageName: String, attributes: (String, String)*) {
-    result.append("<li>" + messageName + " " + attributes.map {
-      case (k, v) => k + ": " + tidy(v)
-    }.mkString(" ") + "</li>")
-  }
 }
