50d8d816de0841f93cee0e492a35982fc129fb09
Erwan Loisant <eloisant@gmail.com>
2011-12-20 09:56:31 +0100

    Trailing whitespaces

--- framework/src/play/src/main/java/play/mvc/WebSocket.java
+++ framework/src/play/src/main/java/play/mvc/WebSocket.java
@@ -5,35 +5,35 @@ import java.util.*;
 import play.libs.F.*;
 
 public abstract class WebSocket<A> {
-    
+
     public abstract void onReady(In<A> in, Out<A> out);
-    
+
     public static class Out<A> {
-        
+
         final play.api.libs.iteratee.CallbackEnumerator<A> enumerator;
-        
+
         public Out(play.api.libs.iteratee.CallbackEnumerator<A> enumerator) {
             this.enumerator = enumerator;
         }
-        
+
         public void write(A frame) {
             enumerator.push(frame);
         }
-        
+
         public void close() {
             enumerator.close();
         }
-        
+
     }
-    
+
     public static class In<A> {
-        
+
         public final List<Callback<A>> callbacks = new ArrayList<Callback<A>>();
-        
+
         public void onMessage(Callback<A> callback) {
             callbacks.add(callback);
         }
-        
+
     }
-    
+
 }
