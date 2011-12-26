b1d03ff2241ef53f0d45b13abf49c4178e7efd0a
Sadek Drobi <github@sadekdrobi.com>
2011-12-26 19:56:43 +0100

    starting EnumerateesSpec

---
+++ framework/src/play/src/test/scala/play/iteratee/EnumerateesSpec.scala
@@ -0,0 +1,60 @@
+package play.api.libs.iteratee
+
+import org.specs2.mutable._
+
+object EnumerateesSpec extends Specification {
+
+  "Enumeratee.drop" should {
+
+    "ignore 3 chunkes when applied with 3" in {
+      
+      val drop3AndConsume = Enumeratee.drop[String](3) &>>  Iteratee.consume[String]()
+      val enumerator = Enumerator(Range(1,20).map(_.toString) :_*)  
+      (enumerator |>> drop3AndConsume).flatMap(_.run).value.get must equalTo(Range(4,20).map(_.toString).mkString)
+
+    }
+
+  }
+
+  "Enumeratee.take" should {
+
+    "pass only first 3 chunkes to Iteratee when applied with 3" in {
+      
+      val take3AndConsume = Enumeratee.take[String](3) &>>  Iteratee.consume()
+      val enumerator = Enumerator(Range(1,20).map(_.toString) :_*)  
+      (enumerator |>> take3AndConsume).flatMap(_.run).value.get must equalTo(List(1,2,3).map(_.toString).mkString)
+
+    }
+
+    "passes along what's left of chunks after taking 3" in {
+      
+      val take3AndConsume = (Enumeratee.take[String](3) &>>  Iteratee.consume()).flatMap(_ => Iteratee.consume())
+      val enumerator = Enumerator(Range(1,20).map(_.toString) :_*)  
+      (enumerator |>> take3AndConsume).flatMap(_.run).value.get must equalTo(Range(4,20).map(_.toString).mkString)
+
+    }
+
+  }
+
+
+  "Traversable.take" should {
+
+    "pass only first 3 elements to Iteratee when applied with 3" in {
+      
+      val take3AndConsume = Traversable.take[String](3) &>>  Iteratee.consume()
+      val enumerator = Enumerator("he","ybbb","bbb")  
+      (enumerator |>> take3AndConsume).flatMap(_.run).value.get must equalTo("hey")
+
+    }
+
+    "pass alnog what's left after taking 3 elements" in {
+      
+      val take3AndConsume = (Traversable.take[String](3) &>>  Iteratee.consume()).flatMap(_ => Iteratee.consume())
+      val enumerator = Enumerator("he","ybbb","bbb")  
+      (enumerator |>> take3AndConsume).flatMap(_.run).value.get must equalTo("bbbbbb")
+
+    }
+
+  }
+
+}
