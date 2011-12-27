7f9ff305fcd3a8395b78d2d3893be82da252c076
Peter Hausel <peter_kovac11@hotmail.com>
2011-12-26 23:38:22 -0500

    start cleaning up NettyServer

---
+++ framework/src/play/src/main/scala/play/core/server/netty/Helpers.scala
@@ -0,0 +1,72 @@
+package play.core.server.netty
+
+import org.jboss.netty.buffer._
+import org.jboss.netty.channel._
+import org.jboss.netty.bootstrap._
+import org.jboss.netty.channel.Channels._
+import org.jboss.netty.handler.codec.http._
+import org.jboss.netty.channel.socket.nio._
+import org.jboss.netty.handler.stream._
+import org.jboss.netty.handler.codec.http.HttpHeaders._
+import org.jboss.netty.handler.codec.http.HttpHeaders.Names._
+import org.jboss.netty.handler.codec.http.HttpHeaders.Values._
+
+import org.jboss.netty.channel.group._
+import java.util.concurrent._
+
+import play.core._
+import play.api._
+import play.api.mvc._
+import play.api.libs.iteratee._
+import play.api.libs.iteratee.Input._
+import play.api.libs.concurrent._
+
+import scala.collection.JavaConverters._
+
+private[netty] trait Helpers {
+
+  def socketOut[A](ctx: ChannelHandlerContext)(frameFormatter: play.api.mvc.WebSocket.FrameFormatter[A]): Iteratee[A, Unit] = {
+    val channel = ctx.getChannel()
+    val nettyFrameFormatter = frameFormatter.asInstanceOf[play.core.server.websocket.FrameFormatter[A]]
+
+    def step(future: Option[ChannelFuture])(input: Input[A]): Iteratee[A, Unit] =
+      input match {
+        case El(e) => Cont(step(Some(channel.write(nettyFrameFormatter.toFrame(e)))))
+        case e @ EOF => future.map(_.addListener(ChannelFutureListener.CLOSE)).getOrElse(channel.close()); Done((), e)
+        case Empty => Cont(step(future))
+      }
+
+    Cont(step(None))
+  }
+
+  def getHeaders(nettyRequest: HttpRequest): Headers = {
+
+    val headers: Map[String, Seq[String]] = nettyRequest.getHeaderNames.asScala.map { key =>
+      key.toUpperCase -> nettyRequest.getHeaders(key).asScala
+    }.toMap
+
+    new Headers {
+      def getAll(key: String) = headers.get(key.toUpperCase).flatten.toSeq
+      def keys = headers.keySet
+      override def toString = headers.toString
+    }
+
+  }
+
+  def getCookies(nettyRequest: HttpRequest): Cookies = {
+
+    val cookies: Map[String, play.api.mvc.Cookie] = getHeaders(nettyRequest).get(play.api.http.HeaderNames.COOKIE).map { cookiesHeader =>
+      new CookieDecoder().decode(cookiesHeader).asScala.map { c =>
+        c.getName -> play.api.mvc.Cookie(
+          c.getName, c.getValue, c.getMaxAge, Option(c.getPath).getOrElse("/"), Option(c.getDomain), c.isSecure, c.isHttpOnly)
+      }.toMap
+    }.getOrElse(Map.empty)
+
+    new Cookies {
+      def get(name: String) = cookies.get(name)
+      override def toString = cookies.toString
+    }
+
+  }
+
+}
