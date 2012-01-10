4955f4e1a91ab7e9e981ebbab49dae2caa13148d
Guillaume Bort <guillaume.bort@gmail.com>
2012-01-10 09:33:17 +0100

    Oops, missing file

---
+++ framework/src/play/src/main/scala/play/core/utils/Threads.scala
@@ -0,0 +1,14 @@
+package play.utils
+
+object Threads {
+  
+  def withContextClassLoader[T](classloader: ClassLoader)(b: => T): T = {
+    val thread = Thread.currentThread
+    val oldLoader = thread.getContextClassLoader
+    thread.setContextClassLoader(classloader)
+    val result = b
+    thread.setContextClassLoader(oldLoader)
+    result
+  }
+  
+}
