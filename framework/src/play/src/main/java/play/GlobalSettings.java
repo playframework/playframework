67df1929b3997b948eddb46abc05a3c8785ed5ac
Guillaume Bort <guillaume.bort@gmail.com>
2012-01-23 22:08:38 +0100

    Add onRequest(...) to the Java Global API

--- framework/src/play/src/main/java/play/GlobalSettings.java
+++ framework/src/play/src/main/java/play/GlobalSettings.java
@@ -1,6 +1,9 @@
 package play;
 
-import play.mvc.Result;
+import play.mvc.*;
+import play.mvc.Http.*;
+
+import java.lang.reflect.*;
 
 /**
  * GlobalSettings is instantiated by the framework when an application starts, to let you perform specific tasks
@@ -8,7 +11,7 @@ import play.mvc.Result;
  * <p>
  * How to use it: create a <code>Global.java</code> class in your Java application and override the methods you want.
  */
-public abstract class GlobalSettings {
+public class GlobalSettings {
     
     /**
      * Executed before any plugin - you can set-up your database schema here, for instance.
@@ -38,6 +41,22 @@ public abstract class GlobalSettings {
      */
     public Result onError(Throwable t) {
         return null;
+    }
+    
+    /**
+     * Call to create the root Action of a request for a Java application.
+     * The request and actionMethod values are passed for information.
+     *
+     * @param request The HTTP Request
+     * @param actionMethod The action method containing the user code for this Action.
+     * @return The default implementation returns a raw Action calling the method.
+     */
+    public Action onRequest(Request request, Method actionMethod) {
+        return new Action.Simple() {
+            public Result call(Context ctx) throws Throwable {
+                return delegate.call(ctx);
+            }
+        };
     }
 
     /**
