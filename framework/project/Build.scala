4486ee1ae44401e9a27d6179f45495e2467a4933
peter hausel <peter_kovac11@hotmail.com>
2012-02-29 09:46:15 -0500

    upgrading akka to 2.0-RC3

--- framework/project/Build.scala
+++ framework/project/Build.scala
@@ -180,8 +180,8 @@ object PlayBuild extends Build {
             "ch.qos.logback"                    %    "logback-core"             %   "1.0.0",
             "ch.qos.logback"                    %    "logback-classic"          %   "1.0.0",
             "com.github.scala-incubator.io"     %%   "scala-io-file"            %   "0.2.0",
-            "com.typesafe.akka"                 %    "akka-actor"               %   "2.0-RC2",
-            "com.typesafe.akka"                 %    "akka-slf4j"               %   "2.0-RC2",
+            "com.typesafe.akka"                 %    "akka-actor"               %   "2.0-RC3",
+            "com.typesafe.akka"                 %    "akka-slf4j"               %   "2.0-RC3",
             "com.typesafe.config"               %    "config"                   %   "0.2.1",
             "com.google.guava"                  %    "guava"                    %   "10.0.1",
             
