238ef4cf3817e35d7a18663120ab726a504ff010
Guillaume Bort <guillaume.bort@gmail.com>
2012-03-05 15:41:17 +0100

    Fix Java Form issues

--- framework/src/play/src/main/java/play/data/Form.java
+++ framework/src/play/src/main/java/play/data/Form.java
@@ -140,8 +140,8 @@ public class Form<T> {
      *
      * @return a copy of this form filled with the new data
      */
-    public Form<T> bindFromRequest() {
-        return bind(requestData());
+    public Form<T> bindFromRequest(String... allowedFields) {
+        return bind(requestData(), allowedFields);
     }
     
     /**
@@ -150,7 +150,7 @@ public class Form<T> {
      * @param data data to submit
      * @return a copy of this form filled with the new data
      */
-    public Form<T> bind(org.codehaus.jackson.JsonNode data) {
+    public Form<T> bind(org.codehaus.jackson.JsonNode data, String... allowedFields) {
         return bind(
             play.libs.Scala.asJava(
                 play.api.data.FormUtils.fromJson("", 
@@ -158,7 +158,8 @@ public class Form<T> {
                         play.libs.Json.stringify(data)
                     )
                 )
-            )
+            ),
+            allowedFields
         );
     }
     
@@ -168,7 +169,7 @@ public class Form<T> {
      * @param data data to submit
      * @return a copy of this form filled with the new data
      */
-    public Form<T> bind(Map<String,String> data) {
+    public Form<T> bind(Map<String,String> data, String... allowedFields) {
         
         DataBinder dataBinder = null;
         Map<String, String> objectData = data;
@@ -182,6 +183,9 @@ public class Form<T> {
                     objectData.put(key.substring(rootName.length() + 1), data.get(key));
                 }
             }
+        }
+        if(allowedFields.length > 0) {
+            dataBinder.setAllowedFields(allowedFields);
         }
         dataBinder.setValidator(new SpringValidatorAdapter(play.data.validation.Validation.getValidator()));
         dataBinder.setConversionService(play.data.format.Formatters.conversion);
@@ -215,7 +219,10 @@ public class Form<T> {
                 try {
                     java.lang.reflect.Method v = result.getTarget().getClass().getMethod("validate");
                     globalError = (String)v.invoke(result.getTarget());
-                } catch(Exception e) {}
+                } catch(NoSuchMethodException e) {
+                } catch(Throwable e) {
+                    throw new RuntimeException(e);
+                }
             }
             if(globalError != null) {
                 Map<String,List<ValidationError>> errors = new HashMap<String,List<ValidationError>>();
--- framework/src/play/src/test/scala/play/data/FormSpec.scala
+++ framework/src/play/src/test/scala/play/data/FormSpec.scala
@@ -33,21 +33,21 @@ object FormSpec extends Specification {
       val req = new DummyRequest(Map("id" -> Array("1234567891"), "name" -> Array("peter"), "done" -> Array("true"), "dueDate" -> Array("15/12/2009")))
       Context.current.set(new Context(req, Map.empty.asJava, Map.empty.asJava))
 
-      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest
+      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest()
       myForm hasErrors () must beEqualTo(false)
     }
     "be valid with mandatory params passed" in {
       val req = new DummyRequest(Map("id" -> Array("1234567891"), "name" -> Array("peter"), "dueDate" -> Array("15/12/2009")))
       Context.current.set(new Context(req, Map.empty.asJava, Map.empty.asJava))
 
-      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest
+      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest()
       myForm hasErrors () must beEqualTo(false)
     }
     "have an error due to baldy formatted date" in {
       val req = new DummyRequest(Map("id" -> Array("1234567891"), "name" -> Array("peter"), "dueDate" -> Array("2009/11/11")))
       Context.current.set(new Context(req, Map.empty.asJava, Map.empty.asJava))
 
-      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest
+      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest()
       myForm hasErrors () must beEqualTo(true)
 
     }
@@ -55,7 +55,7 @@ object FormSpec extends Specification {
       val req = new DummyRequest(Map("id" -> Array("1234567891x"), "name" -> Array("peter"), "dueDate" -> Array("12/12/2009")))
       Context.current.set(new Context(req, Map.empty.asJava, Map.empty.asJava))
 
-      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest
+      val myForm = Controller.form(classOf[play.data.models.Task]).bindFromRequest()
       myForm hasErrors () must beEqualTo(true)
 
     }
