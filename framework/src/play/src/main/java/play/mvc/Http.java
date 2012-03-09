9c3e3f1ba589d8821e6955792868270e68b0e715
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-09 16:25:36 +0100

    Suppress unchecked cast warning

--- framework/src/play/src/main/java/play/mvc/Http.java
+++ framework/src/play/src/main/java/play/mvc/Http.java
@@ -377,6 +377,7 @@ public class Http {
         /**
          * Cast this RequestBody as T if possible.
          */
+        @SuppressWarnings("unchecked")
         public <T> T as(Class<T> tType) {
             if(this.getClass().isAssignableFrom(tType)) {
                 return (T)this;
