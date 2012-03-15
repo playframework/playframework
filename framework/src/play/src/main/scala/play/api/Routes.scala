eef5f7a772a64278a5a351639493059722837efa
Benoît Ponsero <b.ponsero@gmail.com>
2012-03-15 23:24:47 +0100

    don't override js routes collection if already defined
    tested this time

--- framework/src/play/src/main/scala/play/api/Routes.scala
+++ framework/src/play/src/main/scala/play/api/Routes.scala
@@ -29,14 +29,14 @@ package play.api {
      * @return the JavaScript code
      */
     def javascriptRouter(name: String = "Router", ajaxMethod: Option[String] = Some("jQuery.ajax"))(routes: JavascriptReverseRoute*): String = {
-      """|var %s = {}; (function(_root){
+      """|var %s = %s || {}; (function(_root){
              |var _nS = function(c,f,b){var e=c.split(f||"."),g=b||_root,d,a;for(d=0,a=e.length;d<a;d++){g=g[e[d]]=g[e[d]]||{}}return g}
              |var _qS = function(items){var qs = ''; for(var i=0;i<items.length;i++) {if(items[i]) qs += (qs ? '&' : '') + items[i]}; return qs ? ('?' + qs) : ''}
              |var _wA = function(r){return {%s method:r.method,url:r.url}}
              |%s   
              |})(%s)
           """.stripMargin.format(
-        name,
+        name, name,
         ajaxMethod.map("ajax:function(c){c.url=r.url;c.type=r.method;" + _ + "(c)},").getOrElse(""),
         routes.map { route =>
           "_nS('%s'); _root.%s = %s".format(
