cd16b2303f8ee9b8ae43da147549b434b74cb3a1
peter hausel <peter_kovac11@hotmail.com>
2012-03-27 09:35:26 -0400

    add scaladoc to new Invoker

--- framework/src/play/src/main/scala/play/core/system/Invoker.scala
+++ framework/src/play/src/main/scala/play/core/system/Invoker.scala
@@ -19,7 +19,7 @@ import play.utils._
  */
 class Invoker(applicationProvider: Option[ApplicationProvider] = None) {
 
-  val system: ActorSystem = applicationProvider.map{a=>
+  val system: ActorSystem = applicationProvider.map{a =>
     Invoker.appProviderActorSystem(a)
   }.getOrElse(ActorSystem("play")) 
 
@@ -30,7 +30,6 @@ class Invoker(applicationProvider: Option[ApplicationProvider] = None) {
   val actionInvoker = {
     system.actorOf(Props[ActionInvoker].withDispatcher("akka.actor.actions-dispatcher").withRouter(RoundRobinRouter(100)), name = "actions")
   }
-
 
   /**
    * kills actor system 
@@ -46,26 +45,15 @@ class Invoker(applicationProvider: Option[ApplicationProvider] = None) {
  */
 object Invoker {
 
+  /**
+   * provides an extractor for body parser
+   */ 
   case class GetBodyParser(request: RequestHeader, bodyParser: BodyParser[_])
+
+  /**
+   * provides actor helper
+   */ 
   case class HandleAction[A](request: Request[A], response: Response, action: Action[A], app: Application)
-
-  private def appProviderActorSystem(applicationProvider: ApplicationProvider) = {
-    val conf = play.api.Play.maybeApplication.filter(_.mode == Mode.Prod).map(app =>
-      ConfigFactory.load()).getOrElse(Configuration.loadDev(applicationProvider.path))
-    ActorSystem("play", conf.getConfig("play"))
-  }
-
-  def apply(applicationProvider: ApplicationProvider): Invoker = new Invoker(Some(applicationProvider))
-
-  def init(invoker: Invoker): Unit = {
-    if (invokerOption.isDefined)
-      throw new IllegalStateException("Invoker was initialized twice without an intervening uninit; two Server created at once?")
-    invokerOption = Some(invoker)
-  }
-
-  def uninit(): Unit = {
-    invokerOption = None
-  }
 
   private var invokerOption: Option[Invoker] = None
 
@@ -74,12 +62,48 @@ object Invoker {
       invokerOption = Some(default)
       Logger.warn ("Invoker was created outside of Invoker#init - this potentially could lead to initialization problems in production mode")
       default
-    }
+  }
 
+  private def appProviderActorSystem(applicationProvider: ApplicationProvider) = {
+    val conf = play.api.Play.maybeApplication.filter(_.mode == Mode.Prod).map(app =>
+      ConfigFactory.load()).getOrElse(Configuration.loadDev(applicationProvider.path))
+    ActorSystem("play", conf.getConfig("play"))
+  }
+  
+  /**
+   * contructor used by Server
+   */
+  def apply(applicationProvider: ApplicationProvider): Invoker = new Invoker(Some(applicationProvider))
+
+  /**
+   * saves invoker instance in global scope
+   */
+  def init(invoker: Invoker): Unit = {
+    if (invokerOption.isDefined)
+      throw new IllegalStateException("Invoker was initialized twice without an intervening uninit; two Server created at once?")
+    invokerOption = Some(invoker)
+  }
+
+  /**
+   * removes invoker instance from global scope
+   */
+  def uninit(): Unit = {
+    invokerOption = None
+  }
+
+  /**
+   * provides actor system
+   */
   def system = invoker.system
 
+  /**
+   * provides promise dispatcher
+   */
   def promiseDispatcher = invoker.promiseDispatcher
 
+   /**
+   * provides invoker used for Action dispatching
+   */
   def actionInvoker = invoker.actionInvoker
 }
 
