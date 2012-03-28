9dbb745b36f313502fa42ba6bb1661b74e537cde
Guillaume Bort <guillaume.bort@gmail.com>
2012-03-28 12:25:22 +0200

    Fix EBean enhancer issue when no models package is defined in a Java project.

--- framework/src/sbt-plugin/src/main/scala/PlayCommands.scala
+++ framework/src/sbt-plugin/src/main/scala/PlayCommands.scala
@@ -486,20 +486,25 @@ exec java $* -cp "`dirname $0`/lib/*" """ + config.map(_ => "-Dconfig.file=`dirn
         import com.avaje.ebean.enhance.ant._
         import collection.JavaConverters._
         import com.typesafe.config._
+        
         val cl = ClassLoader.getSystemClassLoader
 
         val t = new Transformer(cp, "debug=-1")
 
         val ft = new OfflineFileTransform(t, cl, classes.getAbsolutePath, classes.getAbsolutePath)
 
-        //model definition only can come from bundled application.conf at this point and "conf" folder is not visible as a resource from this classloader, so
-
         val config = ConfigFactory.load(ConfigFactory.parseFileAnySyntax(new File("conf/application.conf")))
 
         val models = try {
           config.getConfig("ebean").entrySet.asScala.map(_.getValue.unwrapped).toSet.mkString(",")
         } catch { case e: ConfigException.Missing => "models.*" }
-        ft.process(models)
+        
+        try {
+          ft.process(models)
+        } catch {
+          case _ =>
+        }
+        
       } catch {
         case e => throw e
       } finally {
