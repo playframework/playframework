74a323d8967698ed68c03d0ae0e92f39377606dc
peter hausel <peter_kovac11@hotmail.com>
2012-03-29 20:18:12 -0400

    adding tests to skeletons [lh 271]

--- framework/skeletons/scala-skel/test/ApplicationSpec.scala
+++ framework/skeletons/scala-skel/test/ApplicationSpec.scala
@@ -5,6 +5,11 @@ import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
 
+/**
+ * Add your spec here.
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
 class ApplicationSpec extends Specification {
   
   "Application" should {
@@ -15,12 +20,14 @@ class ApplicationSpec extends Specification {
       }
     }
     
-    "render an empty form on index" in {
+    "render the index page" in {
       running(FakeApplication()) {
         val home = routeAndCall(FakeRequest(GET, "/")).get
         
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/html")
+        contentAsString(home) must contain ("Your new application is ready.")
       }
     }
+  }
 }
