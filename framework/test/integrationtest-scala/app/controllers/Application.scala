ec71eb7dc4da29d57a274201318cda2e3aa5d2b6
Clement Escoffier <clement.escoffier@gmail.com>
2012-04-02 19:12:19 +0200

    #310 - Add a test checking the behavior of play.api.test.FakeRequest#withJsonBody
    Add a simple test in the Scala integration test checking the behavior of play.api.test.FakeRequest#withJsonBody. The test is similar to the Java test.

--- framework/test/integrationtest-scala/app/controllers/Application.scala
+++ framework/test/integrationtest-scala/app/controllers/Application.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api._
+import libs.json.Json
 import play.api.mvc._
 
 import play.api.Play.current
@@ -15,6 +16,15 @@ object Application extends Controller {
     Play.configuration.getString("key").map {
       key => Ok("Key=" + key)
     }.getOrElse(InternalServerError("Configuration missing"))
+  }
+
+  def json = Action { request =>
+    request.body.asJson.map { json =>
+      Ok(json)
+    }.getOrElse(
+        Ok(Json.toJson(Map("status" -> "KO", "message" -> "JSON Body missing")))
+    )
+
   }
   
 }
