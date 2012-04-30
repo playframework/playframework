b18e925a38c00bb5d70daf316fddf918ebff04c0
peter hausel <peter_kovac11@hotmail.com>
2012-04-30 18:39:44 -0400

    no code change, just adding an extra source folder to anorm to follow play subprojects' standard layout

---
+++ framework/src/anorm/src/main/scala/anorm/package.scala
@@ -0,0 +1,25 @@
+
+/**
+ * Anorm API
+ *
+ * Use the SQL method to start an SQL query
+ *
+ * {{{
+ * import anorm._
+ *
+ * SQL("Select 1")
+ * }}}
+ */
+package object anorm {
+
+  implicit def sqlToSimple(sql: SqlQuery): SimpleSql[Row] = sql.asSimple
+  implicit def sqlToBatch(sql: SqlQuery): BatchSql = sql.asBatch
+
+  implicit def implicitID[ID](id: Id[ID] with NotNull): ID = id.id
+
+  implicit def toParameterValue[A](a: A)(implicit p: ToStatement[A]): ParameterValue[A] =
+    ParameterValue(a, p)
+
+  def SQL(stmt: String) = Sql.sql(stmt)
+
+}
