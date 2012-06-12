ccbd7e1a87ce79fcbf7c75a1aa056ef966b94f6b
Ben McCann <ben@benmccann.com>
2012-06-11 18:19:49 -0700

    Fix compilation error for bad package name and warnings for unused imports

--- framework/skeletons/java-skel/test/ApplicationTest.java
+++ framework/skeletons/java-skel/test/ApplicationTest.java
@@ -1,24 +1,10 @@
-package test;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.junit.Test;
 
-import org.codehaus.jackson.JsonNode;
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
+import play.mvc.Content;
 
 
 /**
