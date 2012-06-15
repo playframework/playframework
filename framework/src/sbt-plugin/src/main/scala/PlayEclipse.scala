f7d1aa986aa9e9258eae5a91b41ec46ae879017a
peter hausel <peter_kovac11@hotmail.com>
2012-06-15 10:26:20 -0400

    [lh 175] (eclipsify) add classes_managed only if it exists

--- framework/src/sbt-plugin/src/main/scala/PlayEclipse.scala
+++ framework/src/sbt-plugin/src/main/scala/PlayEclipse.scala
@@ -40,8 +40,10 @@ trait PlayEclipse {
               new RewriteRule {
                 override def transform(node: Node): Seq[Node] = node match {
                   //add classes_managed  
-                  case elem if (elem.label == "classpathentry" && elem.attribute("path").getOrElse(err(elem)).toString.contains("org.scala-ide.sdt.launching.SCALA_CONTAINER")) =>
-                    <classpathentry path={ct + java.io.File.separator + "classes_managed"} kind="lib"></classpathentry>
+                  case elem if (elem.label == "classpathentry" && 
+                                elem.attribute("path").getOrElse(err(elem)).toString.contains("org.scala-ide.sdt.launching.SCALA_CONTAINER") && 
+                                new java.io.File(ct + f + "classes_managed").exists) =>
+                    <classpathentry path={ct + f + "classes_managed"} kind="lib"></classpathentry>
                  case other =>
                     other  
                 }
