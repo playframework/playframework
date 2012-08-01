e005555eb129c28e2d0610e4823bc0ba802854d1
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2012-07-31 21:38:00 -0700

    Merge commit 06923dc6 : using application.context to set the path for session and flash cookie

--- framework/src/play/src/main/scala/play/api/mvc/Http.scala
+++ framework/src/play/src/main/scala/play/api/mvc/Http.scala
@@ -282,6 +282,11 @@ package play.api.mvc {
     val secure = false
 
     /**
+     *  The cookie path. 
+     */
+    val path = "/"
+
+    /**
      * Encodes the data as a `String`.
      */
     def encode(data: Map[String, String]): String = {
@@ -334,7 +339,7 @@ package play.api.mvc {
      */
     def encodeAsCookie(data: T): Cookie = {
       val cookie = encode(serialize(data))
-      Cookie(COOKIE_NAME, cookie, maxAge, "/", None, secure, httpOnly)
+      Cookie(COOKIE_NAME, cookie, maxAge, path, None, secure, httpOnly)
     }
 
     /**
@@ -422,6 +427,7 @@ package play.api.mvc {
     override val secure = Play.maybeApplication.flatMap(_.configuration.getBoolean("session.secure")).getOrElse(false)
     override val maxAge = Play.maybeApplication.flatMap(_.configuration.getInt("session.maxAge")).getOrElse(-1)
     override val httpOnly = Play.maybeApplication.flatMap(_.configuration.getBoolean("session.httpOnly")).getOrElse(true)
+    override val path = Play.maybeApplication.flatMap(_.configuration.getString("application.context")).getOrElse("/")
 
     def deserialize(data: Map[String, String]) = new Session(data)
 
@@ -484,6 +490,8 @@ package play.api.mvc {
   object Flash extends CookieBaker[Flash] {
 
     val COOKIE_NAME = "PLAY_FLASH"
+    override val path = Play.maybeApplication.flatMap(_.configuration.getString("application.context")).getOrElse("/")
+
     val emptyCookie = new Flash
 
     def deserialize(data: Map[String, String]) = new Flash(data)
