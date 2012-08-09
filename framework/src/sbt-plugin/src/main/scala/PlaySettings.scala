170bcf6b69400652e93585a8e777f6b631fd2b8c
peter hausel <peter_kovac11@hotmail.com>
2012-08-08 21:51:05 -0400

    disable fork in Test for the time being

--- framework/src/sbt-plugin/src/main/scala/PlaySettings.scala
+++ framework/src/sbt-plugin/src/main/scala/PlaySettings.scala
@@ -88,7 +88,8 @@ trait PlaySettings {
 
     parallelExecution in Test := false,
     
-    fork in Test := true,
+    //TODO: this should be re-enabled once we know more about xsbt/issues/512
+    fork in Test := false,
     
     testOptions in Test += Tests.Setup { loader =>
       loader.loadClass("play.api.Logger").getMethod("init", classOf[java.io.File]).invoke(null, new java.io.File("."))
