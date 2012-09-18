1c74624d1929be9dfad543f419cffc38424f24f0
James Roper <james@jazzy.id.au>
2012-09-18 12:41:03 +1000

    Backported small change from b028c18d7910fffaef3e2a16529a5ec2df2cd69e to fix Scala compilation error in routes file

--- framework/src/play/src/main/scala/play/core/router/Router.scala
+++ framework/src/play/src/main/scala/play/core/router/Router.scala
@@ -1010,7 +1010,11 @@ object Router {
 
   object Route {
 
-    def apply(method: String, pathPattern: PathPattern) = new {
+    trait ParamsExtractor {
+      def unapply(request: RequestHeader): Option[RouteParams]
+    }
+
+    def apply(method: String, pathPattern: PathPattern) = new ParamsExtractor{
 
       def unapply(request: RequestHeader): Option[RouteParams] = {
         if (method == request.method) {
