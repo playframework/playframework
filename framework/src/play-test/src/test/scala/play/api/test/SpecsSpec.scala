86c7ba271e18c443508fb3236f86cff0adcddb35
James Roper <james@jazzy.id.au>
2012-09-19 13:11:14 +1000

    Fixed specs

--- framework/src/play-test/src/test/scala/play/api/test/SpecsSpec.scala
+++ framework/src/play-test/src/test/scala/play/api/test/SpecsSpec.scala
@@ -5,17 +5,19 @@ import play.api.{Play, Application}
 
 object SpecsSpec extends Specification {
 
+  sequential
+
   def fakeApp[A](elems: (String, String)*) = FakeApplication(additionalConfiguration = Map(elems:_*))
   def getConfig(key: String)(implicit app: Application) = app.configuration.getString(key)
 
   "WithApplication context" should {
-    "provide an app" in new WithApplication(fakeApp("foo" -> "bar")) {
+    "provide an app" in new WithApplication(fakeApp("foo" -> "bar", "ehcacheplugin" -> "disabled")) {
       app.configuration.getString("foo") must beSome("bar")
     }
-    "make the app available implicitly" in new WithApplication(fakeApp("foo" -> "bar")) {
-      getConfig("foo") must_== "bar"
+    "make the app available implicitly" in new WithApplication(fakeApp("foo" -> "bar",  "ehcacheplugin" -> "disabled")) {
+      getConfig("foo") must beSome("bar")
     }
-    "start the application" in new WithApplication(fakeApp("foo" -> "bar")) {
+    "start the application" in new WithApplication(fakeApp("foo" -> "bar",  "ehcacheplugin" -> "disabled")) {
       Play.maybeApplication must beSome(app)
     }
   }
