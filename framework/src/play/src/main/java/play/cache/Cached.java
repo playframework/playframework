78d64ddcbaf6e97c291b7ad54e625e7afd14a157
James Roper <james@jazzy.id.au>
2012-09-21 11:11:01 +1000

    Added missing import

--- framework/src/play/src/main/java/play/cache/Cached.java
+++ framework/src/play/src/main/java/play/cache/Cached.java
@@ -1,16 +1,22 @@
 package play.cache;
+import play.mvc.With;
+
 import java.lang.annotation.*;
 
 /**
  * Mark an action to be cached on server side.
- *
- * @param key The cache key
- * @param duration Cache duration in seconds
  */
 @With(CachedAction.class)
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Cached {
+    /**
+     * The cache key to store the result in
+     */
     String key();
+
+    /**
+     * The duration the action should be cached for.  Defaults to 0.
+     */
     int duration() default 0;
-}   
+}
