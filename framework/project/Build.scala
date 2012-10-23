0b4e798ea0ad00ab3a4f3ba5633b377bab2ef9ae
Ben McCann <ben@benmccann.com>
2012-10-23 08:08:12 -0700

    Remove dependency on org.apache.ws.commons

--- framework/project/Build.scala
+++ framework/project/Build.scala
@@ -464,7 +464,6 @@ object PlayBuild extends Build {
             "org.joda"                          %    "joda-convert"             %   "1.2",
             
             "org.apache.commons"                %    "commons-lang3"            %   "3.1",
-            "org.apache.ws.commons"             %    "ws-commons-util"          %   "1.0.1" exclude("junit", "junit"),
 
             ("com.ning"                         %    "async-http-client"        %   "1.7.6" notTransitive())
               .exclude("org.jboss.netty", "netty")
