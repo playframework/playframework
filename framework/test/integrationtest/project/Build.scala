ad67fc56ff4808a8dea1ec7090573cd6194eed1f
Dean Thompson <dthompson@linkedin.com>
2012-11-08 15:44:50 -0800

    Changed playPackageEverythingTask to package jar files returned from the packagedArtifacts task, instead of just the packageBin artifact.

--- framework/test/integrationtest/project/Build.scala
+++ framework/test/integrationtest/project/Build.scala
@@ -4,16 +4,16 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "integrationtest"
-    val appVersion      = "0.1"
+  val appName = "integrationtest"
+  val appVersion = "0.1"
 
-    val appDependencies = Seq(
-    	javaJdbc,
-    	javaCore,
-    	anorm
-    ) 
+  val appDependencies = Seq(
+    javaJdbc,
+    javaCore,
+    anorm)
 
-    val main = play.Project(appName, appVersion, appDependencies)
+  val distTestSettings = DistTest.makeSettings(appName, appVersion)
 
+  val main = play.Project(appName, appVersion, appDependencies).settings(distTestSettings: _*)
 }
             
