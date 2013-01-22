b88ff1e8ae12373f0ff6d6d873087aef918db64d
Henry Story <henry.story@bblfish.net>
2013-01-22 20:52:18 +0100

    add MKCOL

--- framework/src/routes-compiler/src/main/scala/play/router/RoutesCompiler.scala
+++ framework/src/routes-compiler/src/main/scala/play/router/RoutesCompiler.scala
@@ -145,7 +145,7 @@ object RoutesCompiler {
       }
     }
 
-    def httpVerb: Parser[HttpVerb] = namedError("GET" | "POST" | "PUT" | "PATCH" | "HEAD" | "DELETE" | "OPTIONS", "HTTP Verb expected") ^^ {
+    def httpVerb: Parser[HttpVerb] = namedError("GET" | "POST" | "PUT" | "PATCH" | "HEAD" | "DELETE" | "OPTIONS" | "MKCOL", "HTTP Verb expected") ^^ {
       case v => HttpVerb(v)
     }
 
