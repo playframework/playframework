092c079cf6ef21c57e052817f43538bcd1fe892b
Chris Williams <chwilliam@gmail.com>
2013-02-15 15:05:47 -0500

    #980 Added ability to use Scala Durations as cache expiration periods

--- framework/src/play/src/main/scala/play/api/cache/Cached.scala
+++ framework/src/play/src/main/scala/play/api/cache/Cached.scala
@@ -2,10 +2,11 @@ package play.api.cache
 
 import play.api._
 import play.api.mvc._
+import scala.concurrent.duration.Duration
 import reflect.ClassTag
 
 /**
- * Cache an action.
+ * Cache an action, with duration in seconds.
  *
  * @param key Compute a key from the request header
  * @param duration Cache duration (in seconds)
@@ -46,7 +47,18 @@ object Cached {
   }
 
   /**
-   * Cache an action.
+   * Cache an action.  Use Duration.Inf for infinite cache.
+   *
+   * @param key Cache key
+   * @param duration Cache duration (as a Scala Duration, rounded to seconds)
+   * @param action Action to cache
+   */
+  def apply[A](key: String, duration: Duration)(action: Action[A])(implicit app: Application): Cached[A] = {
+    apply(key, duration = Cache.durationToExpiration(duration) )(action)
+  }
+
+  /**
+   * Cache an action.  Duration of 0 is Infinite
    *
    * @param key Cache key
    * @param duration Cache duration (in seconds)
