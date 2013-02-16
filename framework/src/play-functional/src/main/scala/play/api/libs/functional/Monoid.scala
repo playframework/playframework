6e292ef1c288cfde7e6931bfa2ddeea79bb11315
Pascal Voitot <pascal.voitot.dev@gmail.com>
2013-02-16 14:30:08 +0100

    refactored play-functional to make it clearer + moved Constraints back to play core project because I want to keep Forms as is for now

---
+++ framework/src/play-functional/src/main/scala/play/api/libs/functional/Monoid.scala
@@ -0,0 +1,29 @@
+package play.api.libs.functional
+
+trait Monoid[A] {
+
+  def append(a1: A, a2: A): A
+  def identity: A
+
+}
+
+/* A practical variant of monoid act/action/operator (search on wikipedia)
+ * - allows to take an element A to create a B
+ * - allows a prepend/append a A to a B
+ * cf Reducer[JsValue, JsArray]
+ */
+trait Reducer[A, B] {
+
+  def unit(a: A): B
+  def prepend(a: A, b: B): B
+  def append(b: B, a: A): B
+  
+}
+
+object Reducer {
+  def apply[A, B](f: A => B)(implicit m: Monoid[B]) = new Reducer[A, B] {
+    def unit(a: A): B = f(a)
+    def prepend(a: A, b: B) = m.append(unit(a), b)
+    def append(b: B, a: A) = m.append(b, unit(a))
+  }
+}
---
+++ framework/src/play-functional/src/main/scala/play/api/libs/functional/syntax/package.scala
@@ -0,0 +1,51 @@
+package play.api.libs.functional.syntax
+
+import scala.language.higherKinds
+import scala.language.implicitConversions
+
+import play.api.libs.functional._
+
+/**
+  * Don't forget to {{{import play.api.libs.functional.syntax._}}} to enable functional combinators 
+  * when using Json API.
+  */
+object `package` {
+
+  implicit def toAlternativeOps[M[_],A](a: M[A])(implicit app: Alternative[M]): AlternativeOps[M,A] = new AlternativeOps(a)
+
+  implicit def toApplicativeOps[M[_],A](a: M[A])(implicit app: Applicative[M]): ApplicativeOps[M,A] = new ApplicativeOps(a)
+
+  implicit def toFunctionalBuilderOps[M[_],A](a: M[A])(implicit fcb: FunctionalCanBuild[M]) = new FunctionalBuilderOps[M,A](a)(fcb)
+
+  implicit def functionalCanBuildApplicative[M[_]](implicit app: Applicative[M]): FunctionalCanBuild[M] = new FunctionalCanBuild[M] {
+
+    def apply[A,B](a: M[A], b: M[B]): M[A~B] = app.apply(app.map[A, B => A ~ B](a, a => ((b: B) => new ~(a,b))),b)
+
+  }
+
+  implicit def functorOption: Functor[Option] = new Functor[Option] {
+
+    def fmap[A,B](a:Option[A], f: A => B):Option[B] = a.map(f)
+
+  }
+
+  implicit def applicativeOption: Applicative[Option] = new Applicative[Option]{
+
+    def pure[A](a: A):Option[A] = Some(a)
+
+    def map[A,B](m:Option[A], f: A => B):Option[B] = m.map(f)
+
+    def apply[A,B](mf:Option[A => B], ma: Option[A]):Option[B] = mf.flatMap(f => ma.map(f))
+
+  }
+
+  implicit def toFunctorOps[M[_], A](ma: M[A])(implicit fu: Functor[M]): FunctorOps[M, A] = new FunctorOps(ma)
+  implicit def toContraFunctorOps[M[_], A](ma: M[A])(implicit fu: ContravariantFunctor[M]): ContravariantFunctorOps[M, A] = new ContravariantFunctorOps(ma)
+  implicit def toInvariantFunctorOps[M[_], A](ma: M[A])(implicit fu: InvariantFunctor[M]): InvariantFunctorOps[M, A] = new InvariantFunctorOps(ma)
+
+  def unapply[B, A](f: B => Option[A]) = { b: B => f(b).get }
+
+  def unlift[A, B](f: A => Option[B]): A => B = Function.unlift(f)
+
+}
+
