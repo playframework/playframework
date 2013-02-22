a8735b70d904f7767db6b1b64222fa96916e1b27
xuwei-k <6b656e6a69@gmail.com>
2013-02-23 00:43:49 +0900

    use new Helper classes in skeleton test code

--- framework/skeletons/scala-skel/test/ApplicationSpec.scala
+++ framework/skeletons/scala-skel/test/ApplicationSpec.scala
@@ -14,20 +14,16 @@ class ApplicationSpec extends Specification {
   
   "Application" should {
     
-    "send 404 on a bad request" in {
-      running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beNone        
-      }
+    "send 404 on a bad request" in new WithApplication{
+      route(FakeRequest(GET, "/boum")) must beNone
     }
     
-    "render the index page" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
-        
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Your new application is ready.")
-      }
+    "render the index page" in new WithApplication{
+      val home = route(FakeRequest(GET, "/")).get
+      
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/html")
+      contentAsString(home) must contain ("Your new application is ready.")
     }
   }
 }
