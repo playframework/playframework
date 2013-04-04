c70a17f0a7d7fdd91c7c1d9f57621e3677ab2fd5
Sadek Drobi <sadek.drobi+github@gmail.com>
2013-04-04 21:55:43 +0200

    a bit of organization, added Future to App

--- framework/src/play-functional/src/main/scala/play/api/libs/functional/Functors.scala
+++ framework/src/play-functional/src/main/scala/play/api/libs/functional/Functors.scala
@@ -10,6 +10,16 @@ trait Functor[M[_]] extends Variant[M]{
 
 }
 
+object Functor {
+
+  implicit def functorOption: Functor[Option] = new Functor[Option] {
+
+    def fmap[A,B](a:Option[A], f: A => B):Option[B] = a.map(f)
+
+  }
+
+}
+
 trait InvariantFunctor[M[_]] extends Variant[M]{
 
   def inmap[A,B](m: M[A], f1: A => B, f2: B => A): M[B]
