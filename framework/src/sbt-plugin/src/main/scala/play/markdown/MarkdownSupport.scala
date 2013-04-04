0e3956d8944a3bc2138a222f859f1053314dcca3
James Roper <james@jazzy.id.au>
2013-04-04 16:23:44 +1100

    Integrated sample code tests into Play build

--- framework/src/sbt-plugin/src/main/scala/play/markdown/MarkdownSupport.scala
+++ framework/src/sbt-plugin/src/main/scala/play/markdown/MarkdownSupport.scala
@@ -43,6 +43,11 @@ object MarkdownSupport {
     new ToHtmlSerializer(links, util.Arrays.asList(new CodeReferenceSerializer(root, pagePath))).toHtml(astRoot)
   }
 
+  // Directives to insert code, skip code and replace code
+  val Insert = """.*###insert: (.*?)(?:###.*)?""".r
+  val Skip = """.*###skip:\s*(\d+).*""".r
+  val ReplaceNext = """.*###replace: (.*?)(?:###.*)?""".r
+
   class CodeReferenceSerializer(root: File, pagePath: String) extends ToHtmlSerializerPlugin {
     def visit(node: Node, visitor: Visitor, printer: Printer) = node match {
       case code: CodeReferenceNode => {
@@ -67,11 +72,45 @@ object MarkdownSupport {
         // Find the code segment
         val sourceCode = Path(sourceFile).lines()
         val notLabel = (s: String) => !s.contains("#" + label)
-        val segment = sourceCode dropWhile(notLabel) drop(1) takeWhile(notLabel) mkString("\n")
-
+        val segment = sourceCode dropWhile(notLabel) drop(1) takeWhile(notLabel)
         if (segment.isEmpty) {
           throw new IllegalArgumentException("Could not find segment " + label + " in source file " + sourceFile)
         }
+
+        // Calculate the indent, which is equal to the smallest indent of any line, excluding lines that only consist
+        // of space characters
+        val indent = segment map { line =>
+          if (!line.exists(_ != ' ')) None else Some(line.indexWhere(_ != ' '))
+        } reduce ((i1, i2) => (i1, i2) match {
+          case (None, None) => None
+          case (i, None) => i
+          case (None, i) => i
+          case (Some(i1), Some(i2)) => Some(math.min(i1, i2))
+        }) getOrElse(0)
+
+        // Process directives in segment
+        case class State(buffer: StringBuilder = new StringBuilder, skip: Option[Int] = None) {
+          def dropIndentAndAppendLine(s: String): State = {
+            buffer.append(s.drop(indent)).append("\n")
+            this
+          }
+          def appendLine(s: String): State = {
+            buffer.append(s).append("\n")
+            this
+          }
+        }
+        val compiledSegment = (segment.foldLeft(State()) { (state, line) =>
+          state.skip match {
+            case Some(n) if (n > 1) => state.copy(skip = Some(n - 1))
+            case Some(n) => state.copy(skip = None)
+            case None => line match {
+              case Insert(code) => state.appendLine(code)
+              case Skip(n) => state.copy(skip = Some(n.toInt))
+              case ReplaceNext(code) => state.appendLine(code).copy(skip = Some(1))
+              case _ => state.dropIndentAndAppendLine(line)
+            }
+          }
+        }).buffer/* Drop last newline */.dropRight(1).toString()
 
         // Guess the type of the file
         val fileType = source.split("\\.") match {
@@ -80,7 +119,7 @@ object MarkdownSupport {
         }
 
         // And visit it
-        fileType.map(t => new VerbatimNode(segment, t)).getOrElse(new VerbatimNode(segment)).accept(visitor)
+        fileType.map(t => new VerbatimNode(compiledSegment, t)).getOrElse(new VerbatimNode(compiledSegment)).accept(visitor)
 
         true
       }
