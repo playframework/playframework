5a05f060409887ca36afb1285ca2159cb264be46
Ricky Elrod <ricky@elrod.me>
2013-04-16 10:25:35 -0400

    Now that we have Scalariform on core, let's pretty up the skeleton files.

--- framework/skeletons/scala-skel/test/IntegrationSpec.scala
+++ framework/skeletons/scala-skel/test/IntegrationSpec.scala
@@ -10,17 +10,17 @@ import play.api.test.Helpers._
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
 class IntegrationSpec extends Specification {
-  
+
   "Application" should {
-    
+
     "work from within a browser" in new WithBrowser{
 
       browser.goTo("http://localhost:" + port)
 
       browser.pageSource must contain("Your new application is ready.")
-       
+
     }
-    
+
   }
-  
+
 }
