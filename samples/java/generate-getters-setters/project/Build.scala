eaba3aa0fc8805ae755a185752513ba21565c44c
Kevin Sheedy <kevinsheedy@gmail.com>
2013-05-16 16:37:19 +0100

    Created a new project. Not yet modified.

---
+++ samples/java/generate-getters-setters/project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "generate-getters-setters"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
