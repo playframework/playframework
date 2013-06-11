9457688fb577dbcd7b87bae12e223e801474892e
Marcus Linke <linke@newsaktuell.de>
2013-06-11 15:32:14 +0200

    Issue 1196: Added spec/test

---
+++ framework/src/play/src/test/scala/play/api/mvc/ContentTypesSpec.scala
@@ -0,0 +1,18 @@
+package play.api.mvc
+
+import org.specs2.mutable.Specification
+
+object ContentTypesSpec extends Specification {
+
+  import play.api.mvc.BodyParsers.parse.Multipart._
+
+  "FileInfoMatcher" should {
+
+    "parse headers with semicolon inside quotes" in {
+      val result = FileInfoMatcher.unapply(Map("content-disposition" -> "form-data; name=\"document\"; filename=\"semicolon;inside.jpg\"", "content-type" -> "image/jpeg"))
+      result must not beEmpty;
+      result.get must equalTo(("document", "semicolon;inside.jpg", Option("image/jpeg")));
+    }
+
+  }
+}
