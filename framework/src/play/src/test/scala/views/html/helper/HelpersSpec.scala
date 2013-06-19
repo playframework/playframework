ad97c0687fdec475e71c7b18f97e2e837267b0c7
Timshel <timshel@R700.(none)>
2013-06-19 21:03:43 +0200

    Change the select to support multiple

--- framework/src/play/src/test/scala/views/html/helper/HelpersSpec.scala
+++ framework/src/play/src/test/scala/views/html/helper/HelpersSpec.scala
@@ -36,4 +36,41 @@ object HelpersSpec extends Specification {
       body.substring(body.indexOf(typeAttr) + typeAttr.length) must not contain(typeAttr)
     }
   }
+
+  "@select" should {
+
+    "allow setting a custom id" in {
+
+      val body = select.apply(Form(single("foo" -> Forms.text))("foo"),Seq(("0", "test")), 'id -> "someid").body
+
+      val idAttr = "id=\"someid\""
+      body must contain(idAttr)
+
+      // Make sure it doesn't have it twice, issue #478
+      body.substring(body.indexOf(idAttr) + idAttr.length) must not contain(idAttr)
+    }
+
+    "Work as a simple select" in {
+      val form = Form(single("foo" -> Forms.text)).fill("0")
+      val body = select.apply(form("foo"), Seq(("0", "test"), ("1", "test"))).body
+
+      body must contain( "name=\"foo\"" )
+
+      body must contain( """<option value="0" selected>""" )
+      body must contain( """<option value="1" >""" )
+    }
+
+    "Work as a multiple select" in {
+      val form = Form(single("foo" -> Forms.list(Forms.text))).fill(List("0", "1"))
+      val body = select.apply(form("foo"), Seq(("0", "test"), ("1", "test")), 'multiple -> None).body
+
+      // Append [] to the name for the form binding
+      body must contain( "name=\"foo[]\"" )
+      body must contain( "multiple" )
+
+      body must contain( """<option value="0" selected>""" )
+      body must contain( """<option value="1" selected>""" )
+    }
+
+  }
 }
