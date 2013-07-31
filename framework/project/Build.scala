f1dcc10d50666c789a9132d6048a45cf7f5dfba9
Christopher Hunt <huntchr@gmail.com>
2013-07-31 11:12:27 +1000

    Applying #1406 fix to the 2.1.x branch.

--- framework/project/Build.scala
+++ framework/project/Build.scala
@@ -525,7 +525,7 @@ object PlayBuild extends Build {
             specsBuild % "test",
 
             "org.mockito"                       %    "mockito-all"              %   "1.9.0"    %  "test",
-            "com.novocode"                      %    "junit-interface"          %   "0.9"      %  "test",
+            "com.novocode"                      %    "junit-interface"          %   "0.10"     %  "test",
 
             "org.fluentlenium"                  %    "fluentlenium-festassert"  %   "0.7.3"    %  "test" exclude("org.jboss.netty", "netty"),
             "org.scala-lang"                    %    "scala-reflect"            %   "2.10.0"
@@ -598,7 +598,7 @@ object PlayBuild extends Build {
         val testDependencies = Seq(
             "junit"                             %    "junit-dep"                %   "4.10",
             specsBuild,
-            "com.novocode"                      %    "junit-interface"          %   "0.9",
+            "com.novocode"                      %    "junit-interface"          %   "0.10",
 
             "org.fluentlenium"                  %    "fluentlenium-festassert"  %   "0.7.3" exclude("org.jboss.netty", "netty")
         )
