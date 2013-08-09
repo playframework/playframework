e859273785b5c8228a08f00f4217280cf1c2f95a
jto <boudhevil@gmail.com>
2013-08-09 17:13:33 +0200

    Path: scaladoc

--- framework/src/play-datacommons/src/main/scala/play/api/data/validation/Path.scala
+++ framework/src/play-datacommons/src/main/scala/play/api/data/validation/Path.scala
@@ -39,12 +39,49 @@ class Path[I](val path: List[PathNode]) {
 
   def as[J] = Path[J](path)
 
+  /**
+  * Aggregate 2 paths
+  * {{{
+  *   val __ = Path[JsValue]()
+  *   (__ \ "foo" \ "bar").compose(__ \ "baz") == (__ \ "foo" \ "bar" \ "baz")
+  * }}}
+  */
   def compose(p: Path[I]): Path[I] = Path(this.path ++ p.path)
   def ++(other: Path[I]) = this compose other
 
+  /**
+  * Creates a Rule searching and validating data in a stucture of type I
+  * When applied, the rule will lookup for data at the given path, and apply the given Constraint on it
+  * {{{
+  *   val __ = Path[JsValue]()
+  *   val json = Json.parse("""{"firstname": "Julien"}""")
+  *   val r = (__ \ "firstame").read(nonEmptyText)
+  *   r.validate(json) == Success("Julien")
+  * }}}
+  * @param v the constraint to apply on data
+  * @param m a lookup function. This function finds data in a structure of type I, and coerce it to tyoe O
+  * @return A Rule validating the presence and validity of data at this Path
+  */
   def read[O](v: Constraint[O])(implicit m: Path[I] => Mapping[ValidationError, I, O]): Rule[I, O] =
     Rule(this, (p: Path[I]) => (d: I) => m(p)(d).fail.map{ errs => Seq(p -> errs) }, v) // XXX: DRY "fail.map" thingy
 
+  /**
+  * When applied, the rule will lookup for data at the given path, and apply the given Constraint on it
+  * {{{
+  *   val __ = Path[JsValue]()
+  *   val json = Json.parse("""{
+  *      "informations": {
+  *        "label": "test"
+  *      }
+  *   }""")
+  *   val infoValidation = (__ \ "label").read(nonEmptyText)
+  *   val v = (__ \ "informations").read(infoValidation))
+  *   v.validate(json) == Success("test")
+  * }}}
+  * @param sub the constraint to apply on the subdata
+  * @param l a lookup function. This function finds data in a structure of type I, and coerce it to tyoe O
+  * @return A Rule validating the presence and validity of data at this Path
+  */
   def read[J, O](sub: Rule[J, O])(implicit l: Path[I] => Mapping[ValidationError, I, J]): Rule[I, O] = {
     val parent = this
     Rule(parent compose Path[I](sub.p.path), { p => d =>
@@ -57,9 +94,32 @@ class Path[I](val path: List[PathNode]) {
     }, sub.v)
   }
 
+  /**
+  * Creates a Rule searching data in a stucture of type I
+  * When applied, the rule will lookup for data at the given path, and coerce it to type O
+  * {{{
+  *   val __ = Path[JsValue]()
+  *   val json = Json.parse("""{"company": "typesafe"}""")
+  *   val r = (__ \ "company").read[Option[String]]
+  *   r.validate(validJson) == Success(Some("typesafe"))
+  * }}}
+  * @param m a lookup function. This function finds data in a structure of type I, and coerce it to tyoe O
+  * @return A Rule validating the presence of data at this Path
+  */
   def read[O](implicit m: Path[I] => Mapping[ValidationError, I, O]): Rule[I, O] =
     read(Constraints.noConstraint[O])
 
+  /**
+  * Creates a Writes the serialize data to the desired type
+  * {{{
+  *   val contact = Contact("Julien", "Tournay")
+  *   val __ = Path[Map[String, Seq[String]]]()
+  *   implicit def contactWrite = (__ \ "firstname").write[String]
+  *   Writes[Contact, Map[String, Seq[String]]](contact) mustEqual Map("firstname" -> "Julien")
+  * }}}
+  * @param m a lookup function. This function finds data in a structure of type I, and coerce it to tyoe O
+  * @return A Rule validating the presence of data at this Path
+  */
   def write[In](implicit w: Path[I] => Writes[In, I]) = w(this)
 
   override def toString = this.path match {
