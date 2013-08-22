c6c04a8a91f58e740a3e0850444707f4930c5833
wsargent <will.sargent@gmail.com>
2013-08-21 18:49:15 -0700

    Make the samples usable with Eclipse out of the box, by leveraging the JUnitRunner.

--- samples/scala/zentasks/test/ApplicationSpec.scala
+++ samples/scala/zentasks/test/ApplicationSpec.scala
@@ -1,10 +1,11 @@
-package test
-
 import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
 
+@RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
   
   
