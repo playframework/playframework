f26e046b05e58459ac221b5383969d2126172fb8
James Roper <james@jazzy.id.au>
2013-08-31 11:07:02 +1000

    Updated SBT link documentation

--- framework/src/sbt-link/src/main/java/play/core/server/ServerWithStop.java
+++ framework/src/sbt-link/src/main/java/play/core/server/ServerWithStop.java
@@ -1,8 +1,20 @@
 package play.core.server;
 
+/**
+ * A server that can be stopped.
+ */
 public interface ServerWithStop {
 
+    /**
+     * Stop the server.
+     */
 	public void stop();
+
+    /**
+     * Get the address of the server.
+     *
+     * @return The address of the server.
+     */
 	public java.net.InetSocketAddress mainAddress(); 
 
 }
