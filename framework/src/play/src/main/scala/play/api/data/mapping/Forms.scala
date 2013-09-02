b3ba7b04831c7029aaa6ff422e186f1f5d1e0984
jto <boudhevil@gmail.com>
2013-09-02 22:16:19 +0200

    Path parser

--- framework/src/play/src/main/scala/play/api/data/mapping/Forms.scala
+++ framework/src/play/src/main/scala/play/api/data/mapping/Forms.scala
@@ -4,42 +4,58 @@ import play.api.{ data => d }
 
 object Form {
 
-	import scala.util.parsing.combinator.RegexParsers
+	import scala.util.parsing.combinator.{ Parsers, RegexParsers }
 	object PathParser extends RegexParsers {
 	  override type Elem = Char
 	  def int 	= """\d""".r ^^ { _.toInt }
-	  def key  = """\w+""".r ^^ { KeyPathNode(_) }
 	  def idx 	= "[" ~> int <~ "]" ^^ { IdxPathNode(_) }
+    def key   = rep1(not("." | idx) ~> ".".r) ^^ { ks => KeyPathNode(ks.mkString) }
 	  def node  = key ~ opt(idx) ^^ { case k ~ i => k :: i.toList }
-	  def path  = repsep(node, ".") ^^ { ns => Path(ns.flatten) }
+	  def path  = (opt(idx) ~ repsep(node, ".")) ^^ { case i ~ ns => Path(i.toList ::: ns.flatten) }
 
-	  def parse(s: String) = parseAll(path, new scala.util.parsing.input.CharArrayReader(s.toArray))
+    def parse(s: String) = parseAll(path, new scala.util.parsing.input.CharArrayReader(s.toArray))
 	}
 
-  def asKey(p: Path) = p.path.head.toString + p.path.tail.foldLeft("") {
-    case (path, IdxPathNode(i)) => path + s"[$i]"
-    case (path, KeyPathNode(k)) => path + "." + k
+  def toPM(m: Map[String, Seq[String]]) =
+    m.map { case (p, v) => asPath(p) -> v }
+
+  def asNodeKey(n: PathNode): String = n match {
+    case IdxPathNode(i) => s"[$i]"
+    case KeyPathNode(k) => k
   }
 
-  def asPath(k: String) = PathParser.parse(k).get
-
-  def fill[T](t: T)(implicit w: Writes[T, Map[String, Seq[String]]]) = Form(w.writes(t), Nil, Some(t))
-
-  def apply[T](data: Map[String, Seq[String]], validation: VA[Map[String, Seq[String]], T]): Form[T] = validation match {
-  	case Success(d) => Form(data, Nil, Some(d))
-  	case Failure(es) => Form(data, es, None)
+  def asKey(p: Path): String = p.path.headOption.toList.map(asNodeKey).mkString ++ p.path.foldLeft("") {
+    case (path, n@IdxPathNode(i)) => path + asNodeKey(n)
+    case (path, n@KeyPathNode(k)) => path + "." + asNodeKey(n)
   }
+
+  def asPath(k: String): Path = PathParser.parse(k) match {
+    case PathParser.Failure(m, _) => throw new RuntimeException(s"Invalid field name $k: $m")
+    case PathParser.Error(m, _) => throw new RuntimeException(s"Invalid field name $k: $m")
+    case PathParser.Success(r, _) => r
+  }
+
+  // def fill[T](t: T)(implicit w: Writes[T, Map[Path, Seq[String]]]) =
+  //   Form(w.writes(t))
 }
 
-case class Form[T](data: Map[String, Seq[String]] = Map.empty, errors: Seq[(Path, Seq[ValidationError])] = Nil, value: Option[T] = None) {
-  lazy val hasErrors: Boolean = !errors.isEmpty
+case class Form[T](data: Map[String, Seq[String]] = Map.empty, validation: Validation[(Path, Seq[ValidationError]), T] = Failure(Nil)) {lazy val hasErrors: Boolean = !errors.isEmpty
+
+  val dataP = Form.toPM(data)
+  lazy val errors: Seq[(Path, Seq[ValidationError])] =
+    validation.fold(identity, _ => Nil)
+  lazy val value: Option[T] =
+    validation.fold(_ => None, Some(_))
 
   def apply(key: String): Field = apply(Form.asPath(key))
 
   def apply(path: Path): Field = {
-    val value = data.get(Form.asKey(path)).flatMap(_.headOption)
+    val value = dataP.get(path).flatMap(_.headOption)
     Field(this, path, value)
   }
+
+  // def fill(t: T)(implicit w: Writes[T, Map[Path, Seq[String]]]) =
+  //   Form.fill(t)
 }
 
 class Field(private val form: Form[_], val path: Path, override val value: Option[String])
@@ -51,22 +67,24 @@ class Field(private val form: Form[_], val path: Path, override val value: Optio
   def apply(index: Int): Field =
     apply(Path() \ index)
 
-  override val errors = form.errors.filter(_._1.path.startsWith(path.path))
-    .flatMap { case (p, errs) =>
-      errs.map(e => d.FormError(Form.asKey(p), e.message))
-    }
+  override val errors =
+    form.errors.filter{ e =>
+        e._1.path.startsWith(path.path)
+      }
+      .flatMap { case (p, errs) =>
+        errs.map(e => d.FormError(Form.asKey(p), e.message))
+      }
 
   def apply(_path: Path): Field = {
-    val expanded = path.compose(_path)
-    val prefix = Form.asKey(expanded)
-    val d = form.data.filter(_._1.startsWith(prefix)).map(_._2).flatten
-    Field(form, expanded, d.headOption)
+    val p = path ++ _path
+    val d = form.dataP.filter(_._1.path.startsWith(p.path)).map(_._2).flatten
+    Field(form, p, d.headOption)
   }
 
   override val name = Form.asKey(path)
 
   override lazy val indexes: Seq[Int] = {
-    form.data.keys.map(Form.asPath)
+    form.dataP.keys
       .filter(_.path.startsWith(path.path))
       .map(_.path.drop(path.path.length).head)
       .flatMap{
@@ -74,9 +92,11 @@ class Field(private val form: Form[_], val path: Path, override val value: Optio
         case _ => Seq()
       }.toSeq
   }
+
+  override def toString = s"Field($form, ${path.path}, $value)"
 }
 
 object Field {
-	def apply(form: Form[_], path: Path, value: Option[String]) =
-		new Field(form, path, value)
+	def apply(form: Form[_], path: Path, value: Option[String]) = new Field(form, path, value)
+  def unapply(f: Field) = Some((f.form, f.path, f.value))
 }
