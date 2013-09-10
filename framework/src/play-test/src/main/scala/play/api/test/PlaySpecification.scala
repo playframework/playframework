6e6a3ccdabe0c4795ce530a626a3e1799ec8d987
Rich Dougherty <rich@rd.gen.nz>
2013-09-10 14:11:15 +1200

    Ensures that DTDs do not cause resources to be downloaded
    (Backport of #1622/dc94b94 from master)

---
+++ framework/src/play-test/src/main/scala/play/api/test/PlaySpecification.scala
@@ -0,0 +1,19 @@
+package play.api.test
+
+import org.specs2.mutable.Specification
+import org.specs2.time.NoTimeConversions
+import play.api.http.{ Status, HeaderNames }
+
+/**
+ * Play specs2 specification.
+ *
+ * This trait excludes some of the mixins provided in the default specs2 specification that clash with Play helpers
+ * methods.  It also mixes in the Play test helpers and types for convenience.
+ */
+trait PlaySpecification extends Specification
+    with NoTimeConversions
+    with HeaderNames
+    with Status
+    with DefaultAwaitTimeout
+    with FutureAwaits {
+}
