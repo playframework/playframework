f918214adddea931eb2ffe72e00d6ad541c4e737
blopker <lopkerk@gmail.com>
2013-09-21 13:14:59 -0700

    Accept all WebSocket subprotocols.

--- framework/src/play/src/main/scala/play/core/server/websocket/WebSocketHandshake.scala
+++ framework/src/play/src/main/scala/play/core/server/websocket/WebSocketHandshake.scala
@@ -31,7 +31,7 @@ object WebSocketHandshake {
 
   def shake(ctx: ChannelHandlerContext, req: HttpRequest): Unit = {
     val factory = new WebSocketServerHandshakerFactory(getWebSocketLocation(req),
-      null, /* subprotocols */
+      "*", /* wildcard to accept all subprotocols */
       true /* allowExtensions */ )
 
     val shaker = factory.newHandshaker(req)
