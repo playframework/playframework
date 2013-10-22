d997bcd56eda48798d46caee6b672c814be9ddc9
Arthur Gautier <aga@zenexity.com>
2013-10-22 22:03:16 +0200

    [#1846] Attempt to make message parsing faster

--- framework/src/play/src/main/scala/play/api/i18n/Messages.scala
+++ framework/src/play/src/main/scala/play/api/i18n/Messages.scala
@@ -67,7 +67,7 @@ object Lang {
   }
 
   private val SimpleLocale = """([a-zA-Z]{2})""".r
-  private val CountryLocale = """([a-zA-Z]{2})-([a-zA-Z]{2}|[0-9]{3})""".r
+  private val CountryLocale = (SimpleLocale.toString + """-([a-zA-Z]{2}|[0-9]{3})""").r
 
   /**
    * Create a Lang value from a code (such as fr or en-US) and
@@ -175,7 +175,7 @@ object Messages {
     case class Comment(msg: String)
 
     override def skipWhitespace = false
-    override val whiteSpace = """[ \t]+""".r
+    override val whiteSpace = """^[ \t]+""".r
 
     def namedError[A](p: Parser[A], msg: String) = Parser[A] { i =>
       p(i) match {
@@ -184,32 +184,35 @@ object Messages {
       }
     }
 
-    def end = """\s*""".r
-    def newLine = namedError((("\r"?) ~> "\n"), "End of line expected")
-    def blankLine = ignoreWhiteSpace <~ newLine ^^ { case _ => Comment("") }
-    def ignoreWhiteSpace = opt(whiteSpace)
+    val end = """^\s*""".r
+    val newLine = namedError((("\r"?) ~> "\n"), "End of line expected")
+    val ignoreWhiteSpace = opt(whiteSpace)
+    val blankLine = ignoreWhiteSpace <~ newLine ^^ { case _ => Comment("") }
 
-    def comment = """#.*""".r ^^ { case s => Comment(s) }
+    val comment = """^#.*""".r ^^ { case s => Comment(s) }
 
-    def messageKey = namedError("""[a-zA-Z0-9_.-]+""".r, "Message key expected")
+    val messageKey = namedError("""^[a-zA-Z0-9_.-]+""".r, "Message key expected")
 
-    def messagePattern = namedError(
+    val messagePattern = namedError(
       rep(
-        """\""" ~> ("\r"?) ~> "\n" ^^ (_ => "") | // Ignore escaped end of lines \
-          """\n""" ^^ (_ => "\n") | // Translate literal \n to real newline
-          """\\""" ^^ (_ => """\""") | // Handle escaped \\
-          """.""".r // Or any character
+        ("""\""" ^^ (_ => "")) ~> ( // Ignore the leading \
+          ("\r"?) ~> "\n" ^^ (_ => "") | // Ignore escaped end of lines \
+          "n" ^^ (_ => "\n") | // Translate literal \n to real newline
+          """\""" | // Handle escaped \\
+          "^.".r ^^ ("""\""" + _)
+        ) |
+          "^.".r // Or any character
       ) ^^ { case chars => chars.mkString },
       "Message pattern expected"
     )
 
-    def message = ignoreWhiteSpace ~ messageKey ~ (ignoreWhiteSpace ~ "=" ~ ignoreWhiteSpace) ~ messagePattern ^^ {
+    val message = ignoreWhiteSpace ~ messageKey ~ (ignoreWhiteSpace ~ "=" ~ ignoreWhiteSpace) ~ messagePattern ^^ {
       case (_ ~ k ~ _ ~ v) => Messages.Message(k, v.trim, messageInput, messageSourceName)
     }
 
-    def sentence = (comment | positioned(message)) <~ newLine
+    val sentence = (comment | positioned(message)) <~ newLine
 
-    def parser = phrase((sentence | blankLine *) <~ end) ^^ {
+    val parser = phrase((sentence | blankLine *) <~ end) ^^ {
       case messages => messages.collect {
         case m @ Messages.Message(_, _, _, _) => m
       }
@@ -284,7 +287,7 @@ class MessagesPlugin(app: Application) extends Plugin {
   import scalax.file._
   import scalax.io.JavaConverters._
 
-  private def loadMessages(file: String): Map[String, String] = {
+  protected def loadMessages(file: String): Map[String, String] = {
     app.classloader.getResources(file).asScala.toList.reverse.map { messageFile =>
       new Messages.MessagesParser(messageFile.asInput, messageFile.toString).parse.map { message =>
         message.key -> message.pattern
@@ -292,7 +295,7 @@ class MessagesPlugin(app: Application) extends Plugin {
     }.foldLeft(Map.empty[String, String]) { _ ++ _ }
   }
 
-  private lazy val messages = {
+  protected lazy val messages = {
     MessagesApi {
       Lang.availables(app).map(_.code).map { lang =>
         (lang, loadMessages("messages." + lang))
