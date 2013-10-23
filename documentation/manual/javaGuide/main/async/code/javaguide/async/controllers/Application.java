2b2f0b4c7e555a78514ada3f4298aad2e28929dc
Rich Dougherty <rich@rd.gen.nz>
2013-10-24 09:28:01 +1300

    Added test for JavaAsync doc's controller method signature

--- documentation/manual/javaGuide/main/async/code/javaguide/async/controllers/Application.java
+++ documentation/manual/javaGuide/main/async/code/javaguide/async/controllers/Application.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
  */
-package javaguide.async;
+package javaguide.async.controllers;
 
 import play.mvc.Result;
 import play.libs.F.Function;
