42ee93503192bc1fd691f0277c26ccd397463bd2
wsargent <will.sargent@gmail.com>
2013-10-29 00:29:54 -0700

    Add commons-codec

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -82,7 +82,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-slf4j" % "2.2.0",
 
     "org.scala-stm" %% "scala-stm" % "0.7",
-
+    "commons-codec" % "commons-codec" % "1.7",
 
     "joda-time" % "joda-time" % "2.2",
     "org.joda" % "joda-convert" % "1.3.1",
