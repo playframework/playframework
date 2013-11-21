6c00acbd3a4f3cf359ccdd635227661e19d1f08d
Yann Simon <yann.simon.fr@gmail.com>
2013-11-21 12:45:47 +0100

    introduce testing API to call EssentialAction

---
+++ documentation/manual/scalaGuide/main/tests/code/ExampleEssentialActionSpec.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
+ */
+package scalaguide.tests
+
+import play.api.mvc._
+import play.api.test._
+import play.api.mvc.Results._
+import play.api.libs.json.Json
+
+// #scalatest-exampleessentialactionspec
+object ExampleEssentialActionSpec extends PlaySpecification {
+
+  "An essential action" should {
+    "can parse a JSON body" in {
+      val action: EssentialAction = Action { request =>
+        val value = (request.body.asJson.get \ "field").as[String]
+        Ok(value)
+      }
+
+      val request = FakeRequest(POST, "/").withJsonBody(Json.parse("""{ "field": "value" }"""))
+
+      val result = call(action, request)
+
+      status(result) mustEqual OK
+      contentAsString(result) mustEqual "value"
+    }
+  }
+}
+// #scalatest-exampleessentialactionspec
