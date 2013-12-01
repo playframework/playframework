b8e1691f793677a782435961174ed9b253a2b00a
Filippo De Luca <me@filippodeluca.com>
2013-12-01 21:12:37 +0000

    Adds set repeated mapping
    In the Forms there are list and seq to define a repeated mapping, but not set. Altought HTTP request doesn't permit to have a set, it can contains repetitions and the order is preserved, it is very common to have a data object having a member with Set semantic instead.

--- framework/src/play/src/main/scala/play/api/data/Forms.scala
+++ framework/src/play/src/main/scala/play/api/data/Forms.scala
@@ -401,6 +401,18 @@ object Forms {
    * @param mapping The mapping to make repeated.
    */
   def seq[A](mapping: Mapping[A]): Mapping[Seq[A]] = RepeatedMapping(mapping).transform(_.toSeq, _.toList)
+  
+  /**
+   * Defines a repeated mapping.
+   * {{{
+   * Form(
+   *   "name" -> set(text)
+   * )
+   * }}}
+   *
+   * @param mapping The mapping to make repeated.
+   */
+  def set[A](mapping: Mapping[A]): Mapping[Set[A]] = RepeatedMapping(mapping).transform(_.toSet, _.toList)
 
   /**
    * Constructs a simple mapping for a date field.
