cc26fdb510b83a564178c9aaf7c4ef551607fc7d
Will Sargent <will.sargent@gmail.com>
2014-01-29 00:04:39 +0100

    Add WS integration test
    Better unwrap for CompositeCertificateException
    Add a generated.truststore to FakeKeyStore, leverage truststore for
    integration test

--- framework/src/play-ws/src/main/scala/play/api/libs/ws/ssl/CompositeCertificateException.scala
+++ framework/src/play-ws/src/main/scala/play/api/libs/ws/ssl/CompositeCertificateException.scala
@@ -13,3 +13,22 @@ import java.security.cert.CertificateException
 class CompositeCertificateException(msg: String, val throwables: Array[Throwable]) extends CertificateException(msg) {
   def getSourceExceptions: Array[Throwable] = throwables
 }
+
+object CompositeCertificateException {
+
+  def unwrap(e: Throwable)(block: Throwable => Unit) = {
+    var cause: Throwable = e
+    while (cause != null) {
+      cause match {
+        case composite: CompositeCertificateException =>
+          composite.getSourceExceptions.foreach { sourceException =>
+            block(sourceException)
+          }
+        case other =>
+          block(other)
+      }
+      cause = cause.getCause
+    }
+  }
+
+}
---
+++ framework/test/integrationtest-scala/test/play/api/libs/ws/ssl/WsSslSpec.scala
@@ -0,0 +1,63 @@
+package play.api.libs.ws.ssl
+
+import play.api.test._
+import play.api.libs.ws.WS
+import play.api.test.TestServer
+import play.api.test.FakeApplication
+import play.api.mvc.{Results, Action}
+import com.typesafe.config.ConfigFactory
+import scala.util.control.NonFatal
+
+object WsSslSpec extends PlaySpecification with WsTestClient with Results {
+
+  sequential
+
+  "WS SSL" should {
+
+    "run against a local test server configured with SSL" in {
+      try {
+        // FakeKeyStore should provide us with generated.keystore and generated.truststore, we cannot use OCSP here
+        val config = """
+                       |ws.ssl {
+                       | loose.disableCheckRevocation = true
+                       | keyManager = {
+                       |   stores = [ { type: "JKS", path: ./conf/generated.keystore, password: "" } ]
+                       | }
+                       | trustManager = {
+                       |   stores = [ { type: "JKS", path: ./conf/generated.truststore, password: "" } ]
+                       | }
+                       |}
+                       | """.stripMargin
+
+        // simplest possible app
+        implicit val application = FakeApplication(additionalConfiguration = configToMap(config), withRoutes = {
+          case ("GET", "/ssl") => Action {
+            Ok("ok!")
+          }
+        })
+
+        try {
+          val server = TestServer(port = Helpers.testServerPort, application, sslPort = Some(11443))
+          Helpers.running(server) {
+            val client = WS.client
+            await(client.url("https://localhost:11443/ssl").get()).status must_== 200
+          }
+          success
+        } catch {
+          case NonFatal(e) =>
+            // JSSE stacktraces are embarrassingly deep
+            CompositeCertificateException.unwrap(e) { ex =>
+              ex.printStackTrace()
+            }
+            failure
+        }
+      }
+    }
+  }
+
+  def configToMap(configString: String): Map[String, _] = {
+    import scala.collection.JavaConverters._
+    ConfigFactory.parseString(configString).root().unwrapped().asScala.toMap
+  }
+
+}
