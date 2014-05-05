f56d0d0c8584a7b4e41bceb53fa5f5e128b6727d
James Roper <james@jazzy.id.au>
2014-05-05 15:00:41 +1000

    Added note about session.maxAge changing to migration guide
    Also added a MigrationHelper plugin that detects errors caused by not
    following the migration guide.

---
+++ framework/src/play-integration-test/src/test/scala/play/it/LogTester.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package play.it
+
+import org.slf4j.LoggerFactory
+import ch.qos.logback.core.AppenderBase
+import ch.qos.logback.classic.spi.ILoggingEvent
+import scala.collection.mutable.ListBuffer
+import ch.qos.logback.classic.{Logger, LoggerContext, Level}
+import play.api.Play
+
+/**
+ * Test utility for testing Play logs
+ */
+object LogTester {
+
+  def withLogBuffer[T](block: LogBuffer => T) = {
+    val ctx = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+    val root = ctx.getLogger("ROOT")
+    val rootLevel = root.getLevel
+    val playLogger = Play.logger.logger.asInstanceOf[Logger]
+    val playLevel = playLogger.getLevel
+    val appender = new LogBuffer()
+    appender.start()
+    try {
+      root.addAppender(appender)
+      root.setLevel(Level.ALL)
+      playLogger.addAppender(appender)
+      playLogger.setLevel(Level.ALL)
+      block(appender)
+    } finally {
+      root.detachAppender(appender)
+      root.setLevel(rootLevel)
+      playLogger.detachAppender(appender)
+      playLogger.setLevel(playLevel)
+
+    }
+  }
+
+}
+
+class LogBuffer extends AppenderBase[ILoggingEvent] {
+  private val buffer = ListBuffer.empty[ILoggingEvent]
+
+  def append(eventObject: ILoggingEvent) = buffer.synchronized {
+    buffer.append(eventObject)
+  }
+
+  def find(level: Option[Level] = None,
+           logger: Option[String] = None,
+           messageContains: Option[String] = None
+            ): List[ILoggingEvent] = buffer.synchronized {
+    val byLevel = level.fold(buffer) { l => buffer.filter(_.getLevel == l) }
+    val byLogger = logger.fold(byLevel) { l => byLevel.filter(_.getLoggerName == l)}
+    val byMessageContains = logger.fold(byLogger) { m => byLogger.filter(_.getMessage.contains(m))}
+    byMessageContains.toList
+  }
+}
---
+++ framework/src/play-integration-test/src/test/scala/play/it/system/MigrationHelperSpec.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package play.it.system
+
+import play.api.test._
+import play.it.{LogBuffer, LogTester}
+import ch.qos.logback.classic.Level
+import org.specs2.execute.Result
+import play.api.Play
+
+class MigrationHelperSpec extends PlaySpecification {
+
+  sequential
+
+  def logMessage(logs: LogBuffer) = {
+    logs.find(level = Some(Level.WARN), logger = Some("play"), messageContains = Some("session.maxAge")).headOption
+  }
+
+  def assertWasLogged(logs: LogBuffer): Result = {
+    if (logMessage(logs).isDefined) {
+      ok
+    } else {
+      ko("Could not find log message in \n" + logs.find().mkString("\n"))
+    }
+  }
+
+  def assertWasNotLogged(logs: LogBuffer): Result = {
+    logMessage(logs) match {
+      case Some(msg) => ko("Expected no log message but found " + msg)
+      case None => ok
+    }
+  }
+
+  def runTest[T](config: (String, String)*)(block: LogBuffer => T) = {
+    val app = FakeApplication(additionalConfiguration = Map(config:_*))
+    try {
+      LogTester.withLogBuffer { logs =>
+        Play.start(app)
+        block(logs)
+      }
+    } finally {
+      Play.stop()
+    }
+
+  }
+
+  "MigrationHelper" should {
+    "warn when session timeout is too low raw value" in runTest("session.maxAge" -> "3600") { logs =>
+      assertWasLogged(logs)
+    }
+    "not warn when session timeout is not set" in runTest() { logs =>
+      assertWasNotLogged(logs)
+    }
+    "not warn when session timeout has units" in runTest("session.maxAge" -> "3600s") { logs =>
+      assertWasNotLogged(logs)
+    }
+    "not warn when session timeout is significantly large" in runTest("session.maxAge" -> "36000000") { logs =>
+      assertWasNotLogged(logs)
+    }
+    "not warn when migration helper is disabled" in runTest("session.maxAge" -> "3600", "play.migrationhelper" -> "disabled") { logs =>
+      assertWasNotLogged(logs)
+    }
+  }
+
+}
