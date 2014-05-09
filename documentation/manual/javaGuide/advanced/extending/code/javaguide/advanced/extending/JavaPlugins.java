0fb1ee88de905148ab68468f767b6187f67668ca
James Roper <james@jazzy.id.au>
2014-05-09 13:56:22 +1000

    Various documentation improvements
    * Added YourKit link to highlights
    * Added note about Scala 2.11 to highlights
    * Added note about local ivy repo to highlights and migration
    * Rewrote documntation on extending Play

---
+++ documentation/manual/javaGuide/advanced/extending/code/javaguide/advanced/extending/JavaPlugins.java
@@ -0,0 +1,46 @@
+package javaguide.advanced.extending;
+
+import akka.actor.ActorRef;
+import org.junit.Test;
+import play.Play;
+import play.libs.F;
+import play.test.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.*;
+import static play.test.Helpers.*;
+import static akka.pattern.Patterns.ask;
+
+public class JavaPlugins {
+
+    @Test
+    public void pluginsShouldBeAccessible() {
+        final AtomicReference<MyComponent> myComponentRef = new AtomicReference<MyComponent>();
+        FakeApplication app = fakeApplication(new HashMap<String, Object>(), Arrays.asList(MyPlugin.class.getName()));
+        running(app, new Runnable() {
+            public void run() {
+                //#access-plugin
+                MyComponent myComponent = Play.application().plugin(MyPlugin.class).getMyComponent();
+                //#access-plugin
+                assertTrue(myComponent.started);
+                myComponentRef.set(myComponent);
+            }
+        });
+        assertTrue(myComponentRef.get().stopped);
+    }
+
+    @Test
+    public void actorExampleShouldWork() {
+        FakeApplication app = fakeApplication(new HashMap<String, Object>(), Arrays.asList(Actors.class.getName()));
+        running(app, new Runnable() {
+            public void run() {
+                ActorRef actor = Actors.getMyActor();
+                assertEquals("hi", F.Promise.wrap(ask(actor, "hi", 20000)).get(20000));
+            }
+        });
+    }
+
+}
