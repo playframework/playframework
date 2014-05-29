de79be3dfa8702c958887b69a1280797e9f92ff3
cchantep <chantepie@altern.org>
2014-05-29 23:58:05 +0200

    Anorm refactoring

--- framework/src/anorm/src/main/scala/anorm/MayErr.scala
+++ framework/src/anorm/src/main/scala/anorm/MayErr.scala
@@ -3,26 +3,38 @@
  */
 package anorm
 
-case class MayErr[+E, +A](e: Either[E, A]) {
+case class MayErr[+E, +A](toEither: Either[E, A]) {
 
   def flatMap[B, EE >: E](f: A => MayErr[EE, B]): MayErr[EE, B] =
-    MayErr(e.right.flatMap(a => f(a).e))
+    MayErr(toEither.right.flatMap(a => f(a).toEither))
 
-  def map[B](f: A => B): MayErr[E, B] = MayErr(e.right.map(f))
+  def map[B](f: A => B): MayErr[E, B] = MayErr(toEither.right.map(f))
 
+  @deprecated(
+    message = "Use `filter` on `toEither.right`.", since = "2.3.0")
   def filter[EE >: E](p: A => Boolean, error: EE): MayErr[EE, A] =
-    MayErr(e.right.filter(p).getOrElse(Left(error)))
+    MayErr(toEither.right.filter(p).getOrElse(Left(error)))
 
+  @deprecated(since = "2.3.0")
   def toOptionLoggingError(): Option[A] =
-    e.left.map(m => { println(m.toString); m }).right.toOption
+    toEither.left.map(m => { println(m.toString); m }).right.toOption
 
-  def get = e.fold(e =>
-    throw new RuntimeException(e.toString), a => a)
+  /**
+   * Applies `f` if this is a failure or `s` if this is a successful value.
+   *
+   * @param f the function to apply on failure
+   * @param s the function to apply on successful `A` value
+   * @return the results of applying appropriate function
+   */
+  def fold[B](f: E => B, s: A => B): B = toEither.fold(f, s)
+
+  def get = toEither.fold(e =>
+    throw new RuntimeException(toEither.toString), a => a)
+
 }
 
 // TODO Remove (make it more explicit)
 object MayErr {
   import scala.language.implicitConversions
   implicit def eitherToError[E, EE >: E, A, AA >: A](e: Either[E, A]): MayErr[EE, AA] = MayErr[E, A](e)
-  implicit def errorToEither[E, EE >: E, A, AA >: A](e: MayErr[E, A]): Either[EE, AA] = e.e
 }
