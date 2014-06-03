73ba4cb8e9d876845806487331770c90c1ea10c5
James Roper <james@jazzy.id.au>
2014-06-03 14:15:17 +1000

    Add MiMa

--- framework/project/Build.scala
+++ framework/project/Build.scala
@@ -30,7 +30,7 @@ object BuildSettings {
   val buildOrganization = "com.typesafe.play"
   val buildVersion = propOr("play.version", "2.3-SNAPSHOT")
   val buildWithDoc = boolProp("generate.doc")
-  val previousVersion = "2.1.0"
+  val previousVersion = "2.3.0"
   // Libraries that are not Scala libraries or are SBT libraries should not be published if the binary
   // version doesn't match this.
   val publishForScalaBinaryVersion = "2.10"
