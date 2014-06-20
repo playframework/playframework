c4d827ef7f7ea083be7dd29160de3e0037d396f4
James Roper <james@jazzy.id.au>
2014-06-20 21:17:35 +0200

    Removed sample apps and integration tests

--- framework/test/integrationtest/app/controllers/my/JavaAssets.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
- */
-package controllers.my;
-
-public class JavaAssets {
-	public static controllers.AssetsBuilder delegate = new controllers.AssetsBuilder();
-}
