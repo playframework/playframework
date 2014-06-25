29b57bb72daa870f92398f95ba0589d2c81ce55f
Rich Dougherty <rich@rd.gen.nz>
2014-06-26 09:59:40 +1200

    Remove duplicate documentation test
    I accidentally added a duplicate test when cutting and
    pasting from another test in #3701.

--- documentation/manual/detailedTopics/configuration/code/ThreadPoolsJava.java
+++ documentation/manual/detailedTopics/configuration/code/ThreadPoolsJava.java
@@ -1,17 +1,12 @@
 /*
- * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
+ * Copyright (C) 2009-2014 Typesafe Inc. <http://www.typesafe.com>
  */
 package detailedtopics.configuration.threadpools;
 
 import org.junit.Test;
 import play.Play;
 import play.test.FakeApplication;
-import javaguide.testhelpers.MockJavaAction;
-import play.libs.F.Promise;
 
-import play.mvc.*;
-import play.mvc.Http.*;
-import static javaguide.testhelpers.MockJavaAction.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static play.test.Helpers.*;
@@ -33,21 +28,6 @@ public class ThreadPoolsJava {
                 }
             }
         });
-    }
-
-    @Test
-    public void context1() {
-        assertThat(header("Content-Type", call(new Controller1(), fakeRequest())), containsString("text/html"));
-    }
-
-    public static class Controller1 extends MockJavaAction {
-        //#context-content-type
-        public static Result index() {
-            // Access the context's response
-            response().setContentType("text/html");
-            return ok("<h1>Hello World!</h1>");
-        }
-        //#context-content-type
     }
 
 }
