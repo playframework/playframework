6fb19d3bdb165265fc8c18a8898a32c5bd7e12de
cchantep <chantepie@altern.org>
2014-06-27 17:44:35 +0200

    Test for HTTP calls

---
+++ framework/src/play-integration-test/src/test/scala/play/it/mvc/HttpSpec.scala
@@ -0,0 +1,65 @@
+package play.api.mvc
+
+import play.api.http.HeaderNames
+import play.api.test.FakeRequest
+
+object HttpSpec extends org.specs2.mutable.Specification {
+  "HTTP" title
+
+  "Absolute URL" should {
+    val req = FakeRequest().withHeaders(HeaderNames.HOST -> "playframework.com")
+
+    "have HTTP scheme" in {
+      (Call("GET", "/playframework").absoluteURL()(req).
+        aka("absolute URL 1") must_== "http://playframework.com/playframework").
+        and(Call("GET", "/playframework").absoluteURL(false)(req).
+          aka("absolute URL 2") must_== (
+            "http://playframework.com/playframework"))
+    }
+
+    "have HTTPS scheme" in {
+      (Call("GET", "/playframework").absoluteURL()(req.copy(secure = true)).
+        aka("absolute URL 1") must_== (
+          "https://playframework.com/playframework")) and (
+        Call("GET", "/playframework").absoluteURL(true)(req).
+          aka("absolute URL 2") must_== (
+            "https://playframework.com/playframework"))
+    }
+  }
+
+  "Web socket URL" should {
+    val req = FakeRequest().withHeaders(
+      HeaderNames.HOST -> "playframework.com")
+
+    "have WS scheme" in {
+      (Call("GET", "/playframework").webSocketURL()(req).
+        aka("absolute URL 1") must_== "ws://playframework.com/playframework").
+        and(Call("GET", "/playframework").webSocketURL(false)(req).
+          aka("absolute URL 2") must_== (
+            "ws://playframework.com/playframework"))
+    }
+
+    "have WSS scheme" in {
+      (Call("GET", "/playframework").webSocketURL()(req.copy(secure = true)).
+        aka("absolute URL 1") must_== (
+          "wss://playframework.com/playframework")) and (
+        Call("GET", "/playframework").webSocketURL(true)(req).
+          aka("absolute URL 2") must_== (
+            "wss://playframework.com/playframework"))
+    }
+  }
+
+  "RequestHeader" should {
+    "parse quoted and unquoted charset" in {
+      FakeRequest().withHeaders(
+        HeaderNames.CONTENT_TYPE -> """text/xml; charset="utf-8"""").
+        charset aka "request charset" must beSome("utf-8")
+    }
+
+    "parse quoted and unquoted charset" in {
+      FakeRequest().withHeaders(
+        HeaderNames.CONTENT_TYPE -> "text/xml; charset=utf-8").
+        charset aka "request charset" must beSome("utf-8")
+    }
+  }
+}
--- framework/src/play/src/test/scala/play/api/mvc/HttpSpec.scala
+++ framework/src/play/src/test/scala/play/api/mvc/HttpSpec.scala
@@ -5,61 +5,42 @@ package play.api.mvc
 
 import org.specs2.mutable._
 
-class HttpSpec extends Specification {
+object HttpSpec extends Specification {
+  "HTTP" title
 
   val headers = new Headers {
     val data = Seq(("a", Seq("a1", "a2")), ("b", Seq("b1", "b2")))
   }
 
   "Headers" should {
-
     "return the header value associated with a" in {
-
-      headers.get("a") must be_==(Some("a1"))
+      headers.get("a") must beSome("a1")
     }
 
     "return the header values associated with b" in {
-
       headers.getAll("b") must be_==(Seq("b1", "b2"))
     }
 
     "not return an empty sequence of values associated with an unknown key" in {
-
-      headers.getAll("z") must be_==(Seq.empty)
+      headers.getAll("z") must beEmpty
     }
 
     "should return all keys" in {
-
       headers.keys must be_==(Set("a", "b"))
     }
 
     "should return a simple map" in {
-
       headers.toSimpleMap must be_==(Map("a" -> "a1", "b" -> "b1"))
     }
 
     "return the value from a map by case insensitive" in {
-      headers.toMap.get("A") must be_==(Some(Seq("a1", "a2")))
-      headers.toMap.get("b") must be_==(Some(Seq("b1", "b2")))
+      (headers.toMap.get("A") must_== Some(Seq("a1", "a2"))) and 
+        (headers.toMap.get("b") must_== Some(Seq("b1", "b2")))
     }
 
     "return the value from a simple map by case insensitive" in {
-      headers.toSimpleMap.get("A") must be_==(Some("a1"))
-      headers.toSimpleMap.get("b") must be_==(Some("b1"))
-    }
-  }
-
-  "RequestHeader" should {
-    "parse quoted and unquoted charset" in {
-      case class TestRequestHeader(headers: Headers, method: String = "GET", uri: String = "/", path: String = "", remoteAddress: String = "127.0.0.1", version: String = "HTTP/1.1", id: Long = 666, tags: Map[String, String] = Map.empty[String, String], queryString: Map[String, Seq[String]] = Map(), secure: Boolean = false) extends RequestHeader
-
-      TestRequestHeader(headers = new Headers {
-        val data = Seq(play.api.http.HeaderNames.CONTENT_TYPE -> Seq("""text/xml; charset="utf-8""""))
-      }).charset must beSome("utf-8")
-
-      TestRequestHeader(headers = new Headers {
-        val data = Seq(play.api.http.HeaderNames.CONTENT_TYPE -> Seq("text/xml; charset=utf-8"))
-      }).charset must beSome("utf-8")
+      (headers.toSimpleMap.get("A") must beSome("a1")) and
+       (headers.toSimpleMap.get("b") must beSome("b1"))
     }
   }
 
