86760740b7f2c591c68aaf253a640994962c4484
Yann Simon <yann.simon.fr@gmail.com>
2014-07-10 17:52:59 +0200

    fix compilation with OpenJDK
    The links cannot be resolved as the BuildLink project does
    not have the dependencies to satisfy them.

--- framework/src/build-link/src/main/java/play/core/BuildDocHandler.java
+++ framework/src/build-link/src/main/java/play/core/BuildDocHandler.java
@@ -19,8 +19,8 @@ public interface BuildDocHandler {
   /**
    * Given a request, either handle it and return some result, or don't, and return none.
    *
-   * @param request A request of type {@link play.api.mvc.RequestHeader}.
-   * @return A value of type {@link Option<play.api.mvc.SimpleResult>}, Some if the result was
+   * @param request A request of type {@code play.api.mvc.RequestHeader}.
+   * @return A value of type {@code Option<play.api.mvc.SimpleResult>}, Some if the result was
    *        handled, None otherwise.
    */
   public Object maybeHandleDocRequest(Object request);
