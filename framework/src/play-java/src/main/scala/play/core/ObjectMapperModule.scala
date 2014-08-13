44f02fc7f33eb3a20ac198cf7c72a955f2ec7a7f
James Roper <james@jazzy.id.au>
2014-08-13 17:13:50 +1000

    Ported ObjectMapperPlugin to be a module

---
+++ framework/src/play-java/src/main/scala/play/core/ObjectMapperModule.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
+ */
+package play.core
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import play.api._
+import play.api.inject._
+import play.libs.Json
+import play.api.libs.json.JacksonJson
+
+import javax.inject._
+
+import scala.concurrent.Future
+
+/**
+ * Module that injects an object mapper to the JSON library on start and on stop.
+ *
+ * This solves the issue of the ObjectMapper cache from holding references to the application class loader between
+ * reloads.
+ */
+class ObjectMapperModule extends Module {
+
+  def bindings(environment: Environment, configuration: Configuration) = Seq(
+    bind[ObjectMapper].toProvider[ObjectMapperProvider].eagerly()
+  )
+}
+
+@Singleton
+class ObjectMapperProvider @Inject() (lifecycle: ApplicationLifecycle) extends Provider[ObjectMapper] {
+  lazy val get = {
+    val objectMapper = JacksonJson.createMapper()
+    Json.setObjectMapper(objectMapper)
+    lifecycle.addStopHook { () =>
+      Future.successful(Json.setObjectMapper(null))
+    }
+    objectMapper
+  }
+}
