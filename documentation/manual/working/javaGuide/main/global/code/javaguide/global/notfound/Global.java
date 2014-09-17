077c4b191f0f33192eefc99c7b9e3de877c4ea48
Peter Vlugter <pvlugter@gmail.com>
2014-09-17 13:26:09 +1200

    Update doc test source directories
    The documentation tests have missed most sources
    since being moved to the `working` directory.
    
    Also update the few broken tests since this change.

--- documentation/manual/working/javaGuide/main/global/code/javaguide/global/notfound/Global.java
+++ documentation/manual/working/javaGuide/main/global/code/javaguide/global/notfound/Global.java
@@ -11,6 +11,8 @@ import play.mvc.*;
 import play.mvc.Http.*;
 import play.libs.F.*;
 
+import static play.mvc.Results.notFound;
+
 public class Global extends GlobalSettings {
 
     public Promise<Result> onHandlerNotFound(RequestHeader request) {
