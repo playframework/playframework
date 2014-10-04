604d651c4eb5ba5aa7f09e0c95383e0548370c19
Jim Powers <jim@corruptmemory.com>
2014-10-03 21:52:25 -0400

    Added support for additional settings in shared projects
    * Added publish settings

--- framework/project/Build.scala
+++ framework/project/Build.scala
@@ -117,10 +117,11 @@ object BuildSettings {
       )
   }
 
-  def PlaySharedRuntimeProject(name: String, dir: String, targetPrefix:String, scalaVersion: SharedProjectScalaVersion): Project = {
+  def PlaySharedRuntimeProject(name: String, dir: String, targetPrefix:String, scalaVersion: SharedProjectScalaVersion, additionalSettings:Seq[Setting[_]]): Project = {
     Project(name, file("src/" + dir))
       .settings(defaultScalariformSettings: _*)
       .settings(scalaVersion.toSettings(targetPrefix): _*)
+      .settings(additionalSettings: _*)
   }
 
   /**
@@ -213,14 +214,14 @@ object PlayBuild extends Build {
     .settings(libraryDependencies ++= link)
     .dependsOn(PlayExceptionsProject)
 
-  def runSupportProject(prefix:String,sv:SharedProjectScalaVersion) =
-    PlaySharedRuntimeProject(s"$prefix-${sv.nameSuffix}", s"run-support", prefix, sv).settings(
+  def runSupportProject(prefix:String,sv:SharedProjectScalaVersion,additionalSettings:Seq[Setting[_]]) =
+    PlaySharedRuntimeProject(s"$prefix-${sv.nameSuffix}", s"run-support", prefix, sv, additionalSettings).settings(
       libraryDependencies ++= runSupportDependencies
     )
 
-  lazy val SbtRunSupportProject = runSupportProject("SBT-Run-Support",SharedProjectScalaVersion.forScalaVersion(buildScalaVersionForSbt))
+  lazy val SbtRunSupportProject = runSupportProject("SBT-Run-Support",SharedProjectScalaVersion.forScalaVersion(buildScalaVersionForSbt),PublishSettings.sbtPluginPublishSettings)
 
-  lazy val RunSupportProject = runSupportProject("Run-Support", SharedProjectScalaVersion.forScalaVersion(buildScalaVersion))
+  lazy val RunSupportProject = runSupportProject("Run-Support", SharedProjectScalaVersion.forScalaVersion(buildScalaVersion),PublishSettings.publishSettings)
 
   lazy val RoutesCompilerProject = PlaySbtProject("Routes-Compiler", "routes-compiler")
     .settings(libraryDependencies ++= routersCompilerDependencies)
