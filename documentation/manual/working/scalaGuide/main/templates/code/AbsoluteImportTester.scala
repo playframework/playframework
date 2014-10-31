b28a9d8e99dd7a06d381e860a2eb887ee2833e1d
Peter Vlugter <pvlugter@gmail.com>
2014-10-31 15:36:58 +1300

    Remove twirl documentation

--- documentation/manual/working/scalaGuide/main/templates/code/AbsoluteImportTester.scala
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
- */
-package company.product.core
-
-object AbsoluteImportTester {
-  def test = "absolute import is working"
-}
