60c3ca8a55bf33ad733661166bd8e6f2595e4d4f
Ben McCann <benjamin.j.mccann@gmail.com>
2014-11-25 18:36:09 -0800

    Replace tabs/spaces with only spaces

--- framework/src/build-link/src/main/java/play/core/server/ServerWithStop.java
+++ framework/src/build-link/src/main/java/play/core/server/ServerWithStop.java
@@ -8,16 +8,16 @@ package play.core.server;
  */
 public interface ServerWithStop {
 
-    /**
-     * Stop the server.
-     */
-	public void stop();
+  /**
+   * Stop the server.
+   */
+  public void stop();
 
-    /**
-     * Get the address of the server.
-     *
-     * @return The address of the server.
-     */
-	public java.net.InetSocketAddress mainAddress(); 
+  /**
+   * Get the address of the server.
+   *
+   * @return The address of the server.
+   */
+  public java.net.InetSocketAddress mainAddress(); 
 
 }
