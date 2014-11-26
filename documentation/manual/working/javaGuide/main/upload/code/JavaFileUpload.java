449ddf9e131b8365272613398e4027f94dc58c53
Martin Pallmann <martin.pallmann_external@immobilienscout24.de>
2014-11-26 11:51:09 +0100

    fix #2562 extract code samples

---
+++ documentation/manual/working/javaGuide/main/upload/code/JavaFileUpload.java
@@ -0,0 +1,31 @@
+import play.mvc.Controller;
+
+public class JavaFileUpload {
+
+    static class SyncUpload extends Controller {
+        //#syncUpload
+        public static play.mvc.Result upload() {
+            play.mvc.Http.MultipartFormData body = request().body().asMultipartFormData();
+            play.mvc.Http.MultipartFormData.FilePart picture = body.getFile("picture");
+            if (picture != null) {
+                String fileName = picture.getFilename();
+                String contentType = picture.getContentType();
+                java.io.File file = picture.getFile();
+                return ok("File uploaded");
+            } else {
+                flash("error", "Missing file");
+                return badRequest();
+            }
+        }
+        //#syncUpload
+    }
+
+    static class AsyncUpload extends Controller {
+        //#asyncUpload
+        public static play.mvc.Result upload() {
+            java.io.File file = request().body().asRaw().asFile();
+            return ok("File uploaded");
+        }
+        //#asyncUpload
+    }
+}
