49158ee2d47183b831158e03744c236411e7c57a
Rich Dougherty <rich@rd.gen.nz>
2014-12-16 14:05:55 +1300

    Use normal string literal to fix Sublime's syntax highlighting
    Sublime struggles when it sees four quotes in a row.

--- framework/src/play/src/main/scala/play/api/mvc/Results.scala
+++ framework/src/play/src/main/scala/play/api/mvc/Results.scala
@@ -374,7 +374,7 @@ trait Results {
         ResponseHeader(status, Map(
           CONTENT_LENGTH -> content.length.toString,
           CONTENT_TYPE -> play.api.libs.MimeTypes.forFileName(name).getOrElse(play.api.http.ContentTypes.BINARY)
-        ) ++ (if (inline) Map.empty else Map(CONTENT_DISPOSITION -> s"""attachment; filename="$name""""))),
+        ) ++ (if (inline) Map.empty else Map(CONTENT_DISPOSITION -> ("attachment; filename=\"" + name + "\"")))),
         Enumerator.fromFile(content) &> Enumeratee.onIterateeDone(onClose)(defaultContext)
       )
     }
