0aa071eba23c81eb7911391e39aefe5b55418679
Rich Dougherty <rich@rd.gen.nz>
2015-04-01 09:47:31 +1300

    Make header comparisons faster by comparing lengths
    By comparing lengths in many cases we can avoid the need to look
    at the contents of the headers. This makes header operations a lot
    faster.

--- framework/src/play/src/main/scala/play/utils/CaseInsensitiveOrdered.scala
+++ framework/src/play/src/main/scala/play/utils/CaseInsensitiveOrdered.scala
@@ -4,10 +4,16 @@
 package play.core.utils
 
 /**
- * Case Insensitive Ordering
+ * Case Insensitive Ordering. We first compare by length, then
+ * use a case insensitive lexicographic order. This allows us to
+ * use a much faster length comparison before we even start looking
+ * at the content of the strings.
  */
-
 object CaseInsensitiveOrdered extends Ordering[String] {
-  def compare(x: String, y: String): Int = x.compareToIgnoreCase(y)
+  def compare(x: String, y: String): Int = {
+    val xl = x.length
+    val yl = y.length
+    if (xl < yl) -1 else if (xl > yl) 1 else x.compareToIgnoreCase(y)
+  }
 }
 
