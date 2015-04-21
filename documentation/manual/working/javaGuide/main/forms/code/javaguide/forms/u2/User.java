a67ba1fc24ef968875693c66a9219e42db7213a6
Ben McCann <ben@connectifier.com>
2015-04-20 18:23:24 -0700

    Don't make PlayEnhancer required

--- documentation/manual/working/javaGuide/main/forms/code/javaguide/forms/u2/User.java
+++ documentation/manual/working/javaGuide/main/forms/code/javaguide/forms/u2/User.java
@@ -9,7 +9,24 @@ import play.data.validation.Constraints.Required;
 public class User {
 
     @Required
-    public String email;
-    public String password;
+    protected String email;
+    protected String password;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+    	this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
 //#user
