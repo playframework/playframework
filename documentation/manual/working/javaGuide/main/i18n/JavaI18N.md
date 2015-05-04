6facbedfbdff667cb0f8174bff53e0446a54e758
Rich Dougherty <rich@rd.gen.nz>
2015-05-04 13:29:47 +1200

    Allow lang to be set transiently
    Currently in the Java API it's impossible to set the current
    Context's language without also setting a cookie. That means
    that it's impossible to just change the language for a single
    request. This change adds new necessary methods to support
    changing and clearing the language without changing the
    cookie.
    
    Other changes:
    * Added unit tests for changing the language.
    * Added documentation tests for Java internationalization.

--- documentation/manual/working/javaGuide/main/i18n/JavaI18N.md
+++ documentation/manual/working/javaGuide/main/i18n/JavaI18N.md
@@ -17,25 +17,49 @@ You can externalize messages in the `conf/messages.xxx` files.
 
 The default `conf/messages` file matches all languages. You can specify additional language messages files, such as `conf/messages.fr` or `conf/messages.en-US`.
 
-You can retrieve messages for the current language using the `play.i18n.Messages` object:
+You can retrieve messages for the _current language_ using the `play.i18n.Messages` object:
 
 ```
 String title = Messages.get("home.title")
 ```
 
-You can also specify the language explicitly:
+The _current language_ is found by looking at the `lang` field in the current [`Context`](api/java/play/mvc/Http.Context.html). If there's no current `Context` then the default language is used. The `Context`'s `lang` value is determined by:
+
+1. Seeing if the `Context`'s `lang` field has been set explicitly.
+2. Looking for a `PLAY_LANG` cookie in the request.
+3. Looking at the `Accept-Language` headers of the request.
+4. Using the application's default language.
+
+You can change the `Context`'s `lang` field by calling `changeLang` or `setTransientLang`. The `changeLang` method will change the field and also set a `PLAY_LANG` cookie for future requests. The `setTransientLang` will set the field for the current request, but doesn't set a cookie. See [below](#Use-in-templates) for example usage.
+
+If you don't want to use the current language you can specify a message's language explicitly:
 
 ```
 String title = Messages.get(new Lang(Lang.forCode("fr")), "home.title")
 ```
 
-> **Note:** If you have a `Request` in the scope, it will use the preferred language extracted from the `Accept-Language` header and matching one of the application’s supported languages.
-
 ## Use in templates
-```
-@import play.i18n._
-@Messages.get("key")
-```
+
+You can use the `Messages.get` method from within a template. This will localize a message with the current language.
+
+@[template](code/javaguide/i18n/hellotemplate.scala.html)
+
+You can also use the Scala `Messages` object from within templates. The Scala `Messages` object has a shorter form that's equivalent to `Messages.get` which many people find useful. If you use the Scala `Messages` object remember not to import the Java `play.i18n.Messages` class or they will conflict!
+
+@[template](code/javaguide/i18n/helloscalatemplate.scala.html)
+
+Localized templates that use `Messages.get` or the Scala `Messages` object are invoked like normal:
+
+@[default-lang-render](code/javaguide/i18n/JavaI18N.java)
+
+If you want to change the language for the template you can call `changeLang` on the current [`Context`](api/java/play/mvc/Http.Context.html). This will change the language for the current request, and set the language into a cookie so that the language is changed for future requests:
+
+@[change-lang-render](code/javaguide/i18n/JavaI18N.java)
+
+If you just want to change the language, but only for the current request and not for future requests, call `setTransientLang`:
+
+@[set-transient-lang-render](code/javaguide/i18n/JavaI18N.java)
+
 ## Formatting messages
 
 Messages are formatted using the `java.text.MessageFormat` library. For example, if you have defined a message like this:
---
+++ framework/src/play-java/src/test/java/play/mvc/HttpTest.java
@@ -0,0 +1,147 @@
+package play.mvc;
+
+import com.typesafe.config.ConfigFactory;
+import play.Application;
+import play.Configuration;
+import play.Environment;
+import play.Play;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.mvc.Http.*;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.Play.langCookieName;
+
+/**
+ * Tests for the Http class. This test is in the play-java project
+ * because we want to use some of the play-java classes, e.g.
+ * the GuiceApplicationBuilder.
+ */
+public class HttpTest {
+
+    /** Gets the PLAY_LANG cookie, or the last one if there is more than one */
+    private String responseLangCookie(Context ctx) {
+        String value = null;
+        for (Cookie c : ctx.response().cookies()) {
+          if (c.name().equals(Play.langCookieName())) {
+            value = c.value();
+          }
+        }
+        return value;
+    }
+
+    private static Configuration addLangs(Environment environment) {
+      Configuration langOverrides = new Configuration(ConfigFactory.parseString("play.i18n.langs = [\"en\", \"en-US\", \"fr\" ]"));
+      Configuration loaded = Configuration.load(environment);
+      return langOverrides.withFallback(loaded);
+    }
+
+    private static void withApplication(Runnable r) {
+        Application app = new GuiceApplicationBuilder()
+          .loadConfig(HttpTest::addLangs)
+          .build();
+        play.api.Play.start(app.getWrappedApplication());
+        try {
+            r.run();
+        } finally {
+            play.api.Play.stop(app.getWrappedApplication());
+        }
+    }
+
+    @Test
+    public void testChangeLang() {
+        withApplication(() -> {
+            Context ctx = new Context(new RequestBuilder());
+            // Start off as 'en' with no cookie set
+            assertThat(ctx.lang().code()).isEqualTo("en");
+            assertThat(responseLangCookie(ctx)).isNull();
+            // Change the language to 'en-US'
+            assertThat(ctx.changeLang("en-US")).isTrue();
+            // The language and cookie should now be 'en-US'
+            assertThat(ctx.lang().code()).isEqualTo("en-US");
+            assertThat(responseLangCookie(ctx)).isEqualTo("en-US");
+        });
+    }
+
+    @Test
+    public void testChangeLangFailure() {
+        withApplication(() -> {
+            Context ctx = new Context(new RequestBuilder());
+            // Start off as 'en' with no cookie set
+            assertThat(ctx.lang().code()).isEqualTo("en");
+            assertThat(responseLangCookie(ctx)).isNull();
+            // Try to change the language to 'en-NZ' - which will fail and return false
+            assertThat(ctx.changeLang("en-NZ")).isFalse();
+            // The language should still be 'en' and cookie should still be empty
+            assertThat(ctx.lang().code()).isEqualTo("en");
+            assertThat(responseLangCookie(ctx)).isNull();
+        });
+    }
+
+    @Test
+    public void testClearLang() {
+        withApplication(() -> {
+            Context ctx = new Context(new RequestBuilder());
+            // Set 'fr' as our initial language
+            assertThat(ctx.changeLang("fr")).isTrue();
+            assertThat(ctx.lang().code()).isEqualTo("fr");
+            assertThat(responseLangCookie(ctx)).isEqualTo("fr");
+            // Clear language
+            ctx.clearLang();
+            // The language should now be 'en' and the cookie should be null
+            assertThat(ctx.lang().code()).isEqualTo("en");
+            assertThat(responseLangCookie(ctx)).isEqualTo("");
+        });
+    }
+
+    @Test
+    public void testSetTransientLang() {
+        withApplication(() -> {
+            Context ctx = new Context(new RequestBuilder());
+            // Start off as 'en' with no cookie set
+            assertThat(ctx.lang().code()).isEqualTo("en");
+            assertThat(responseLangCookie(ctx)).isNull();
+            // Change the language to 'en-US'
+            ctx.setTransientLang("en-US");
+            // The language should now be 'en-US', but the cookie mustn't be set
+            assertThat(ctx.lang().code()).isEqualTo("en-US");
+            assertThat(responseLangCookie(ctx)).isNull();
+        });
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testSetTransientLangFailure() {
+        withApplication(() -> {
+            Context ctx = new Context(new RequestBuilder());
+            // Start off as 'en' with no cookie set
+            assertThat(ctx.lang().code()).isEqualTo("en");
+            assertThat(responseLangCookie(ctx)).isNull();
+            // Try to change the language to 'en-NZ' - which will throw an exception
+            ctx.setTransientLang("en-NZ");
+        });
+    }
+
+    @Test
+    public void testClearTransientLang() {
+        withApplication(() -> {
+            Cookie frCookie = new Cookie("PLAY_LANG", "fr", null, "/", null, false, false);
+            RequestBuilder rb = new RequestBuilder().cookie(frCookie);
+            Context ctx = new Context(rb);
+            // Start off as 'en' with no cookie set
+            assertThat(ctx.lang().code()).isEqualTo("fr");
+            assertThat(responseLangCookie(ctx)).isNull();
+            // Change the language to 'en-US'
+            ctx.setTransientLang("en-US");
+            // The language should now be 'en-US', but the cookie mustn't be set
+            assertThat(ctx.lang().code()).isEqualTo("en-US");
+            assertThat(responseLangCookie(ctx)).isNull();
+            // Clear the language to the default for the current request
+            ctx.clearTransientLang();
+            // The language should now be back to 'fr', and the cookie still mustn't be set
+            assertThat(ctx.lang().code()).isEqualTo("fr");
+            assertThat(responseLangCookie(ctx)).isNull();
+        });
+    }
+
+
+}
