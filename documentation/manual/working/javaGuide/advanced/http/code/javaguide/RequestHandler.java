e6fff104075f1c280064ee6c70bb6f7be5ba9f77
Mirco Dotta <mirco.dotta@gmail.com>
2015-05-15 18:35:01 +0200

    Created documentation for Java http request handler and filters
    (cherry picked from commit 1bbbbb63c760e1064284f95a465b6ab196447894)

---
+++ documentation/manual/working/javaGuide/advanced/http/code/javaguide/RequestHandler.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+
+//#default
+import play.http.HttpRequestHandler;
+import play.libs.F;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.lang.reflect.Method;
+
+public class RequestHandler implements HttpRequestHandler {
+
+    @Override
+    public Action createAction(Http.Request request, Method actionMethod) {
+        return new Action.Simple() {
+            @Override
+            public F.Promise<Result> call(Http.Context ctx) throws Throwable {
+                return delegate.call(ctx);
+            }
+        };
+    }
+
+    @Override
+    public Action wrapAction(Action action) {
+        return action;
+    }
+}
+//#default
