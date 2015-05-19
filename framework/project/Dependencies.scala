4d8c7ea6d88d35cf3b07bc8b85ff0a396a23a95a
James Roper <james@jazzy.id.au>
2015-05-19 13:16:11 +1000

    Upgrade sbt-js-engine
    This introduces the ability to auto detect node.js.

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -218,7 +218,7 @@ object Dependencies {
       sbtDep("com.typesafe.sbt" % "sbt-native-packager" % BuildInfo.sbtNativePackagerVersion),
 
       sbtDep("com.typesafe.sbt" % "sbt-web" % "1.2.0"),
-      sbtDep("com.typesafe.sbt" % "sbt-js-engine" % "1.1.1")
+      sbtDep("com.typesafe.sbt" % "sbt-js-engine" % "1.1.2")
     ) ++ javassist ++ specsBuild.map(_ % Test)
   }
 
