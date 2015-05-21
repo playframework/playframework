e6cec6bc003fb3612f4337cf943da96a3f533c5a
godenji <admin@ushr.com>
2015-05-21 19:39:59 +0200

    fixes missing Char binding for PathBindable and QueryStringBindable; for context see: https://github.com/playframework/playframework/issues/4523

--- framework/src/play/src/test/scala/play/api/mvc/BindersSpec.scala
+++ framework/src/play/src/test/scala/play/api/mvc/BindersSpec.scala
@@ -93,4 +93,42 @@ object BindersSpec extends Specification {
     }
   }
 
+  "URL QueryStringBindable Char" should {
+    val subject = implicitly[QueryStringBindable[Char]]
+    val char = 'X'
+    val string = "X"
+
+    "Unbind query string char as string" in {
+      subject.unbind("key", char) must equalTo("key=" + char.toString)
+    }
+    "Bind query string as char" in {
+      subject.bind("key", Map("key" -> Seq(string))) must equalTo(Some(Right(char)))
+    }
+    "Fail on length > 1" in {
+      subject.bind("key", Map("key" -> Seq("foo"))) must be_==(Some(Left("Cannot parse parameter key with value 'foo' as Char: key must be exactly one digit in length.")))
+    }
+    "Fail on empty" in {
+      subject.bind("key", Map("key" -> Seq(""))) must be_==(Some(Left("Cannot parse parameter key with value '' as Char: key must be exactly one digit in length.")))
+    }
+  }
+
+  "URL PathBindable Char" should {
+    val subject = implicitly[PathBindable[Char]]
+    val char = 'X'
+    val string = "X"
+
+    "Unbind Path char as string" in {
+      subject.unbind("key", char) must equalTo(char.toString)
+    }
+    "Bind Path string as char" in {
+      subject.bind("key", string) must equalTo(Right(char))
+    }
+    "Fail on length > 1" in {
+      subject.bind("key", "foo") must be_==(Left("Cannot parse parameter key with value 'foo' as Char: key must be exactly one digit in length."))
+    }
+    "Fail on empty" in {
+      subject.bind("key", "") must be_==(Left("Cannot parse parameter key with value '' as Char: key must be exactly one digit in length."))
+    }
+  }
+
 }
