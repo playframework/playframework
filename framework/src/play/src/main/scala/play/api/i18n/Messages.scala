9e1a02f39515590ccf00092a65750448c6170df0
Mihail Burduja <warchildmd@gmail.com>
2015-05-27 20:20:34 +0300

    Added messageSourceName to MessageParser initialization

--- framework/src/play/src/main/scala/play/api/i18n/Messages.scala
+++ framework/src/play/src/main/scala/play/api/i18n/Messages.scala
@@ -237,7 +237,7 @@ object Messages {
    * Parse all messages of a given input.
    */
   def parse(messageSource: MessageSource, messageSourceName: String): Either[PlayException.ExceptionSource, Map[String, String]] = {
-    new Messages.MessagesParser(messageSource, "").parse.right.map { messages =>
+    new Messages.MessagesParser(messageSource, messageSourceName).parse.right.map { messages =>
       messages.map { message => message.key -> message.pattern }.toMap
     }
   }
