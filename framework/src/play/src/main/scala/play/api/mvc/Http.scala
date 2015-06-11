0b0d42c09e0145b628400f7b051a375120c449c9
Matthias Kurz <m.kurz@irregular.at>
2015-06-11 11:30:05 +0200

    Improvements to Call + support for fragments in form actions

--- framework/src/play/src/main/scala/play/api/mvc/Http.scala
+++ framework/src/play/src/main/scala/play/api/mvc/Http.scala
@@ -321,6 +321,10 @@ package play.api.mvc {
    */
   case class Call(method: String, url: String, fragment: String = null) extends play.mvc.Call {
 
+    override def unique(): Call = copy(url = uniquify(url))
+
+    override def withFragment(fragment: String): Call = copy(fragment = fragment)
+
     /**
      * Transform this call to an absolute URL.
      *
@@ -359,8 +363,6 @@ package play.api.mvc {
      * Transform this call to an WebSocket URL.
      */
     def webSocketURL(secure: Boolean)(implicit request: RequestHeader): String = "ws" + (if (secure) "s" else "") + "://" + request.host + this.url
-
-    override def toString = this.url + this.appendFragment
 
   }
 
