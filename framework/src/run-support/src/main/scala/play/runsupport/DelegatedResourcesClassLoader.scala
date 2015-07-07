d24cbe358d5f0dd9b3a99afd2f425ae79cf35c56
Roland Bär <planetenkiller@users.noreply.github.com>
2015-07-07 22:10:57 +0200

    fix dev-mode memory leaks (#4649)

---
+++ framework/src/run-support/src/main/scala/play/runsupport/DelegatedResourcesClassLoader.scala
@@ -0,0 +1,11 @@
+package play.runsupport
+
+import java.net.URL
+
+/**
+ * A ClassLoader that only uses resources from its parent
+ */
+class DelegatedResourcesClassLoader(name: String, urls: Array[URL], parent: ClassLoader) extends NamedURLClassLoader(name, urls, parent) {
+  require(parent ne null)
+  override def getResources(name: String): java.util.Enumeration[java.net.URL] = getParent.getResources(name)
+}
