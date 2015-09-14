36d0192a52bc53842d07dd39ddf7f2e1533818fe
Mirco Dotta <mirco.dotta@gmail.com>
2015-09-14 17:09:56 +0200

    Removed last reference to Scala 2.10
    This was missed in #5091, as pointed out by @cchantep

--- framework/src/sbt-plugin/src/sbt-test/play-sbt-plugin/play-position-mapper/project/Build.scala
+++ framework/src/sbt-plugin/src/sbt-test/play-sbt-plugin/play-position-mapper/project/Build.scala
@@ -55,7 +55,7 @@ object ApplicationBuild extends Build {
         bufferLogger +: currentFunction(key)
       }
     },
-    scalaVersion := sys.props.get("scala.version").getOrElse("2.10.7"),
+    scalaVersion := sys.props.get("scala.version").getOrElse("2.11.7"),
     checkLogContainsTask,
     compileIgnoreErrorsTask
   )
