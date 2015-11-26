2ab83b2375f836992de2641cbe587b59737e5da6
Rich Dougherty <rich@rd.gen.nz>
2015-11-26 15:26:36 +1300

    More fixes to forward header parsing
    Fixes #5299.
    
    Parsing for Forward and X-Forwarded-* headers no longer throws
    exceptions if an invalid header is found. Now a debug log
    message is thrown instead. This reverses the stricter errors
    introduced in #5266.
    
    Additional changes were made:
    - Don't bother parsing the supplied IP address headers if we
      don't trust the connection that supplied it.
    - Support values in Forward headers surrounded with double
      quotes. This is needed to support IPv6.
    - Support IPv6 values in X-Forwarded-For headers even when they
      don't have surrounding square brackets. This format is
      commonly supplied by proxies.

--- framework/src/play-server/src/test/scala/play/core/server/common/NodeIdentifierParserSpec.scala
+++ framework/src/play-server/src/test/scala/play/core/server/common/NodeIdentifierParserSpec.scala
@@ -2,33 +2,49 @@ package play.core.server.common
 
 import java.net.InetAddress.getByName
 import org.specs2.mutable.Specification
-import play.core.server.common.NodeIdentifierParser._
+
+import ForwardedHeaderHandler.{ ForwardedHeaderVersion, Rfc7239, Xforwarded }
+import NodeIdentifierParser._
 
 class NodeIdentifierParserSpec extends Specification {
+
+  def parseNode(version: ForwardedHeaderVersion, str: String) = {
+    val parser = new NodeIdentifierParser(version)
+    parser.parseNode(str)
+  }
+
   "NodeIdentifierParser" should {
 
     "parse an ip v6 address with port" in {
-      parseNode("[8F:F3B::FF]:9000").right.get mustEqual Ip(getByName("8F:F3B::FF")) -> Some(PortNumber(9000))
+      parseNode(Rfc7239, "[8F:F3B::FF]:9000") must beRight(Ip(getByName("8F:F3B::FF")) -> Some(PortNumber(9000)))
+    }
+
+    "not parse unescaped ip v6 address in rfc7239 header" in {
+      parseNode(Rfc7239, "8F:F3B::FF") must beLeft
+    }
+
+    "parse unescaped ip v6 address in x-forwarded-for header" in {
+      parseNode(Xforwarded, "8F:F3B::FF") must beRight(Ip(getByName("8F:F3B::FF")) -> None)
     }
 
     "parse an ip v6 address with obfuscated port" in {
-      parseNode("[::FF]:_obf").right.get mustEqual Ip(getByName("::FF")) -> Some(ObfuscatedPort("_obf"))
+      parseNode(Rfc7239, "[::FF]:_obf") must beRight(Ip(getByName("::FF")) -> Some(ObfuscatedPort("_obf")))
     }
 
     "parse an ip v4 address with port" in {
-      parseNode("127.0.0.1:8080").right.get mustEqual Ip(getByName("127.0.0.1")) -> Some(PortNumber(8080))
+      parseNode(Rfc7239, "127.0.0.1:8080") must beRight(Ip(getByName("127.0.0.1")) -> Some(PortNumber(8080)))
     }
 
     "parse an ip v4 address without port" in {
-      parseNode("192.168.0.1").right.get mustEqual Ip(getByName("192.168.0.1")) -> None
+      parseNode(Rfc7239, "192.168.0.1") must beRight(Ip(getByName("192.168.0.1")) -> None)
     }
 
     "parse an unknown ip address without port" in {
-      parseNode("unknown").right.get mustEqual UnknownIp -> None
+      parseNode(Rfc7239, "unknown") must beRight(UnknownIp -> None)
     }
 
     "parse an obfuscated ip address without port" in {
-      parseNode("_harry").right.get mustEqual ObfuscatedIp("_harry") -> None
+      parseNode(Rfc7239, "_harry") must beRight(ObfuscatedIp("_harry") -> None)
     }
   }
 }
