bc6daa00244041260cd751bc2cbb9edf539324cf
Rich Dougherty <rich@rd.gen.nz>
2015-11-26 15:26:36 +1300

    More fixes to forward header parsing
    Fixes #5299.
    
    Parsing for Forward and X-Forwarded-* headers no longer throws
    exceptions if an invalid header is found. Now a debug log
    message is thrown instead. This reverses the stricter errors
    introduced in #5266.
    
    Additional changes were made:
    - Don't bother parsing the supplied IP address headers if we
      don't trust the connection that supplied it.
    - Support values in Forward headers surrounded with double
      quotes. This is needed to support IPv6.
    - Support IPv6 values in X-Forwarded-For headers even when they
      don't have surrounding square brackets. This format is
      commonly supplied by proxies.

--- framework/src/play-server/src/test/scala/play/core/server/common/SubnetSpec.scala
+++ framework/src/play-server/src/test/scala/play/core/server/common/SubnetSpec.scala
@@ -16,7 +16,8 @@ class SubnetSpec extends Specification with DataTables {
         "fe80::/64" !! "fe80::54ff:fffe:32fe" ! true |
         "2001:db8::/32" !! "2001:db9::1" ! false |
         "2001:dbfe::/31" !! "2001:dbff::" ! true |
-        "2001:dbfe::/31" !! "2001:dbff::" ! true |>
+        "2001:dbfe::/31" !! "2001:dbff::" ! true |
+        "2001:db8:cafe::17" !! "2001:db8:cafe::17" ! true |>
         {
           (a, b, c) => Subnet(a).isInRange(InetAddress.getByName(b)) mustEqual c
         }
