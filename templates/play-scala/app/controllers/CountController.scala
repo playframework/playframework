de9348adc54e3f81a54caa39fbd2ec0849d47db4
Rich Dougherty <rich@rd.gen.nz>
2016-02-01 16:22:47 +1300

    Update Scala template with more example code
    The idea is to address common questions by providing more
    example code. This should help users get started. The main
    improvement is to showcase how to use dependency injection to
    join components together. There is also some example code for
    filters and for using futures.
    
    A risk of this update is that the starter application is now
    more complicated and some people might accidentally leave demo
    code in their application. The only time where this will have
    an effect is when the filters are accidentally left on. To
    reduce the effect of this I have written the filters code so
    that the demo filter is only enabled in dev mode.
    
    Updates include:
    * better controller naming
    * using asynchronous actions
    * injecting components into controllers
    * using a Guice module
    * starting and stopping a service
    * using filters
    * common settings added to application.conf

---
+++ templates/play-scala/app/controllers/CountController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+import services.Counter
+
+/**
+ * This controller demonstrates how to use dependency injection to
+ * bind a component into a controller class. The class creates an
+ * `Action` that shows an incrementing count to users. The [[Counter]]
+ * object is injected by the Guice dependency injection system.
+ */
+@Singleton
+class CountController @Inject() (counter: Counter) extends Controller {
+
+  /**
+   * Create an action that responds with the [[Counter]]'s current
+   * count. The result is plain text. This `Action` is mapped to
+   * `GET /count` requests by an entry in the `routes` config file.
+   */
+  def count = Action { Ok(counter.nextCount().toString) }
+
+}
