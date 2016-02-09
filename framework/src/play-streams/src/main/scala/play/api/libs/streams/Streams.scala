921ecf253c688a93e5db4c1fd79c679fd703a667
Nafer Sanabria <nafr.snabr@gmail.com>
2016-02-08 23:01:27 -0500

    Correct string interpolation

--- framework/src/play-streams/src/main/scala/play/api/libs/streams/Streams.scala
+++ framework/src/play-streams/src/main/scala/play/api/libs/streams/Streams.scala
@@ -103,7 +103,7 @@ object Streams {
   def iterateeDoneToPublisher[T, U](iter: Iteratee[T, U]): Publisher[U] = {
     iterateeFoldToPublisher[T, U, U](iter, {
       case Step.Done(x, _) => Future.successful(x)
-      case notDone: Step[T, U] => Future.failed(new Exception("Can only get value from Done iteratee: $notDone"))
+      case notDone: Step[T, U] => Future.failed(new Exception(s"Can only get value from Done iteratee: $notDone"))
     })(Execution.trampoline)
   }
 
