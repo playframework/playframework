95df68fd24dd0e85907238d28cc706a5590b7ebe
Marcos Pereira <marcos.silva@gmail.com>
2016-02-18 23:02:31 -0200

    Improve docs for Javascript Router
    1. Add docs for the Java version
    2. Extract code examples to snippets
    3. Better markdown format/highlights
    
    Fixes #2167

---
+++ documentation/manual/working/javaGuide/advanced/routing/code/javaguide/binder/controllers/Application.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
+ */
+package javaguide.binder.controllers;
+
+//#javascript-router-resource-imports
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.Routes;
+//#javascript-router-resource-imports
+
+// import static javaguide.binder.controllers.routes;
+
+public class Application extends Controller {
+    
+    //#javascript-router-resource
+    public Result javascriptRoutes() {
+        return ok(
+            Routes.javascriptRouter("jsRoutes",
+                routes.javascript.Users.list(),
+                routes.javascript.Users.get()
+            )
+        ).as("text/javascript");
+    }
+    //#javascript-router-resource
+
+    public Result javascriptRoutes2() {
+        return ok(
+            //#javascript-router-resource-custom-method
+            Routes.javascriptRouter("jsRoutes", "myAjaxMethod",
+                routes.javascript.Users.list(),
+                routes.javascript.Users.get()
+            )
+            //#javascript-router-resource-custom-method
+        ).as("text/javascript");
+    }
+}
