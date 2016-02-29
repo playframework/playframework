803595e5d2da200821ac3be441329269df2b29cd
Claudio Bley <cbley@exa-online.de>
2016-02-29 11:51:34 +0100

    Fix deprecation warnings in play-scala-intro
    ```
    test/ApplicationSpec.scala:23:
    
        method route in trait RouteInvokers is
        deprecated: Use the version that takes an application
    ```

--- templates/play-scala-intro/test/ApplicationSpec.scala
+++ templates/play-scala-intro/test/ApplicationSpec.scala
@@ -16,11 +16,11 @@ class ApplicationSpec extends Specification {
   "Application" should {
 
     "send 404 on a bad request" in new WithApplication {
-      route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
+      route(app, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
     }
 
     "render the index page" in new WithApplication {
-      val home = route(FakeRequest(GET, "/")).get
+      val home = route(app, FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
