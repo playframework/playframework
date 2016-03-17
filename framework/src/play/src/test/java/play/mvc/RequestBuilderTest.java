6dd9f914f66bb6c1528d40510506765585564276
Christian Schmitt <c.schmitt@envisia.de>
2016-03-17 02:59:23 +0100

    Fixes #2289 adds a bodyMultipart function to the request builder

--- framework/src/play/src/test/java/play/mvc/RequestBuilderTest.java
+++ framework/src/play/src/test/java/play/mvc/RequestBuilderTest.java
@@ -3,6 +3,7 @@
  */
 package play.mvc;
 
+import akka.stream.javadsl.Source;
 import play.api.Application;
 import play.api.Play;
 import play.api.inject.guice.GuiceApplicationBuilder;
@@ -10,6 +11,12 @@ import play.mvc.Http.Context;
 import play.mvc.Http.Request;
 import play.mvc.Http.RequestBuilder;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.Assert.*;
 
 public class RequestBuilderTest {
@@ -64,4 +71,26 @@ public class RequestBuilderTest {
         assertNull(req1.username());
         assertEquals("user2", req2.username());
     }
+
+  @Test
+  public void multipartForm() throws ExecutionException, InterruptedException {
+    Application app = new GuiceApplicationBuilder().build();
+    Play.start(app);
+    Http.MultipartFormData.DataPart dp = new Http.MultipartFormData.DataPart("hello", "world");
+    final Request request = new RequestBuilder().uri("http://playframework.com/")
+            .bodyMultipart(Collections.singletonList(dp), app.materializer())
+            .build();
+
+   Optional<Http.MultipartFormData<File>> parts = new BodyParser.MultipartFormData()
+           .apply(request)
+           .run(Source.single(request.body().asBytes()), app.materializer())
+           .toCompletableFuture()
+           .get()
+           .right;
+    assertEquals(true, parts.isPresent());
+    assertArrayEquals(new String[]{"world"}, parts.get().asFormUrlEncoded().get("hello"));
+
+    Play.stop(app);
+  }
+
 }
