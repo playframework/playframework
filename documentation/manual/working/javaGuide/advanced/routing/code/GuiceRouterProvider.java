38684aed572a2bff8ac58ba877678bfd61a9db0c
Schmitt Christian <c.schmitt@briefdomain.de>
2016-05-16 21:31:03 +0200

    Fixed #6133 added some docs for routes provider (#6143)

---
+++ documentation/manual/working/javaGuide/advanced/routing/code/GuiceRouterProvider.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2009-2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+import play.routing.Router;
+import router.RoutingDslBuilder;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+//#load-guice2
+@Singleton
+public class GuiceRouterProvider implements Provider<play.api.routing.Router> {
+
+    private final Router javaRouter;
+
+    @Inject
+    public GuiceRouterProvider(RoutingDslBuilder router) {
+        javaRouter = router.getRouter();
+    }
+
+    @Override
+    public play.api.routing.Router get() {
+        return javaRouter.asScala();
+    }
+
+}
+//#load-guice2
