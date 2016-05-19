bf329529d41b190c7ede1ee77878064a3e424511
Matthias Kurz <m.kurz@irregular.at>
2016-05-19 22:43:34 +0200

    Documentation fix: Action should not be static anymore (#6169)

--- documentation/manual/working/javaGuide/main/upload/code/JavaFileUpload.java
+++ documentation/manual/working/javaGuide/main/upload/code/JavaFileUpload.java
@@ -3,18 +3,21 @@
  */
 import play.mvc.Controller;
 import java.io.File;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
 
 public class JavaFileUpload {
 
     static class SyncUpload extends Controller {
         //#syncUpload
-        public static play.mvc.Result upload() {
-            play.mvc.Http.MultipartFormData<File> body = request().body().asMultipartFormData();
-            play.mvc.Http.MultipartFormData.FilePart<File> picture = body.getFile("picture");
+        public Result upload() {
+            MultipartFormData<File> body = request().body().asMultipartFormData();
+            FilePart<File> picture = body.getFile("picture");
             if (picture != null) {
                 String fileName = picture.getFilename();
                 String contentType = picture.getContentType();
-                java.io.File file = picture.getFile();
+                File file = picture.getFile();
                 return ok("File uploaded");
             } else {
                 flash("error", "Missing file");
@@ -26,8 +29,8 @@ public class JavaFileUpload {
 
     static class AsyncUpload extends Controller {
         //#asyncUpload
-        public static play.mvc.Result upload() {
-            java.io.File file = request().body().asRaw().asFile();
+        public Result upload() {
+            File file = request().body().asRaw().asFile();
             return ok("File uploaded");
         }
         //#asyncUpload
