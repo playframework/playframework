734ab4e575c4ad76ced7288b49d9ff586f87d2d0
Rich Dougherty <rich@rd.gen.nz>
2016-05-31 20:39:36 +1200

    Better Result error handling
    This is a backport of #6153.
    
    When the backend server (Netty, Akka HTTP) discovers an error in a
    result (e.g. invalid headers) the server needs to abort processing the
    result and send an error result to the user. Previously, this error
    handling was done with custom error handling code for the server.
    
    This pull request changes the server behavior to use the application's
    HttpErrorHandler to generate the error result instead of generating a
    custom error response. Using the existing error handler makes the
    error handling more standardized. It also means that the application
    will be notified when an error occurs during result generation, which
    will make it easier to track errors that occur in Play applications
    (see #6098, #6099).

--- framework/src/play-netty-server/src/main/scala/play/core/server/netty/NettyModelConversion.scala
+++ framework/src/play-netty-server/src/main/scala/play/core/server/netty/NettyModelConversion.scala
@@ -17,12 +17,13 @@ import io.netty.handler.codec.http._
 import io.netty.handler.ssl.SslHandler
 import io.netty.util.ReferenceCountUtil
 import play.api.Logger
+import play.api.http._
 import play.api.http.HeaderNames._
-import play.api.http.{ Status, HttpChunk, HttpEntity }
 import play.api.mvc._
 import play.core.server.common.{ ConnectionInfo, ServerResultUtils, ForwardedHeaderHandler }
 
 import scala.collection.JavaConverters._
+import scala.concurrent.Future
 import scala.util.{ Failure, Try }
 import scala.util.control.NonFatal
 
@@ -153,36 +154,39 @@ private[server] class NettyModelConversion(forwardedHeaderHandler: ForwardedHead
   }
 
   /** Create a Netty response from the result */
-  def convertResult(result: Result, requestHeader: RequestHeader, httpVersion: HttpVersion)(implicit mat: Materializer): HttpResponse = {
+  def convertResult(
+    result: Result, requestHeader: RequestHeader, httpVersion: HttpVersion, errorHandler: HttpErrorHandler)(
+      implicit mat: Materializer): Future[HttpResponse] = {
 
-    val responseStatus = result.header.reasonPhrase match {
-      case Some(phrase) => new HttpResponseStatus(result.header.status, phrase)
-      case None => HttpResponseStatus.valueOf(result.header.status)
-    }
+    ServerResultUtils.resultConversionWithErrorHandling(requestHeader, result, errorHandler) { result =>
 
-    val connectionHeader = ServerResultUtils.determineConnectionHeader(requestHeader, result)
-    val skipEntity = requestHeader.method == HttpMethod.HEAD.name()
+      val responseStatus = result.header.reasonPhrase match {
+        case Some(phrase) => new HttpResponseStatus(result.header.status, phrase)
+        case None => HttpResponseStatus.valueOf(result.header.status)
+      }
 
-    val response: HttpResponse = result.body match {
+      val connectionHeader = ServerResultUtils.determineConnectionHeader(requestHeader, result)
+      val skipEntity = requestHeader.method == HttpMethod.HEAD.name()
 
-      case any if skipEntity =>
-        ServerResultUtils.cancelEntity(any)
-        new DefaultFullHttpResponse(httpVersion, responseStatus, Unpooled.EMPTY_BUFFER)
+      val response: HttpResponse = result.body match {
 
-      case HttpEntity.Strict(data, _) =>
-        new DefaultFullHttpResponse(httpVersion, responseStatus, byteStringToByteBuf(data))
+        case any if skipEntity =>
+          ServerResultUtils.cancelEntity(any)
+          new DefaultFullHttpResponse(httpVersion, responseStatus, Unpooled.EMPTY_BUFFER)
 
-      case HttpEntity.Streamed(stream, _, _) =>
-        createStreamedResponse(stream, httpVersion, responseStatus)
+        case HttpEntity.Strict(data, _) =>
+          new DefaultFullHttpResponse(httpVersion, responseStatus, byteStringToByteBuf(data))
 
-      case HttpEntity.Chunked(chunks, _) =>
-        createChunkedResponse(chunks, httpVersion, responseStatus)
-    }
+        case HttpEntity.Streamed(stream, _, _) =>
+          createStreamedResponse(stream, httpVersion, responseStatus)
 
-    // Set response headers
-    val headers = ServerResultUtils.splitSetCookieHeaders(result.header.headers)
+        case HttpEntity.Chunked(chunks, _) =>
+          createChunkedResponse(chunks, httpVersion, responseStatus)
+      }
 
-    try {
+      // Set response headers
+      val headers = ServerResultUtils.splitSetCookieHeaders(result.header.headers)
+
       headers foreach {
         case (name, value) => response.headers().add(name, value)
       }
@@ -218,19 +222,14 @@ private[server] class NettyModelConversion(forwardedHeaderHandler: ForwardedHead
         response.headers().add(DATE, dateHeader)
       }
 
+      Future.successful(response)
+    } {
+      // Fallback response
+      val response = new DefaultFullHttpResponse(httpVersion, HttpResponseStatus.INTERNAL_SERVER_ERROR, Unpooled.EMPTY_BUFFER)
+      HttpHeaders.setContentLength(response, 0)
+      response.headers().add(DATE, dateHeader)
+      response.headers().add(CONNECTION, "close")
       response
-    } catch {
-      case NonFatal(e) =>
-        if (logger.isErrorEnabled) {
-          val prettyHeaders = headers.map { case (name, value) => s"$name -> $value" }.mkString("[", ",", "]")
-          val msg = s"Exception occurred while setting response's headers to $prettyHeaders. Action taken is to set the response's status to ${HttpResponseStatus.INTERNAL_SERVER_ERROR} and discard all headers."
-          logger.error(msg, e)
-        }
-        val response = new DefaultFullHttpResponse(httpVersion, HttpResponseStatus.INTERNAL_SERVER_ERROR, Unpooled.EMPTY_BUFFER)
-        HttpHeaders.setContentLength(response, 0)
-        response.headers().add(DATE, dateHeader)
-        response.headers().add(CONNECTION, "close")
-        response
     }
   }
 
