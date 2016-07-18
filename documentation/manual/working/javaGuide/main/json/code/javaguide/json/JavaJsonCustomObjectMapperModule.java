8339d4ecd6b55b270b02fdbea6a8a17db37245e1
Schmitt Christian <c.schmitt@briefdomain.de>
2016-07-18 23:49:50 +0200

    Fixed #6318 made the documentation about customizing a object mapper better (#6330)

---
+++ documentation/manual/working/javaGuide/main/json/code/javaguide/json/JavaJsonCustomObjectMapperModule.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2009-2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package javaguide.json;
+
+import com.google.inject.AbstractModule;
+
+//#custom-java-object-mapper2
+public class JavaJsonCustomObjectMapperModule extends AbstractModule{
+
+    @Override
+    protected void configure() {
+        bind(JavaJsonCustomObjectMapper.class).asEagerSingleton();
+    }
+
+}
+//#custom-java-object-mapper2
