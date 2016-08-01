b43fb534fb2386d0c7ac89933f4081e1957f9fef
Rene Weber <reneweb@users.noreply.github.com>
2016-08-01 08:55:43 +0100

    Add async try-with-resources for java api (#6357)

---
+++ framework/src/play-java/src/test/java/play/libs/ResourcesTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2009-2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.libs;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ResourcesTest {
+
+    @Test
+    public void testAsyncTryWithResource() throws Exception {
+        InputStream inputStream = mock(InputStream.class);
+        CompletionStage<Void> completionStage = Resources.asyncTryWithResource(
+                inputStream,
+                is -> CompletableFuture.completedFuture(null)
+        );
+
+        completionStage.toCompletableFuture().get();
+        verify(inputStream).close();
+    }
+
+    @Test
+    public void testAsyncTryWithResourceExceptionInFuture() throws Exception {
+        InputStream inputStream = mock(InputStream.class);
+        CompletionStage<Void> completionStage = Resources.asyncTryWithResource(
+                inputStream,
+                is -> CompletableFuture.runAsync(() -> { throw new RuntimeException(); })
+        );
+
+        try {
+            completionStage.toCompletableFuture().get();
+        } catch(Exception ignored) {}
+
+        verify(inputStream).close();
+    }
+
+    @Test
+    public void testAsyncTryWithResourceException() throws Exception {
+        InputStream inputStream = mock(InputStream.class);
+        try {
+            CompletionStage<Void> completionStage = Resources.asyncTryWithResource(
+                    inputStream,
+                    is -> { throw new RuntimeException(); }
+            );
+            completionStage.toCompletableFuture().get();
+        } catch(Exception ignored) {}
+
+        verify(inputStream).close();
+    }
+}
