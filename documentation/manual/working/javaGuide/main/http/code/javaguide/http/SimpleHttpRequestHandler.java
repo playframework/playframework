34bda5490299322ea763db9423b46f7f28157e47
Greg Methvin <greg.methvin@gmail.com>
2016-08-02 00:35:44 -0700

    Fix HttpRequestHandler docs to add JavaHandlerComponents (#6371)

--- documentation/manual/working/javaGuide/main/http/code/javaguide/http/SimpleHttpRequestHandler.java
+++ documentation/manual/working/javaGuide/main/http/code/javaguide/http/SimpleHttpRequestHandler.java
@@ -10,19 +10,26 @@ import play.api.mvc.Handler;
 import play.http.*;
 import play.mvc.*;
 import play.libs.streams.Accumulator;
+import play.core.j.JavaHandler;
+import play.core.j.JavaHandlerComponents;
 
 public class SimpleHttpRequestHandler implements HttpRequestHandler {
     private final Router router;
+    private final JavaHandlerComponents components;
 
     @Inject
-    public SimpleHttpRequestHandler(Router router) {
+    public SimpleHttpRequestHandler(Router router, JavaHandlerComponents components) {
         this.router = router;
+        this.components = components;
     }
 
     public HandlerForRequest handlerForRequest(Http.RequestHeader request) {
         Handler handler = router.route(request).orElseGet(() ->
             EssentialAction.of(req -> Accumulator.done(Results.notFound()))
         );
+        if (handler instanceof JavaHandler) {
+            handler = ((JavaHandler)handler).withComponents(components);
+        }
         return new HandlerForRequest(request, handler);
     }
 }
