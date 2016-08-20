b922a482f7c5216f0dfc47fe93e4577b3d157c90
Schmitt Christian <c.schmitt@briefdomain.de>
2016-08-20 07:47:10 +0200

    stop hooks won't be called when Play.stop(app) was called (#6469) (#6470)

--- framework/src/play/src/test/scala/play/api/inject/guice/GuiceApplicationLoaderSpec.scala
+++ framework/src/play/src/test/scala/play/api/inject/guice/GuiceApplicationLoaderSpec.scala
@@ -4,12 +4,12 @@
 package play.api.inject.guice
 
 import org.specs2.mutable.Specification
-
 import com.google.inject.AbstractModule
-
-import play.{ Configuration => JavaConfiguration, Environment => JavaEnvironment }
-import play.api.{ ApplicationLoader, Configuration, Environment }
+import play.{Configuration => JavaConfiguration, Environment => JavaEnvironment}
+import play.api.{ApplicationLoader, Configuration, Environment}
 import play.api.inject.BuiltinModule
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 class GuiceApplicationLoaderSpec extends Specification {
 
@@ -50,6 +50,16 @@ class GuiceApplicationLoaderSpec extends Specification {
       val loader = new GuiceApplicationLoader()
       val app = loader.load(fakeContextWithModule(classOf[JavaConfiguredModule]))
       app.injector.instanceOf[Foo] must beAnInstanceOf[JavaConfiguredFoo]
+    }
+
+    "call the stop hooks from the context" in {
+      val context = ApplicationLoader.createContext(Environment.simple())
+      var hooksCalled = false
+      context.lifecycle.addStopHook(() => Future.successful(hooksCalled = true))
+      val loader = new GuiceApplicationLoader()
+      val app = loader.load(context)
+      Await.ready(app.stop(), 5.minutes)
+      hooksCalled must_== true
     }
 
   }
