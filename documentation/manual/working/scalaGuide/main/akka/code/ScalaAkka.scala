0e2a24b97ad6c371150eeb9211c35ed892aafd07
Richard Lichlyter-Klein <richard.lichlyterklein@gmail.com>
2016-09-05 00:50:09 -0600

    Minor addition: adding the timeout type (#6521)
    * Minor addition: adding the timeout type
    
    * PR Feedback: fixing build failure by adding the necessary override

--- documentation/manual/working/scalaGuide/main/akka/code/ScalaAkka.scala
+++ documentation/manual/working/scalaGuide/main/akka/code/ScalaAkka.scala
@@ -6,12 +6,14 @@ package scalaguide.akka {
 import akka.actor.ActorSystem
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
+
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import play.api.test._
 import java.io.File
+
+import akka.util.Timeout
 
 class ScalaAkkaSpec extends PlaySpecification {
 
@@ -26,7 +28,10 @@ class ScalaAkkaSpec extends PlaySpecification {
       Await.result(system.whenTerminated, Duration.Inf)
     }
   }
-  
+
+  override def defaultAwaitTimeout: Timeout = 5.seconds
+
+
   "The Akka support" should {
 
     "allow injecting actors" in new WithApplication() {
@@ -38,12 +43,13 @@ class ScalaAkkaSpec extends PlaySpecification {
       import play.api.mvc.Results._
       import actors.HelloActor.SayHello
 
+
       //#ask
       import play.api.libs.concurrent.Execution.Implicits.defaultContext
       import scala.concurrent.duration._
       import akka.pattern.ask
-      implicit val timeout = 5.seconds
-      
+      implicit val timeout: Timeout = 5.seconds
+
       def sayHello(name: String) = Action.async {
         (helloActor ? SayHello(name)).mapTo[String].map { message =>
           Ok(message)
@@ -59,6 +65,7 @@ class ScalaAkkaSpec extends PlaySpecification {
       .configure("my.config" -> "foo")
     ) { _ =>
       import injection._
+      implicit val timeout: Timeout = 5.seconds
       val controller = app.injector.instanceOf[Application]
       contentAsString(controller.getConfig(FakeRequest())) must_== "foo"
     }
@@ -72,7 +79,7 @@ class ScalaAkkaSpec extends PlaySpecification {
       import play.api.libs.concurrent.Execution.Implicits.defaultContext
       import scala.concurrent.duration._
       import akka.pattern.ask
-      implicit val timeout = 5.seconds
+      implicit val timeout: Timeout = 5.seconds
 
       val actor = app.injector.instanceOf(bind[ActorRef].qualifiedWith("parent-actor"))
       val futureConfig = for {
