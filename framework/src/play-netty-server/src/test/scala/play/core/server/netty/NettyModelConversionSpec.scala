b8fd938586b59c666906306b268eb931bff0301c
Christian Schmitt <c.schmitt@envisia.de>
2016-09-06 10:37:58 +0200

    more tests

--- framework/src/play-netty-server/src/test/scala/play/core/server/netty/NettyModelConversionSpec.scala
+++ framework/src/play-netty-server/src/test/scala/play/core/server/netty/NettyModelConversionSpec.scala
@@ -22,6 +22,11 @@ class NettyModelConversionSpec extends Specification {
       query must_== Map("hello" -> List("海"))
     }
 
+    "parse all query params correctly" in {
+      val (_, query) = modelConversion.parseUri("/?filter=a=b,c")
+      query must_== Map("filter" -> List("a=b", "c"))
+    }
+
     "throw a URISyntaxException if it contains whitespace" in {
       modelConversion.parseUri("/projects/1234/users/186205 86%20%7C%") must throwA[URISyntaxException]
     }
