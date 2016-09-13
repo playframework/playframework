37aeb1bbd8f9dc77b5df0b93c6f9096cac1356f8
Will Sargent <will.sargent@lightbend.com>
2016-09-13 16:22:42 -0700

    fix documentation tests

--- documentation/manual/working/scalaGuide/main/tests/code/tests/guice/ScalaGuiceApplicationBuilderSpec.scala
+++ documentation/manual/working/scalaGuide/main/tests/code/tests/guice/ScalaGuiceApplicationBuilderSpec.scala
@@ -29,7 +29,7 @@ class ScalaGuiceApplicationBuilderSpec extends PlaySpecification {
       val classLoader = new URLClassLoader(Array.empty)
       // #set-environment
       val application = new GuiceApplicationBuilder()
-        .load(new play.api.inject.BuiltinModule) // ###skip
+        .load(new play.api.inject.BuiltinModule, new play.api.i18n.I18nModule) // ###skip
         .loadConfig(Configuration.reference) // ###skip
         .in(Environment(new File("path/to/app"), classLoader, Mode.Test))
         .build
@@ -44,7 +44,7 @@ class ScalaGuiceApplicationBuilderSpec extends PlaySpecification {
       val classLoader = new URLClassLoader(Array.empty)
       // #set-environment-values
       val application = new GuiceApplicationBuilder()
-        .load(new play.api.inject.BuiltinModule) // ###skip
+        .load(new play.api.inject.BuiltinModule, new play.api.i18n.I18nModule) // ###skip
         .loadConfig(Configuration.reference) // ###skip
         .in(new File("path/to/app"))
         .in(Mode.Test)
