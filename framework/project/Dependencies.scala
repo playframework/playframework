3db33816c63e0f9b7efb7e029cba274bfc74e808
Will Sargent <wsargent@users.noreply.github.com>
2016-09-26 07:33:20 -0700

    Upgrade spec2 and twirl (#6593)

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -184,6 +184,7 @@ object Dependencies {
   def sbtRcClient(scalaBinaryVersion: String): ModuleID = CrossVersion.partialVersion(scalaBinaryVersion) match {
     case Some((2, 10)) => "com.typesafe.sbtrc" % "client-2-10" % sbtRcVersion
     case Some((2, 11)) => "com.typesafe.sbtrc" % "client-2-11" % sbtRcVersion
+    case Some((2, 12)) => "com.typesafe.sbtrc" % "client-2-12" % sbtRcVersion
     case _ => sys.error(s"Unsupported scala version: $scalaBinaryVersion")
   }
 
@@ -196,6 +197,7 @@ object Dependencies {
   def sbtRcActorClient(scalaBinaryVersion: String): ModuleID = CrossVersion.partialVersion(scalaBinaryVersion) match {
     case Some((2, 10)) => "com.typesafe.sbtrc" % "actor-client-2-10" % sbtRcVersion
     case Some((2, 11)) => "com.typesafe.sbtrc" % "actor-client-2-11" % sbtRcVersion
+    case Some((2, 12)) => "com.typesafe.sbtrc" % "actor-client-2-12" % sbtRcVersion
     case _ => sys.error(s"Unsupported scala version: $scalaBinaryVersion")
   }
 
