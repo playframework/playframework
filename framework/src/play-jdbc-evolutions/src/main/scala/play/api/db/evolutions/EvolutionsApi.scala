621b67eb56d354dd77397f7213758def2014bc59
Tsuyoshi Yoshizawa <tsuyoshi.yoshizawa@gmail.com>
2016-09-27 02:13:49 +0900

    fix failed evolutions with query parameter

--- framework/src/play-jdbc-evolutions/src/main/scala/play/api/db/evolutions/EvolutionsApi.scala
+++ framework/src/play-jdbc-evolutions/src/main/scala/play/api/db/evolutions/EvolutionsApi.scala
@@ -553,7 +553,7 @@ case class InconsistentDatabase(db: String, script: String, error: String, rev: 
   private val resolvePathJavascript =
     if (autocommit) s"'/@evolutions/resolve/$db/$rev?redirect=' + encodeURIComponent(window.location)"
     else "'/@evolutions'"
-  private val redirectJavascript = s"""window.location = window.location.href.replace(/\\/@evolutions.*$$|\\/$$/, '') + $resolvePathJavascript"""
+  private val redirectJavascript = s"""window.location = window.location.href.split(/[?#]/)[0].replace(/\\/@evolutions.*$$|\\/$$/, '') + $resolvePathJavascript"""
 
   private val sentenceEnd = if (autocommit) " before marking it as resolved." else "."
 
