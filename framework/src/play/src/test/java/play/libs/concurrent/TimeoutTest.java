4c43b63aa5d45603f22fdc56b33c564b350d9212
Greg Methvin <greg.methvin@gmail.com>
2016-11-08 15:56:10 -0800

    Add Timeout trait to play.api.libs.concurrent (#6459) (#6684)

---
+++ framework/src/play/src/test/java/play/libs/concurrent/TimeoutTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2009-2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.libs.concurrent;
+
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class TimeoutTest {
+
+    @Test
+    public void successfulTimeout() throws Exception {
+        class MyClass implements play.libs.concurrent.Timeout {
+            CompletionStage<Double> callWithTimeout() {
+                return timeout(computePIAsynchronously(), Duration.ofSeconds(1));
+            }
+        }
+        final Double actual = new MyClass().callWithTimeout().toCompletableFuture().get(1, TimeUnit.SECONDS);
+        final Double expected = Math.PI;
+        assertThat(actual, equalTo(expected));
+    }
+
+    @Test
+    public void failedTimeout() throws Exception {
+        class MyClass implements play.libs.concurrent.Timeout {
+            CompletionStage<Double> callWithTimeout() {
+                return timeout(delayedFuture(), Duration.ofMillis(300));
+            }
+        }
+        final Double actual = new MyClass()
+                .callWithTimeout()
+                .toCompletableFuture()
+                .exceptionally(e -> 100d)
+                .get(1, TimeUnit.SECONDS);
+        final Double expected = 100d;
+        assertThat(actual, equalTo(expected));
+    }
+
+    private static CompletionStage<Double> computePIAsynchronously() {
+        return CompletableFuture.completedFuture(Math.PI);
+    }
+
+
+    private static CompletionStage<Double> delayedFuture() {
+        return Futures.delayed(() -> Math.PI, 1, TimeUnit.SECONDS, ForkJoinPool.commonPool());
+    }
+
+    private static Integer intensiveComputation() {
+        return 1 + 1;
+    }
+}
---
+++ framework/src/play/src/test/scala/play/api/libs/concurrent/TimeoutSpec.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2009-2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.api.libs.concurrent
+
+import akka.actor.ActorSystem
+import org.specs2.mutable.Specification
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
+// testOnly play.api.libs.concurrent.TimeoutSpec
+class TimeoutSpec extends Specification {
+
+  class MyService(val actorSystem: ActorSystem) extends Timeout {
+
+    def calculateWithTimeout(timeoutDuration: FiniteDuration): Future[Int] = {
+      timeout(actorSystem, timeoutDuration)(rawCalculation())
+    }
+
+    def rawCalculation(): Future[Int] = {
+      import akka.pattern.after
+      implicit val ec = actorSystem.dispatcher
+      after(300 millis, actorSystem.scheduler)(Future(42))(actorSystem.dispatcher)
+    }
+  }
+
+  val timeoutDuration = 10 seconds
+
+  "Timeout" should {
+
+    "timeout if duration is too small" in {
+      val actorSystem = ActorSystem()
+      implicit val ec = actorSystem.dispatcher
+      val future = new MyService(actorSystem).calculateWithTimeout(100 millis).recover {
+        case _: TimeoutException =>
+          -1
+      }
+      val result = Await.result(future, timeoutDuration) must be_==(-1)
+      actorSystem.terminate()
+      result
+    }
+
+    "succeed eventually with the raw calculation" in {
+      val actorSystem = ActorSystem()
+      implicit val ec = actorSystem.dispatcher
+      val future = new MyService(actorSystem).rawCalculation().recover {
+        case _: TimeoutException =>
+          -1
+      }
+      val result = Await.result(future, timeoutDuration) must be_==(42)
+      actorSystem.terminate()
+      result
+    }
+
+    "succeed with a timeout duration" in {
+      val actorSystem = ActorSystem()
+      implicit val ec = actorSystem.dispatcher
+      val future = new MyService(actorSystem).calculateWithTimeout(600 millis).recover {
+        case _: TimeoutException =>
+          -1
+      }
+      val result = Await.result(future, timeoutDuration) must be_==(42)
+      actorSystem.terminate()
+      result
+    }
+
+  }
+
+  "Future enriched with FutureTimeout implicit class" should {
+
+    "timeout with a duration" in {
+      import Timeout._
+
+      implicit val actorSystem = ActorSystem()
+      implicit val ec = actorSystem.dispatcher
+      val future = new MyService(actorSystem).rawCalculation().withTimeout(100 millis).recover {
+        case _: TimeoutException =>
+          -1
+      }
+      val result = Await.result(future, timeoutDuration) must be_==(-1)
+      actorSystem.terminate()
+      result
+    }
+
+    "succeed with a duration" in {
+      import Timeout._
+
+      implicit val actorSystem = ActorSystem()
+      implicit val ec = actorSystem.dispatcher
+      val future = new MyService(actorSystem).rawCalculation().withTimeout(500 millis).recover {
+        case _: TimeoutException =>
+          -1
+      }
+      val result = Await.result(future, timeoutDuration) must be_==(42)
+      actorSystem.terminate()
+      result
+    }
+
+    "timeout with an implicit akka.util.Timeout" in {
+      import Timeout._
+
+      implicit val actorSystem = ActorSystem()
+      implicit val ec = actorSystem.dispatcher
+      implicit val implicitTimeout = akka.util.Timeout(100 millis)
+      val future = new MyService(actorSystem).rawCalculation().withTimeout.recover {
+        case _: TimeoutException =>
+          -1
+      }
+      val result = Await.result(future, timeoutDuration) must be_==(-1)
+      actorSystem.terminate()
+      result
+    }
+
+    "succeed with an implicit akka.util.Timeout" in {
+      import Timeout._
+
+      implicit val actorSystem = ActorSystem()
+      implicit val ec = actorSystem.dispatcher
+      implicit val implicitTimeout = akka.util.Timeout(500 millis)
+      val future = new MyService(actorSystem).rawCalculation().withTimeout.recover {
+        case _: TimeoutException =>
+          -1
+      }
+      val result = Await.result(future, timeoutDuration) must be_==(42)
+      actorSystem.terminate()
+      result
+    }
+  }
+
+}
