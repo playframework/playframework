a73d3ebcc569ab100f8e3b98373526258b99401f
yutayokoi <dezirus.novajn.iojn@gmail.com>
2016-12-09 15:00:50 +0900

    Refactoring play-server - !(isDefined) to isEmpty (#6783)

--- framework/src/play-server/src/main/scala/play/core/server/ServerConfig.scala
+++ framework/src/play-server/src/main/scala/play/core/server/ServerConfig.scala
@@ -30,7 +30,7 @@ case class ServerConfig(
     properties: Properties,
     configuration: Configuration) {
   // Some basic validation of config
-  if (!port.isDefined && !sslPort.isDefined) throw new IllegalArgumentException("Must provide either an HTTP port or an HTTPS port")
+  if (port.isEmpty && sslPort.isEmpty) throw new IllegalArgumentException("Must provide either an HTTP port or an HTTPS port")
 }
 
 object ServerConfig {
