d283729dac7623c6b1c6687576532583310936b9
Matthias Kurz <m.kurz@irregular.at>
2016-12-15 13:54:16 +0100

    Create ValidatorFactory just once (#6800)
    * Create validatorFactory just once
    
    * Close validatorFactory when app stops

--- framework/src/play-java/src/main/java/play/data/validation/ValidatorProvider.java
+++ framework/src/play-java/src/main/java/play/data/validation/ValidatorProvider.java
@@ -3,27 +3,37 @@
  */
 package play.data.validation;
 
+import java.util.concurrent.CompletableFuture;
+
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 import javax.validation.Validator;
 import javax.validation.Validation;
 import javax.validation.ConstraintValidatorFactory;
+import javax.validation.ValidatorFactory;
+
+import play.inject.ApplicationLifecycle;
 
 @Singleton
 public class ValidatorProvider implements Provider<Validator> {
 
-    private ConstraintValidatorFactory constraintValidatorFactory;
+    private ValidatorFactory validatorFactory;
 
     @Inject
-    public ValidatorProvider(ConstraintValidatorFactory constraintValidatorFactory) {
-        this.constraintValidatorFactory = constraintValidatorFactory;
+    public ValidatorProvider(ConstraintValidatorFactory constraintValidatorFactory, final ApplicationLifecycle lifecycle) {
+        this.validatorFactory = Validation.byDefaultProvider().configure()
+                .constraintValidatorFactory(constraintValidatorFactory)
+                .buildValidatorFactory();
+
+        lifecycle.addStopHook(() -> {
+            this.validatorFactory.close();
+            return CompletableFuture.completedFuture(null);
+        });
     }
 
     public Validator get() {
-        return Validation.buildDefaultValidatorFactory().usingContext()
-            .constraintValidatorFactory(constraintValidatorFactory)
-            .getValidator();
+        return this.validatorFactory.getValidator();
     }
 
 }
