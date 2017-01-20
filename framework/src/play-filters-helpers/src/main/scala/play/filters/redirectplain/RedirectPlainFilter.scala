8bc4e8af99f5a84d144ba12e1df43ee656ed97b6
Dennis Ruhe <dennis@amazingsystems.nl>
2017-01-20 10:54:53 +0100

    Refactored the RedirectPlainFilter a little
    - Changed redirect status code to PERMANENT_REDIRECT
    - Created companion object and extracted some methods
    - Both checking of X-Forwarded-Proto and Forwarded headers
    - Removed the 'enabled' config option as this is useless, the filter
      needs to be injected into the Filters anyway

--- framework/src/play-filters-helpers/src/main/scala/play/filters/redirectplain/RedirectPlainFilter.scala
+++ framework/src/play-filters-helpers/src/main/scala/play/filters/redirectplain/RedirectPlainFilter.scala
@@ -8,11 +8,52 @@ import javax.inject.{ Inject, Provider }
 import akka.stream.Materializer
 import play.api.Configuration
 import play.api.http.HeaderNames._
+import play.api.http.Status._
 import play.api.inject.SimpleModule
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+
+object RedirectPlainFilter {
+
+  /**
+   * Create https redirect url for given request
+   *
+   * @param req  Request to create https request url for
+   * @return
+   */
+  def createHttpsRedirectUrl(req: RequestHeader): String = s"https://${req.host}${req.uri}"
+
+  /**
+   * Checks both the X_FORWARDED_PROTO and FORWARDED headers
+   *
+   * @see {play.filters.redirectplain.RedirectPlainFilter#isSecureForwarded}
+   * @param req  Request to check if it is secure
+   * @return
+   */
+  def isRequestSecure(req: RequestHeader): Boolean = req.secure || isSecureForwarded(req)
+
+  /**
+   * Checks to see if either the X_FORWARDED_PROTO or FORWARDED headers
+   * contains a https proxy. If either one of them contains https
+   * the request is assumed secure.
+   *
+   * @param req  Request to check if it is forwarded securely
+   * @return
+   */
+  def isSecureForwarded(req: RequestHeader): Boolean = {
+    // First check the X_FORWARDED_PROTO
+    req.headers.get(X_FORWARDED_PROTO) match {
+      case Some(value) => value.contains("https")
+      // If X_FORWARDED_PROTO header not found check the forwarded headers
+      case None => req.headers.get(FORWARDED) match {
+        case Some(value) => value.contains("https")
+        case None => false
+      }
+    }
+  }
+}
 
 /**
  * A filter that redirects plain request to https requests
@@ -20,36 +61,29 @@ import scala.concurrent.Future
  */
 case class RedirectPlainFilter @Inject() (val mat: Materializer, config: RedirectPlainConfig)
     extends Filter {
+  import RedirectPlainFilter._
+
+  lazy val strictMaxAge = s"max-age=${config.strictTransportSecurityMaxAge}"
 
   override def apply(nextFilter: RequestHeader => Future[Result])(req: RequestHeader): Future[Result] = {
-    if (config.enabled) {
-      (req.headers.get(X_FORWARDED_PROTO) match {
-        case Some(value) => (value == "https")
-        case None => req.secure
-      }) match {
-        case true => nextFilter(req).map(_.withHeaders(STRICT_TRANSPORT_SECURITY -> s"max-age=${config.strictTransportSecurityMaxAge}"))
-        case false => Future(Results.Redirect(s"https://${req.host}${req.uri}", 301))
-      }
-    } else {
-      nextFilter(req)
-    }
+    if (isRequestSecure(req))
+      nextFilter(req).map(_.withHeaders(STRICT_TRANSPORT_SECURITY -> strictMaxAge))
+    else
+      Future(Results.Redirect(createHttpsRedirectUrl(req), PERMANENT_REDIRECT))
   }
+
 }
 
-case class RedirectPlainConfig(enabled: Boolean, strictTransportSecurityMaxAge: Long)
+case class RedirectPlainConfig(strictTransportSecurityMaxAge: Long)
 
 class RedirectPlainConfigProvider @Inject() (configuration: Configuration)
     extends Provider[RedirectPlainConfig] {
-
-  lazy val enabled: Boolean = configuration
-    .getOptional[Boolean]("play.filters.redirectplain.enabled")
-    .getOrElse(false)
 
   lazy val strictTransportSecurityMaxAge = configuration
     .getOptional[Long]("play.filters.redirectplain.strict-transport-security.max-age")
     .getOrElse(31536000l)
 
-  lazy val get = RedirectPlainConfig(enabled, strictTransportSecurityMaxAge)
+  lazy val get = RedirectPlainConfig(strictTransportSecurityMaxAge)
 
 }
 
--- framework/src/play-filters-helpers/src/test/scala/play/filters/redirectplain/RedirectPlainFilterSpec.scala
+++ framework/src/play-filters-helpers/src/test/scala/play/filters/redirectplain/RedirectPlainFilterSpec.scala
@@ -6,17 +6,16 @@ package play.filters.redirectplain
 import javax.inject.Inject
 
 import com.typesafe.config.ConfigFactory
-import play.api.{ Application, Configuration }
 import play.api.http.HttpFilters
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.ws.WSClient
 import play.api.mvc.Results._
 import play.api.mvc._
 import play.api.routing.Router
 import play.api.test._
+import play.api.{ Application, Configuration }
 
-class TestFilters @Inject() (redirectPlainFilter: RedirectPlainFilter) extends HttpFilters {
+private[redirectplain] class TestFilters @Inject() (redirectPlainFilter: RedirectPlainFilter) extends HttpFilters {
   override def filters: Seq[EssentialFilter] = Seq(redirectPlainFilter)
 }
 
@@ -25,7 +24,7 @@ class RedirectPlainFilterSpec extends PlaySpecification {
   private def request(app: Application, xForwardedProto: String, uri: String = "/", headers: Seq[(String, String)] = Seq()) = {
     val req = FakeRequest(method = "GET", path = uri)
       .withHeaders(headers: _*)
-      .withHeaders(HOST -> "localhost")
+      .withHeaders(HOST -> "playframework.com")
       .withHeaders(X_FORWARDED_PROTO -> xForwardedProto)
     route(app, req).get
   }
@@ -45,32 +44,36 @@ class RedirectPlainFilterSpec extends PlaySpecification {
     running(app)(block(app))
   }
 
-  val TestServerPort = 8192
-  def withServer[T](config: String)(block: WSClient => T): T = {
-    val app = buildApp(config)
-    running(TestServer(TestServerPort, app))(block(app.injector.instanceOf[WSClient]))
-  }
-
   "RedirectPlainFilter" should {
 
-    "do nothing when not explicitly enabled" in withApplication("") { app =>
-      status(request(app, "http")) must_== OK
+    "correctly determine X-Forwarded-Proto security" in {
+      RedirectPlainFilter.isSecureForwarded(FakeRequest().withHeaders(X_FORWARDED_PROTO -> "http")) must_== false
+      RedirectPlainFilter.isSecureForwarded(FakeRequest().withHeaders(X_FORWARDED_PROTO -> "https")) must_== true
     }
 
-    "redirect when not on https" in withApplication("play.filters.redirectplain.enabled=true") { app =>
-      status(request(app, "http")) must_== MOVED_PERMANENTLY
-      header(LOCATION, request(app, "http", "/test/123?foo=bar&bar=baz")) must_== Some ("https://localhost/test/123?foo=bar&bar=baz")
+    "correctly determine Forwarded security" in {
+      RedirectPlainFilter.isSecureForwarded(FakeRequest().withHeaders(FORWARDED -> "for=192.0.2.60;proto=http;by=203.0.113.43")) must_== false
+      RedirectPlainFilter.isSecureForwarded(FakeRequest().withHeaders(FORWARDED -> "for=192.0.2.60;proto=https;by=203.0.113.43")) must_== true
     }
 
-    "not redirect when on https" in withApplication("play.filters.redirectplain.enabled=true") { app =>
+    "create a proper redirect url" in {
+      val request = FakeRequest(GET, "/please/dont?remove=this&foo=bar").withHeaders("Host" -> "playframework.com")
+
+      RedirectPlainFilter.createHttpsRedirectUrl(request) must_== "https://playframework.com/please/dont?remove=this&foo=bar"
+    }
+
+    "redirect when not on https including the path and url query parameters" in withApplication("") { app =>
+      val req = request(app, "http", "/please/dont?remove=this&foo=bar")
+
+      status(req) must_== PERMANENT_REDIRECT
+      header(LOCATION, req) must_== Some("https://playframework.com/please/dont?remove=this&foo=bar")
+    }
+
+    "not redirect when on https" in withApplication("") { app =>
       status(request(app, "https")) must_== OK
     }
 
-    "add strict transport security headers on https" in withApplication(
-      """
-        |play.filters.redirectplain.enabled=true
-        |play.filters.redirectplain.strict-transport-security.max-age=12345
-      """.stripMargin) { app =>
+    "add strict transport security headers on https" in withApplication("play.filters.redirectplain.strict-transport-security.max-age=12345") { app =>
       header(STRICT_TRANSPORT_SECURITY, request(app, "https")) must_== Some ("max-age=12345")
     }
   }
