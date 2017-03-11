2d48b62802ced50a2dc1e9bbd594446b212626f0
Will Sargent <wsargent@users.noreply.github.com>
2017-03-11 07:10:23 -0800

    Add LevelChangePropagator (#6959)

--- framework/src/play-logback/src/main/scala/play/api/libs/logback/LogbackLoggerConfigurator.scala
+++ framework/src/play-logback/src/main/scala/play/api/libs/logback/LogbackLoggerConfigurator.scala
@@ -8,6 +8,7 @@ import java.net.URL
 
 import ch.qos.logback.classic._
 import ch.qos.logback.classic.joran._
+import ch.qos.logback.classic.jul.LevelChangePropagator
 import ch.qos.logback.core.util._
 import org.slf4j.ILoggerFactory
 import org.slf4j.bridge._
@@ -65,18 +66,34 @@ class LogbackLoggerConfigurator extends LoggerConfigurator {
   def configure(properties: Map[String, String], config: Option[URL]): Unit = synchronized {
     // Redirect JUL -> SL4FJ
 
-    Option(java.util.logging.Logger.getLogger("")).map { root =>
-      root.setLevel(java.util.logging.Level.FINEST)
-      root.getHandlers.foreach(root.removeHandler)
-    }
-
-    SLF4JBridgeHandler.install()
+    // Remove existing handlers from JUL
+    SLF4JBridgeHandler.removeHandlersForRootLogger()
 
     // Configure logback
-
     val ctx = loggerFactory.asInstanceOf[LoggerContext]
+
     val configurator = new JoranConfigurator
     configurator.setContext(ctx)
+
+    // Set a level change propagator to minimize the overhead of JUL
+    //
+    // Please note that translating a java.util.logging event into SLF4J incurs the
+    // cost of constructing LogRecord instance regardless of whether the SLF4J logger
+    // is disabled for the given level. Consequently, j.u.l. to SLF4J translation can
+    // seriously increase the cost of disabled logging statements (60 fold or 6000%
+    // increase) and measurably impact the performance of enabled log statements
+    // (20% overall increase). Please note that as of logback-version 0.9.25,
+    // it is possible to completely eliminate the 60 fold translation overhead for
+    // disabled log statements with the help of LevelChangePropagator.
+    //
+    // https://www.slf4j.org/api/org/slf4j/bridge/SLF4JBridgeHandler.html
+    // https://logback.qos.ch/manual/configuration.html#LevelChangePropagator
+    val levelChangePropagator = new LevelChangePropagator()
+    levelChangePropagator.setContext(ctx)
+    levelChangePropagator.setResetJUL(true)
+    ctx.addListener(levelChangePropagator)
+    SLF4JBridgeHandler.install()
+
     ctx.reset()
 
     // Ensure that play.Logger and play.api.Logger are ignored when detecting file name and line number for
