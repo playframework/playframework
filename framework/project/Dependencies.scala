e1c753870abf57ccbd9aa8b12eeceeb4f28eb3c0
Will Sargent <will.sargent@gmail.com>
2017-03-14 15:05:25 -0700

    Try using transition version from binaryVersion

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -161,8 +161,8 @@ object Dependencies {
 
   private def sbtPluginDep(sbtVersion: String, scalaVersion: String, moduleId: ModuleID) = {
     moduleId.extra(
-      "sbtVersion" -> CrossVersion.binarySbtVersion(sbtVersion),
-      "scalaVersion" -> CrossVersion.TransitionScalaVersion
+      "sbtVersion" -> CrossVersion.binarySbtVersion(sbtVersion, CrossVersion.TransitionSbtVersion),
+      "scalaVersion" -> CrossVersion.binaryVersion(scalaVersion, CrossVersion.TransitionSbtVersion) 
     )
   }
 
