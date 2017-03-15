ce6969b1664c2b9643c3286b9687924383192f1d
Greg Methvin <greg.methvin@gmail.com>
2017-03-15 09:35:37 -0500

    Make play.Environment injectable (#7080)

--- framework/src/play-java/src/main/java/play/inject/BuiltInModule.java
+++ framework/src/play-java/src/main/java/play/inject/BuiltInModule.java
@@ -17,11 +17,12 @@ public class BuiltInModule extends play.api.inject.Module {
     @Override
     public Seq<Binding<?>> bindings(Environment environment, Configuration configuration) {
         return seq(
-          bind(ApplicationLifecycle.class).to(DelegateApplicationLifecycle.class),
-          bind(play.Configuration.class).toProvider(ConfigurationProvider.class),
-          bind(ConstraintValidatorFactory.class).to(DefaultConstraintValidatorFactory.class),
-          bind(Validator.class).toProvider(ValidatorProvider.class),
-          bind(Crypto.class).toSelf()
+            bind(ApplicationLifecycle.class).to(DelegateApplicationLifecycle.class),
+            bind(play.Environment.class).toSelf(),
+            bind(play.Configuration.class).toProvider(ConfigurationProvider.class),
+            bind(ConstraintValidatorFactory.class).to(DefaultConstraintValidatorFactory.class),
+            bind(Validator.class).toProvider(ValidatorProvider.class),
+            bind(Crypto.class).toSelf()
         );
     }
 }
