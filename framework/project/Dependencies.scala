193b0a452f240b6d3db2bdaeb2cfa5e7e5a49c51
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-03-20 17:33:27 +0200

    Update Akka HTTP to 10.0.5

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -7,7 +7,7 @@ import buildinfo.BuildInfo
 object Dependencies {
 
   val akkaVersion = "2.5.0-RC1"
-  val akkaHttpVersion = "10.0.4"
+  val akkaHttpVersion = "10.0.5"
   val playJsonVersion = "2.6.0-M4"
 
   val specsVersion = "3.8.8"
