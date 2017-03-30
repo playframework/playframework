98e34c0e9547edf9558f5ab8d99b2a163874b86a
Marcos Pereira <marcos.silva@gmail.com>
2017-03-30 00:14:31 -0300

    Add tests for Java test Helpers

---
+++ framework/src/play-test/src/test/java/play/test/WithApplicationOverrideTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2009-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.test;
+
+import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests WithApplication functionality.
+ */
+public class WithApplicationOverrideTest extends WithApplication {
+
+    @Override
+    protected Application provideApplication() {
+        return new GuiceApplicationBuilder()
+                .configure("extraConfig", "valueForExtraConfig")
+                .build();
+    }
+
+    @Test
+    public void shouldHaveAnAppInstantiated() {
+        assertNotNull(app);
+    }
+
+    @Test
+    public void shouldHaveAMaterializerInstantiated() {
+        assertNotNull(mat);
+    }
+
+    @Test
+    public void shouldHaveExtraConfiguration() {
+        assertThat(app.config().getString("extraConfig"), equalTo("valueForExtraConfig"));
+    }
+}
