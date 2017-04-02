e48b8dadb2b4184eb5bfc5f99a863a047ea10292
Marcos Pereira <marcos.silva@gmail.com>
2017-04-02 01:34:58 -0300

    Add openid Java components

---
+++ framework/src/play-openid/src/main/java/play/libs/openid/OpenIdComponents.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2009-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.libs.openid;
+
+/**
+ * OpenID Java components.
+ */
+public interface OpenIdComponents {
+    // TODO-MARCOS implement this
+}
