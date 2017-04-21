79fb84a6da2d909864e50a6fe304969688dde307
James Roper <james@jazzy.id.au>
2017-04-21 15:45:45 +1000

    Revert "Remove JNDI test (#6736)"
    This reverts commit 4e67bd27dbf31d1a12826a7e1e26719704ecf7d8.
    
    See #7262.

---
+++ framework/src/play/src/main/scala/play/api/libs/JNDI.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2009-2016 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.api.libs
+
+import play.api._
+
+import javax.naming._
+import javax.naming.Context._
+
+/**
+ * JNDI Helpers.
+ */
+object JNDI {
+
+  private val IN_MEMORY_JNDI = "tyrex.naming.MemoryContextFactory"
+  private val IN_MEMORY_URL = "/"
+
+  /**
+   * An in memory JNDI implementation.
+   */
+  lazy val initialContext = {
+
+    val env = new java.util.Hashtable[String, String]
+
+    env.put(INITIAL_CONTEXT_FACTORY, {
+      Play.privateMaybeApplication.flatMap(_.configuration.getOptional[String](INITIAL_CONTEXT_FACTORY)).getOrElse {
+        System.setProperty(INITIAL_CONTEXT_FACTORY, IN_MEMORY_JNDI)
+        IN_MEMORY_JNDI
+      }
+    })
+
+    env.put(PROVIDER_URL, {
+      Play.privateMaybeApplication.flatMap(_.configuration.getOptional[String](PROVIDER_URL)).getOrElse {
+        System.setProperty(PROVIDER_URL, IN_MEMORY_URL)
+        IN_MEMORY_URL
+      }
+    })
+
+    new InitialContext(env)
+
+  }
+
+}
