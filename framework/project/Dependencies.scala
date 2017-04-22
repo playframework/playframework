5abab175f1f466569af8641d1fd38ab8bfa06853
Greg Methvin <greg@methvin.net>
2017-04-21 20:26:28 -0700

    Update to play-json 2.6.0-M7

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
   val akkaVersion = "2.5.0"
   val akkaHttpVersion = "10.0.5"
-  val playJsonVersion = "2.6.0-M6"
+  val playJsonVersion = "2.6.0-M7"
 
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
