c62c987c2f5f4735ed7d66e32896c29e415189fd
Marcos Pereira <marcos.silva@gmail.com>
2017-04-24 15:01:23 -0300

    Provide some components as singletons
    According to if they are annotated with @Singleton or not

--- framework/src/play/src/main/java/play/BuiltInComponentsFromContext.java
+++ framework/src/play/src/main/java/play/BuiltInComponentsFromContext.java
@@ -3,13 +3,20 @@
  */
 package play;
 
+import akka.actor.ActorSystem;
 import com.typesafe.config.Config;
 import play.api.inject.NewInstanceInjector$;
 import play.api.inject.SimpleInjector;
 import play.core.SourceMapper;
+import play.http.HttpRequestHandler;
+import play.i18n.Langs;
 import play.inject.ApplicationLifecycle;
 import play.inject.DelegateInjector;
 import play.inject.Injector;
+import play.libs.Files;
+import play.libs.crypto.CSRFTokenSigner;
+import play.libs.crypto.CookieSigner;
+import play.mvc.FileMimeTypes;
 import scala.collection.immutable.Map$;
 
 import java.util.Optional;
@@ -21,8 +28,31 @@ public abstract class BuiltInComponentsFromContext implements BuiltInComponents 
 
     private final ApplicationLoader.Context context;
 
+    // Class instances to emulate singleton behavior
+    private final Application _application;
+    private final Langs _langs;
+    private final FileMimeTypes _fileMimeTypes;
+    private final HttpRequestHandler _httpRequestHandler;
+    private final ActorSystem _actorSystem;
+    private final CookieSigner _cookieSigner;
+    private final CSRFTokenSigner _csrfTokenSigner;
+    private final Files.TemporaryFileCreator _tempFileCreator;
+
+    private final Injector _injector;
+
     public BuiltInComponentsFromContext(ApplicationLoader.Context context) {
         this.context = context;
+
+        this._application = application();
+        this._langs = langs();
+        this._fileMimeTypes = fileMimeTypes();
+        this._httpRequestHandler = httpRequestHandler();
+        this._actorSystem = actorSystem();
+        this._cookieSigner = cookieSigner();
+        this._csrfTokenSigner = csrfTokenSigner();
+        this._tempFileCreator = tempFileCreator();
+
+        this._injector = createInjector();
     }
 
     @Override
@@ -46,7 +76,50 @@ public abstract class BuiltInComponentsFromContext implements BuiltInComponents 
     }
 
     @Override
+    public Application application() {
+        return this._application;
+    }
+    @Override
+    public Langs langs() {
+        return this._langs;
+    }
+
+    @Override
+    public FileMimeTypes fileMimeTypes() {
+        return this._fileMimeTypes;
+    }
+
+    @Override
+    public HttpRequestHandler httpRequestHandler() {
+        return this._httpRequestHandler;
+    }
+
+    @Override
+    public ActorSystem actorSystem() {
+        return this._actorSystem;
+    }
+
+    @Override
+    public CookieSigner cookieSigner() {
+        return this._cookieSigner;
+    }
+
+    @Override
+    public CSRFTokenSigner csrfTokenSigner() {
+        return this._csrfTokenSigner;
+    }
+
+    @Override
+    public Files.TemporaryFileCreator tempFileCreator() {
+        return this._tempFileCreator;
+    }
+
+    @Override
     public Injector injector() {
+        return this._injector;
+    }
+
+    private Injector createInjector() {
         // TODO do we need to register components like the scala version?
         SimpleInjector scalaInjector = new SimpleInjector(NewInstanceInjector$.MODULE$, Map$.MODULE$.empty());
         return new DelegateInjector(scalaInjector);
