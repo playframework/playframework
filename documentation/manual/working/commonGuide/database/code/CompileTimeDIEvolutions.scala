f99197060a09481f29606623f34bacf2e57fe700
Mark Siebert <mark.jiali.siebert@gmail.com>
2017-05-29 09:24:07 -0600

    improve documentation for running evolutions using compile-time DI (#7173)

---
+++ documentation/manual/working/commonGuide/database/code/CompileTimeDIEvolutions.scala
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2009-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+//#compile-time-di-evolutions
+import play.api.ApplicationLoader.Context
+import play.api.BuiltInComponentsFromContext
+import play.api.db.{Database, DBComponents, HikariCPComponents}
+import play.api.db.evolutions.EvolutionsComponents
+import play.api.routing.Router
+import play.filters.HttpFiltersComponents
+
+class AppComponents(cntx: Context)
+  extends BuiltInComponentsFromContext(cntx)
+  with DBComponents
+  with EvolutionsComponents
+  with HikariCPComponents
+  with HttpFiltersComponents
+{
+  // this will actually run the database migrations on startup
+  applicationEvolutions
+
+  //###skip: 1
+  val router = Router.empty
+}
+//#compile-time-di-evolutions
