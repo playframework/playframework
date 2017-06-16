8f62522e2f1f0527a24c824173c124bf481cbda0
Marcos Pereira <marcos.silva@gmail.com>
2017-06-16 18:09:37 -0300

    Improve Java app loader/provider (#7300)
    * Improve Java app loader/provider
    
    While this API is internal and wouldn’t be used by
    users creating application, it could be useful to people
    integrating with Play.
    
    * Fix test

--- framework/src/play/src/main/java/play/server/ApplicationProvider.java
+++ framework/src/play/src/main/java/play/server/ApplicationProvider.java
@@ -4,7 +4,11 @@
 package play.server;
 
 import play.Application;
+import play.mvc.Http;
+import play.mvc.Result;
+import scala.compat.java8.OptionConverters;
 
+import java.util.Optional;
 
 /**
  * Provides information about a Play Application running inside a Play server.
@@ -12,13 +16,43 @@ import play.Application;
 public class ApplicationProvider {
 
     private final Application application;
+    private final play.core.ApplicationProvider underlying;
 
     public ApplicationProvider(Application application) {
         this.application = application;
+        this.underlying = play.core.ApplicationProvider$.MODULE$.apply(application.asScala());
     }
 
+    /**
+     * @return The Scala version of this application provider.
+     */
+    public play.core.ApplicationProvider asScala() {
+        return underlying;
+    }
+
+    /**
+     * @return the application.
+     *
+     * @deprecated Deprecated as of 2.6.0. Use {@link #get()} instead.
+     */
     public Application getApplication() {
         return application;
     }
 
+    /**
+     * @return Returns an Optional with the application, if available.
+     */
+    public Optional<Application> get() {
+        return Optional.ofNullable(application);
+    }
+
+    /**
+     * Handle a request directly, without using the application.
+     * @param requestHeader the request made.
+     */
+    public Optional<Result> handleWebCommand(Http.RequestHeader requestHeader) {
+        return OptionConverters
+                .toJava(this.underlying.handleWebCommand(requestHeader.asScala()))
+                .map(play.api.mvc.Result::asJava);
+    }
 }
