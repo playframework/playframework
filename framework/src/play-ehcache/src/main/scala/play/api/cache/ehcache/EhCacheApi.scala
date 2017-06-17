9d01114045b790c6bc8079f48824d2b68935c7a3
arpitkulria <arpit.kulria@gmail.com>
2017-06-17 17:44:31 +0530

    Formatted code

--- framework/src/play-ehcache/src/main/scala/play/api/cache/ehcache/EhCacheApi.scala
+++ framework/src/play-ehcache/src/main/scala/play/api/cache/ehcache/EhCacheApi.scala
@@ -198,13 +198,12 @@ class EhCacheApi @Inject() (private[ehcache] val cache: Ehcache)(implicit contex
   }
 
   def remove(key: String): Future[Done] = Future.successful {
-      cache.remove(key)
-      Done
+    cache.remove(key)
+    Done
   }
 
   def removeAll(): Future[Done] = Future {
-      cache.removeAll()
-      Done
+    cache.removeAll()
+    Done
   }
 }
-
