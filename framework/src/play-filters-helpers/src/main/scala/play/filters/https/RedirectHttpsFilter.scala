aa138f840075a13e457eea3b6e51cd4ab8721bf5
Greg Methvin <greg.methvin@gmail.com>
2017-06-26 12:18:44 -0700

    Improvements to RedirectHttpsFilter (#7499)

--- framework/src/play-filters-helpers/src/main/scala/play/filters/https/RedirectHttpsFilter.scala
+++ framework/src/play-filters-helpers/src/main/scala/play/filters/https/RedirectHttpsFilter.scala
@@ -10,6 +10,7 @@ import play.api.http.Status._
 import play.api.inject.{ SimpleModule, bind }
 import play.api.mvc._
 import play.api.{ Configuration, Environment, Mode }
+import play.api.Logger
 
 /**
  * A filter that redirects HTTP requests to https requests.
@@ -21,33 +22,38 @@ import play.api.{ Configuration, Environment, Mode }
  * https://www.playframework.com/documentation/latest/RedirectHttpsFilter
  */
 @Singleton
-class RedirectHttpsFilter @Inject() (config: RedirectHttpsConfiguration)
-    extends EssentialFilter {
+class RedirectHttpsFilter @Inject() (config: RedirectHttpsConfiguration) extends EssentialFilter {
+
+  import RedirectHttpsKeys._
+  import config._
+
+  private val logger = Logger(getClass)
+
+  private[this] lazy val stsHeaders = {
+    if (!redirectEnabled) Seq.empty
+    else strictTransportSecurity.toSeq.map(STRICT_TRANSPORT_SECURITY -> _)
+  }
 
   override def apply(next: EssentialAction): EssentialAction = EssentialAction { req =>
     import play.api.libs.streams.Accumulator
     import play.core.Execution.Implicits.trampoline
     if (req.secure) {
-      next(req).map { result =>
-        config.strictTransportSecurity match {
-          case Some(sts) if config.hstsEnabled =>
-            result.withHeaders(STRICT_TRANSPORT_SECURITY -> sts)
-          case other =>
-            result
-        }
-      }
+      next(req).map(_.withHeaders(stsHeaders: _*))
+    } else if (redirectEnabled) {
+      Accumulator.done(Results.Redirect(createHttpsRedirectUrl(req), redirectStatusCode))
     } else {
-      Accumulator.done(Results.Redirect(createHttpsRedirectUrl(req), config.redirectStatusCode))
+      logger.info(s"Not redirecting to HTTPS because $redirectEnabledPath flag is not set.")
+      next(req)
     }
   }
 
   protected def createHttpsRedirectUrl(req: RequestHeader): String = {
-    config.sslPort match {
-      case None =>
-        s"https://${req.domain}${req.uri}"
-
+    import req.{ domain, uri }
+    sslPort match {
+      case None | Some(443) =>
+        s"https://$domain$uri"
       case Some(port) =>
-        s"https://${req.domain}:${port}${req.uri}"
+        s"https://$domain:$port$uri"
     }
   }
 }
@@ -56,25 +62,41 @@ case class RedirectHttpsConfiguration(
   strictTransportSecurity: Option[String] = Some("max-age=31536000; includeSubDomains"),
   redirectStatusCode: Int = PERMANENT_REDIRECT,
   sslPort: Option[Int] = None, // should match up to ServerConfig.sslPort
-  hstsEnabled: Boolean = false
+  redirectEnabled: Boolean = true
 )
+
+private object RedirectHttpsKeys {
+  val stsPath = "play.filters.https.strictTransportSecurity"
+  val statusCodePath = "play.filters.https.redirectStatusCode"
+  val portPath = "play.filters.https.port"
+  val redirectEnabledPath = "play.filters.https.redirectEnabled"
+}
 
 @Singleton
 class RedirectHttpsConfigurationProvider @Inject() (c: Configuration, e: Environment)
     extends Provider[RedirectHttpsConfiguration] {
-  private val stsPath = "play.filters.https.strictTransportSecurity"
-  private val statusCodePath = "play.filters.https.redirectStatusCode"
-  private val portPath = "play.filters.https.port"
+  import RedirectHttpsKeys._
+
+  private val logger = Logger(getClass)
 
   lazy val get: RedirectHttpsConfiguration = {
-    val strictTransportSecurityMaxAge = c.get[Option[String]](stsPath)
+    val strictTransportSecurity = c.get[Option[String]](stsPath)
     val redirectStatusCode = c.get[Int](statusCodePath)
     if (!isRedirect(redirectStatusCode)) {
       throw c.reportError(statusCodePath, s"Status Code $redirectStatusCode is not a Redirect status code!")
     }
-    val port = c.getOptional[Int](portPath)
+    val port = c.get[Option[Int]](portPath)
+    val redirectEnabled = c.get[Option[Boolean]](redirectEnabledPath).getOrElse {
+      if (e.mode != Mode.Prod) {
+        logger.info(
+          s"RedirectHttpsFilter is disabled by default except in Prod mode.\n" +
+            s"See https://www.playframework.com/documentation/2.6.x/RedirectHttpsFilter"
+        )
+      }
+      e.mode == Mode.Prod
+    }
 
-    RedirectHttpsConfiguration(strictTransportSecurityMaxAge, redirectStatusCode, port, e.mode == Mode.Prod)
+    RedirectHttpsConfiguration(strictTransportSecurity, redirectStatusCode, port, redirectEnabled)
   }
 }
 
@@ -90,6 +112,8 @@ trait RedirectHttpsComponents {
   def configuration: Configuration
   def environment: Environment
 
-  lazy val redirectHttpsConfiguration: RedirectHttpsConfiguration = new RedirectHttpsConfigurationProvider(configuration, environment).get
-  lazy val redirectHttpsFilter: RedirectHttpsFilter = new RedirectHttpsFilter(redirectHttpsConfiguration)
+  lazy val redirectHttpsConfiguration: RedirectHttpsConfiguration =
+    new RedirectHttpsConfigurationProvider(configuration, environment).get
+  lazy val redirectHttpsFilter: RedirectHttpsFilter =
+    new RedirectHttpsFilter(redirectHttpsConfiguration)
 }
