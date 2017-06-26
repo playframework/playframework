e6697ce8cb29e9b7a2e55bf81337934345873474
Greg Methvin <greg@methvin.net>
2017-06-26 12:44:05 -0700

    Add hstsEnabled for backwards compatibility

--- framework/src/play-filters-helpers/src/main/scala/play/filters/https/RedirectHttpsFilter.scala
+++ framework/src/play-filters-helpers/src/main/scala/play/filters/https/RedirectHttpsFilter.scala
@@ -59,11 +59,13 @@ class RedirectHttpsFilter @Inject() (config: RedirectHttpsConfiguration) extends
 }
 
 case class RedirectHttpsConfiguration(
-  strictTransportSecurity: Option[String] = Some("max-age=31536000; includeSubDomains"),
-  redirectStatusCode: Int = PERMANENT_REDIRECT,
-  sslPort: Option[Int] = None, // should match up to ServerConfig.sslPort
-  redirectEnabled: Boolean = true
-)
+    strictTransportSecurity: Option[String] = Some("max-age=31536000; includeSubDomains"),
+    redirectStatusCode: Int = PERMANENT_REDIRECT,
+    sslPort: Option[Int] = None, // should match up to ServerConfig.sslPort
+    redirectEnabled: Boolean = true
+) {
+  def hstsEnabled: Boolean = redirectEnabled && strictTransportSecurity.isDefined
+}
 
 private object RedirectHttpsKeys {
   val stsPath = "play.filters.https.strictTransportSecurity"
