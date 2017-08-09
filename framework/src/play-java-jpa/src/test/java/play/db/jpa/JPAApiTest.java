150511ccf43ea11600dc29fe14ed10fb7d4c1a71
Rich Dougherty <rich@rd.gen.nz>
2017-08-10 09:09:32 +1200

    Configure JPA at "jpa"; make this configurable (#7711)
    This restores the previous default JPA location back
    from "play.jpa" to "jpa", but makes the location
    configurable. This means that users won't have to
    change their JPA configuration when they update
    Play versions.
    
    The configuration location is at "play.jpa.config",
    which is similar to "play.db.config". This config
    location is advanced usage and I've followed the
    lead of "play.db.config" and not documented this
    anywhere except in reference.conf.
    
    For reference, the previous behavior was introduced
    very recently in PRs #7681 and #7698.

--- framework/src/play-java-jpa/src/test/java/play/db/jpa/JPAApiTest.java
+++ framework/src/play-java-jpa/src/test/java/play/db/jpa/JPAApiTest.java
@@ -3,52 +3,85 @@
  */
 package play.db.jpa;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExternalResource;
 import play.db.Database;
 import play.db.Databases;
-
-import org.junit.Test;
 import play.db.jpa.DefaultJPAConfig.JPAConfigProvider;
 
 import javax.persistence.EntityManager;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class JPAApiTest {
 
     @Rule
     public TestDatabase db = new TestDatabase();
 
+    private Set<String> getConfiguredPersistenceUnitNames(String configString) {
+        Config overrides = ConfigFactory.parseString(configString);
+        Config config = overrides.withFallback(ConfigFactory.load());
+        return new JPAConfigProvider(config).get().persistenceUnits().stream()
+                .map(unit -> unit.unitName).collect(Collectors.toSet());
+    }
+
+
     @Test
     public void shouldWorkWithEmptyConfiguration() {
-        Config config = ConfigFactory.load();
-        assertThat(new JPAConfigProvider(config).get().persistenceUnits(), notNullValue());
+        String configString = "";
+        Set<String> unitNames = getConfiguredPersistenceUnitNames(configString);
+        assertThat(unitNames, equalTo(Collections.emptySet()));
     }
 
     @Test
-    public void shouldWorkWithLegacyConfiguration() {
-        Config overrides = ConfigFactory.parseString("jpa.default = defaultPersistenceUnit");
-        Config config = overrides.withFallback(ConfigFactory.load());
-        List<String> unitNames = new JPAConfigProvider(config).get().persistenceUnits().stream()
-            .map(unit -> unit.unitName).collect(Collectors.toList());
-        assertThat(unitNames, equalTo(Arrays.asList("defaultPersistenceUnit")));
+    public void shouldWorkWithSingleValue() {
+        String configString = "jpa.default = defaultPersistenceUnit";
+        Set<String> unitNames = getConfiguredPersistenceUnitNames(configString);
+        assertThat(unitNames, equalTo(new HashSet(Arrays.asList("defaultPersistenceUnit"))));
     }
 
     @Test
-    public void shouldWorkWithPlayConfiguration() {
-        Config overrides = ConfigFactory.parseString("play.jpa.default = defaultPersistenceUnit");
-        Config config = overrides.withFallback(ConfigFactory.load());
-        List<String> unitNames = new JPAConfigProvider(config).get().persistenceUnits().stream()
-            .map(unit -> unit.unitName).collect(Collectors.toList());
-        assertThat(unitNames, equalTo(Arrays.asList("defaultPersistenceUnit")));
+    public void shouldWorkWithMultipleValues() {
+        String configString =
+                "jpa.default = defaultPersistenceUnit\n" +
+                "jpa.number2 = number2Unit";
+        Set<String> unitNames = getConfiguredPersistenceUnitNames(configString);
+        assertThat(unitNames, equalTo(new HashSet(Arrays.asList("defaultPersistenceUnit", "number2Unit"))));
+    }
+
+    @Test
+    public void shouldWorkWithEmptyConfigurationAtConfiguredLocation() {
+        String configString = "play.jpa.config = myconfig.jpa";
+        Set<String> unitNames = getConfiguredPersistenceUnitNames(configString);
+        assertThat(unitNames, equalTo(Collections.emptySet()));
+    }
+
+    @Test
+    public void shouldWorkWithSingleValueAtConfiguredLocation() {
+        String configString =
+                "play.jpa.config = myconfig.jpa\n" +
+                "myconfig.jpa.default = defaultPersistenceUnit";
+        Set<String> unitNames = getConfiguredPersistenceUnitNames(configString);
+        assertThat(unitNames, equalTo(new HashSet(Arrays.asList("defaultPersistenceUnit"))));
+    }
+
+    @Test
+    public void shouldWorkWithMultipleValuesAtConfiguredLocation() {
+        String configString =
+                "play.jpa.config = myconfig.jpa\n" +
+                "myconfig.jpa.default = defaultPersistenceUnit\n" +
+                "myconfig.jpa.number2 = number2Unit";
+        Set<String> unitNames = getConfiguredPersistenceUnitNames(configString);
+        assertThat(unitNames, equalTo(new HashSet(Arrays.asList("defaultPersistenceUnit", "number2Unit"))));
     }
 
     @Test
