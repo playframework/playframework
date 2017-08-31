a2ae2b3100ed7a410231d0e206b6648910858d22
Greg Methvin <greg@methvin.net>
2017-08-30 21:21:44 -0700

    Update play-ws-standalone to 1.0.7

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -265,7 +265,7 @@ object Dependencies {
   ) ++ jcacheApi
 
   val caffeineVersion = "2.5.3"
-  val playWsStandaloneVersion = "1.0.4"
+  val playWsStandaloneVersion = "1.0.7"
   val playWsDeps = Seq(
     "com.typesafe.play" %% "play-ws-standalone" % playWsStandaloneVersion,
     "com.typesafe.play" %% "play-ws-standalone-xml" % playWsStandaloneVersion,
