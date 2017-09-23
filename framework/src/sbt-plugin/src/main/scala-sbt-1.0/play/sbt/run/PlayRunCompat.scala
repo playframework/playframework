e3ccbffcae81296ab94260cf72062aab3f8cc4bb
Marcos Pereira <marcos.silva@gmail.com>
2017-09-23 16:59:25 -0300

    Fix last PlayRun compatibility problem

--- framework/src/sbt-plugin/src/main/scala-sbt-1.0/play/sbt/run/PlayRunCompat.scala
+++ framework/src/sbt-plugin/src/main/scala-sbt-1.0/play/sbt/run/PlayRunCompat.scala
@@ -4,6 +4,8 @@
 package play.sbt.run
 
 import sbt.{ State, Watched }
+import sbt.internal.io.PlaySource
+
 import play.dev.filewatch.SourceModificationWatch
 
 import scala.sys.process._
@@ -28,7 +30,14 @@ private[run] trait PlayRunCompat {
     // A possible current approach here is to copy code from sbt and make it accessible
     // to Play (maybe using play-file-watch), but it is possible that we need to
     // copy a good amount of code just to have access to a "simple" method.
-    Seq.empty[better.files.File].toIterator
+    //
+    // So there is now PlaySource which is class declared inside sbt.internal.io
+    // to make it possible to access sbt.internal.io.Source methods.
+    watched.watchSources(state)
+      .map(source => new PlaySource(source))
+      .flatMap(_.getFiles)
+      .map(f => better.files.File(f.toPath))
+      .toIterator
   }
 
   def kill(pid: String) = s"kill $pid".!
