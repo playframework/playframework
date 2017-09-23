ab4946d61862147160f670f29338ad1189c44eeb
Marcos Pereira <marcos.silva@gmail.com>
2017-09-23 16:59:49 -0300

    Clearer TODO explanation

--- framework/src/sbt-plugin/src/main/scala-sbt-1.0/play/sbt/run/PlayReload.scala
+++ framework/src/sbt-plugin/src/main/scala-sbt-1.0/play/sbt/run/PlayReload.scala
@@ -59,21 +59,21 @@ object PlayReload {
     analysis.apis.internal.foldLeft(Map.empty[String, Source]) {
       case (sourceMap, (sourceClassName, analyzedClass)) =>
         sourceMap // ++ { what? }
-        // TODO: sbt 1.0
-        // What is the new API now?
-        // - For sbt 0.13 analysis.api.internal is a Map[java.io.File, xsbti.api.Source] where
-        //   Source has api: xsbti.api.SourceAPI which has definitions: xsbti.api.Definition[]
-        //   and finally definition has a name. We can then use the java.io.File
-        // - For sbt 1.0 analysis.api.internal is a Map[String, xsbti.api.AnalyzedClass], so we
-        //   don't have a java.io.File and access to the definitions can (should?) be done
-        //   via:
-        //
-        //     analysis -> api -> internal -> analyzedClass -> companions -> classApi   -> structure ->  declared
-        //                                                              \                            `-> inherited
-        //                                                               `-> objectApi  -> structure ->  declared
-        //                                                                                           `-> inherited
-        //
-        // But it this the right path to access this information if there is any?
+      // TODO: sbt 1.0
+      // What is the new API now?
+      // - For sbt 0.13 analysis.api.internal is a Map[java.io.File, xsbti.api.Source] where
+      //   Source has api: xsbti.api.SourceAPI which has definitions: xsbti.api.Definition[]
+      //   and finally definition has a name. We can then use the java.io.File
+      // - For sbt 1.0 analysis.api.internal is a Map[String, xsbti.api.AnalyzedClass], so we
+      //   don't have a java.io.File and access to the definitions can (should?) be done
+      //   via:
+      //
+      //     analysis -> api -> internal -> analyzedClass -> companions -> classApi   -> structure ->  declared
+      //                                                              \                            `-> inherited
+      //                                                               `-> objectApi  -> structure ->  declared
+      //                                                                                           `-> inherited
+      //
+      // But it this the right path to access this information if there is any?
     }
   }
 
