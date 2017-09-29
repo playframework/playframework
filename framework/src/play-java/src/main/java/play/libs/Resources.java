8cb2b258633afa4bf80270a77329caa6397655f7
Greg Methvin <greg.methvin@gmail.com>
2017-09-29 10:04:58 -0700

    Use whenComplete properly for asyncTryWithResources (#7862)

--- framework/src/play-java/src/main/java/play/libs/Resources.java
+++ framework/src/play-java/src/main/java/play/libs/Resources.java
@@ -17,11 +17,7 @@ public class Resources {
     ) {
         try {
             CompletionStage<U> completionStage = body.apply(resource);
-            // Do not use whenCompleteAsync, because it happens in an async thread --
-            // if this gets an exception, it will return the exception and also run the
-            // thread, which can result in the test completing before the close() happens.
-            completionStage.whenComplete((u, throwable) -> tryCloseResource(resource));
-            return completionStage;
+            return completionStage.whenComplete((u, throwable) -> tryCloseResource(resource));
         } catch (RuntimeException e) {
             tryCloseResource(resource);
             throw e;
