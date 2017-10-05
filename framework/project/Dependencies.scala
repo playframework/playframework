672c356ae792dce592da07ec47f1138bea7d9805
Marcos Pereira <marcos.silva@gmail.com>
2017-10-05 16:29:29 -0300

    Update play-ws to version 1.1.2
    See https://github.com/playframework/play-ws/milestone/2?closed=1
    for a list of issues fixed in play-ws 1.1.2.

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -263,7 +263,7 @@ object Dependencies {
   ) ++ jcacheApi
 
   val caffeineVersion = "2.5.6"
-  val playWsStandaloneVersion = "1.1.1"
+  val playWsStandaloneVersion = "1.1.2"
   val playWsDeps = Seq(
     "com.typesafe.play" %% "play-ws-standalone" % playWsStandaloneVersion,
     "com.typesafe.play" %% "play-ws-standalone-xml" % playWsStandaloneVersion,
