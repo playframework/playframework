ee2183dca6ac06c2411f36233e7948320140c14d
dusanstanojeviccs <dusan.stanojevic.cs@gmail.com>
2017-11-21 20:18:49 -0800

    removed spec from the config name

--- framework/src/play-caffeine-cache/src/main/java/play/cache/caffeine/CaffeineConfigConstants.java
+++ framework/src/play-caffeine-cache/src/main/java/play/cache/caffeine/CaffeineConfigConstants.java
@@ -4,7 +4,7 @@
 package play.cache.caffeine;
 
 public class CaffeineConfigConstants {
-    public static final String PLAY_CACHE_CAFFEINE_SPEC = "play.cache.caffeine.spec";
+    public static final String PLAY_CACHE_CAFFEINE_SPEC = "play.cache.caffeine";
     public static final String PLAY_CACHE_DISPATCHER = "play.cache.dispatcher";
     public static final String PLAY_CACHE_DEFAULT_CACHE = "play.cache.defaultCache";
     public static final String PLAY_CACHE_BIND_CACHES = "play.cache.bindCaches";
