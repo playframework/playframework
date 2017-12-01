dcfaac390f2f0acd9005e6343e3cca3295612da1
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-12-01 18:52:29 +0100

    Tests ACtorSystemProvider's stophook invokes CS from apropriate phase

---
+++ framework/src/play/src/test/scala/play/api/libs/concurrent/ActorSystemProviderSpec.scala
@@ -0,0 +1,69 @@
+package play.api.libs.concurrent
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+import akka.Done
+import akka.actor.CoordinatedShutdown
+import com.typesafe.config.ConfigValueFactory
+import org.specs2.mutable.Specification
+import play.api.{ Configuration, Environment }
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, Future, Promise }
+import scala.util.Success
+
+class ActorSystemProviderSpec extends Specification {
+  val fiveSec = Duration(5, "seconds")
+  val oneSec = Duration(100, "milliseconds")
+
+  "ActorSystemProvider" should {
+
+    "run the CoordinatedShutdown from the configured phase system when the stopHook is run" in {
+
+      val mustRunPhase = CoordinatedShutdown.PhaseServiceStop
+      val mustNotRunPhase = CoordinatedShutdown.PhaseBeforeServiceUnbind
+
+      val config = Configuration
+        .load(Environment.simple())
+        .underlying
+        .withValue(
+          "play.akka.run-cs-from-phase",
+          ConfigValueFactory.fromAnyRef(mustRunPhase))
+
+      val (actorSystem, stopHook) = ActorSystemProvider.start(
+        this.getClass.getClassLoader,
+        Configuration(config)
+      )
+
+      val promise = Promise[Done]()
+      val terminated = promise.future
+      val isRun = new AtomicBoolean(false)
+
+      CoordinatedShutdown(actorSystem).addTask(mustRunPhase, "termination-promise") {
+        () =>
+          promise.complete(Success(Done))
+          Future.successful(Done)
+      }
+      CoordinatedShutdown(actorSystem).addTask(mustNotRunPhase, "is-ignored-promise") {
+        () =>
+          isRun.set(true)
+          Future.successful(Done)
+      }
+
+      try {
+        Await.result(terminated, oneSec)
+        failure
+      } catch {
+        case _: Throwable =>
+      }
+
+      import scala.concurrent.ExecutionContext.Implicits.global
+      val completeShutdown = stopHook().flatMap(_ => terminated)
+
+      Await.result(completeShutdown, fiveSec) must equalTo(Done)
+      isRun.get() must equalTo(false)
+    }
+
+  }
+
+}
