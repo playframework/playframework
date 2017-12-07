6c04cd98bc1e7ad8ecef5f0c05791a7260c4a7dd
Matthias Kurz <m.kurz@irregular.at>
2017-12-08 00:13:42 +0100

    Easy way to set server-header when using Netty (#8028)
    * Easy way to set server-header when using Netty
    
    * Satisfy MiMa
    
    * Fix Microbenchmarks
    
    * akka-http and netty server-header default to null
    
    * If not set, akka-http and netty server-header fallback to "play.server.server-header"
    
    * Use get[Option[String]]("server-header")

--- framework/src/play-netty-server/src/main/scala/play/core/server/NettyServer.scala
+++ framework/src/play-netty-server/src/main/scala/play/core/server/NettyServer.scala
@@ -49,6 +49,7 @@ class NettyServer(
 
   private val serverConfig = config.configuration.get[Configuration]("play.server")
   private val nettyConfig = serverConfig.get[Configuration]("netty")
+  private val serverHeader = nettyConfig.get[Option[String]]("server-header").collect { case s if s.nonEmpty => s }
   private val maxInitialLineLength = nettyConfig.get[Int]("maxInitialLineLength")
   private val maxHeaderSize = nettyConfig.get[Int]("maxHeaderSize")
   private val maxChunkSize = nettyConfig.get[Int]("maxChunkSize")
@@ -143,7 +144,7 @@ class NettyServer(
   /**
    * Create a new PlayRequestHandler.
    */
-  protected[this] def newRequestHandler(): ChannelInboundHandler = new PlayRequestHandler(this)
+  protected[this] def newRequestHandler(): ChannelInboundHandler = new PlayRequestHandler(this, serverHeader)
 
   /**
    * Create a sink for the incoming connection channels.
