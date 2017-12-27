0ff9771af35f1fe722b25828a94a70c873e1e74d
Matthias Kurz <m.kurz@irregular.at>
2017-12-27 22:09:02 +0100

    Upgrade jackson

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
 
   val specsSbt = specsBuild
 
-  val jacksonVersion = "2.8.10"
+  val jacksonVersion = "2.8.11"
   val jacksons = Seq(
     "com.fasterxml.jackson.core" % "jackson-core",
     "com.fasterxml.jackson.core" % "jackson-annotations",
