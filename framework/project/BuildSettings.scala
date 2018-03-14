5d381598acb4cd7750b85c7c465353e165afbe6b
Marcos Pereira <marcos.silva@gmail.com>
2018-03-14 16:14:32 -0300

    Put back link to Scala issue

--- framework/project/BuildSettings.scala
+++ framework/project/BuildSettings.scala
@@ -88,7 +88,7 @@ object BuildSettings {
         Resolver.typesafeIvyRepo("releases")
       ),
       scalacOptions in(Compile, doc) := {
-        // disable the new scaladoc feature for scala 2.12.0, might be removed in 2.12.0-1 ()
+        // disable the new scaladoc feature for scala 2.12.0, might be removed in 2.12.0-1 (https://github.com/scala/scala-dev/issues/249)
         CrossVersion.partialVersion(scalaVersion.value) match {
           case Some((2, v)) if v >= 12 => Seq("-no-java-comments")
           case _ => Seq()
