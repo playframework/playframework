a1bbecb4a3b31c67d1396c3d5a8506a613f8bcd1
Matthias Kurz <m.kurz@irregular.at>
2018-04-06 20:47:51 +0200

    Fixes

--- framework/src/play-cache/src/main/java/play/cache/DefaultAsyncCacheApi.java
+++ framework/src/play-cache/src/main/java/play/cache/DefaultAsyncCacheApi.java
@@ -37,36 +37,44 @@ public class DefaultAsyncCacheApi implements AsyncCacheApi {
         return new SyncCacheApiAdapter(asyncCacheApi.sync());
     }
 
-	@Deprecated
+    @Override
+    @Deprecated
     public <T> CompletionStage<T> get(String key) {
         return toJava(asyncCacheApi.get(key, Scala.<T>classTag())).thenApply(Scala::orNull);
     }
 
+    @Override
     public <T> CompletionStage<Optional<T>> getOptional(String key) {
         return toJava(asyncCacheApi.get(key, Scala.<T>classTag())).thenApply(OptionConverters::toJava);
     }
 
+    @Override
     public <T> CompletionStage<T> getOrElseUpdate(String key, Callable<CompletionStage<T>> block, int expiration) {
         return toJava(
             asyncCacheApi.getOrElseUpdate(key, intToDuration(expiration), Scala.asScalaWithFuture(block), Scala.<T>classTag()));
     }
 
+    @Override
     public <T> CompletionStage<T> getOrElseUpdate(String key, Callable<CompletionStage<T>> block) {
         return toJava(asyncCacheApi.getOrElseUpdate(key, Duration.Inf(), Scala.asScalaWithFuture(block), Scala.<T>classTag()));
     }
 
+    @Override
     public CompletionStage<Done> set(String key, Object value, int expiration) {
         return toJava(asyncCacheApi.set(key, value, intToDuration(expiration)));
     }
 
+    @Override
     public CompletionStage<Done> set(String key, Object value) {
         return toJava(asyncCacheApi.set(key, value, Duration.Inf()));
     }
 
+    @Override
     public CompletionStage<Done> remove(String key) {
         return toJava(asyncCacheApi.remove(key));
     }
 
+    @Override
     public CompletionStage<Done> removeAll() {
         return toJava(asyncCacheApi.removeAll());
     }
