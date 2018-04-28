577de00d3e802e03efafe8484c13b2fa8a1b2d8a
Shenker93 <BohdanLevitsky@gmail.com>
2018-04-28 13:44:02 +0300

    Tests for TestServer.java added

---
+++ framework/src/play-test/src/test/java/play/test/TestServerTest.java
@@ -0,0 +1,33 @@
+package play.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestServerTest {
+    @Test
+    public void shouldReturnHttpPort() {
+        int testServerPort = play.api.test.Helpers.testServerPort();
+        final TestServer testServer = Helpers.testServer(testServerPort);
+        testServer.start();
+        assertTrue("No value for http port", testServer.getRunningHttpPort().isPresent());
+        assertFalse("ssl port value is present, but was not set", testServer.getRunningHttpsPort().isPresent());
+        assertEquals(testServerPort, testServer.getRunningHttpPort().getAsInt());
+        testServer.stop();
+    }
+
+    @Test
+    public void shouldReturnHttpAndSslPorts() {
+        int port =  play.api.test.Helpers.testServerPort();
+        int sslPort =  port + 1;
+        final TestServer testServer = Helpers.testServer(port, sslPort);
+        testServer.start();
+        assertTrue("No value for ssl port", testServer.getRunningHttpsPort().isPresent());
+        assertEquals(sslPort, testServer.getRunningHttpsPort().getAsInt());
+        assertTrue("No value for http port", testServer.getRunningHttpPort().isPresent());
+        assertEquals(port, testServer.getRunningHttpPort().getAsInt());
+        testServer.stop();
+    }
+}
