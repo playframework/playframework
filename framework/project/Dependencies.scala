6b30c9c93e0e4168552582e4501b8b2bf9f19c52
Matthias Kurz <m.kurz@irregular.at>
2018-05-07 20:33:48 +0200

    Upgrade netty (#8389)

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -155,7 +155,7 @@ object Dependencies {
     specsBuild.map(_ % Test) ++
     javaTestDeps
 
-  val nettyVersion = "4.1.23.Final"
+  val nettyVersion = "4.1.24.Final"
 
   val netty = Seq(
     "com.typesafe.netty" % "netty-reactive-streams-http" % "2.0.0",
