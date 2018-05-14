dae5d4385c8e72824ead99573c47fbc4ade6dc4a
Rich Dougherty <rich@rd.gen.nz>
2018-05-14 21:36:42 +1200

    Use Guice to initialize database connection pools (#8410)
    Fixes #8402.
    
    We now pass the Injector value to the initialization code so that
    it can use Guice to load a value instead of always using the
    default constructor.
    
    Increases visibility of PooledDatabase.pool field so that we can
    unit test its value more easily.

--- framework/src/play-jdbc/src/test/scala/play/api/db/ConnectionPoolConfigSpec.scala
+++ framework/src/play-jdbc/src/test/scala/play/api/db/ConnectionPoolConfigSpec.scala
@@ -4,32 +4,58 @@
 
 package play.api.db
 
+import javax.inject._
+import play.api.Environment
 import play.api.test._
 
 class ConnectionPoolConfigSpec extends PlaySpecification {
 
   "DBModule bindings" should {
 
-    "use HikariCP when default pool is default" in new WithApplication(_.configure(
+    "use HikariCP as default pool" in new WithApplication(_.configure(
       "db.default.url" -> "jdbc:h2:mem:default",
       "db.other.driver" -> "org.h2.Driver",
       "db.other.url" -> "jdbc:h2:mem:other"
     )) {
-      val db = app.injector.instanceOf[DBApi]
-      db.database("default").withConnection { c =>
-        c.getClass.getName must contain("hikari")
+      val db = app.injector.instanceOf[DBApi].database("default")
+      db must beLike {
+        case pdb: PooledDatabase => pdb.pool must haveClass[HikariCPConnectionPool]
       }
     }
 
-    "use HikariCP when default pool is 'hikaricp'" in new WithApplication(_.configure(
+    "use HikariCP when default pool set to 'hikaricp'" in new WithApplication(_.configure(
       "play.db.pool" -> "hikaricp",
       "db.default.url" -> "jdbc:h2:mem:default",
       "db.other.driver" -> "org.h2.Driver",
       "db.other.url" -> "jdbc:h2:mem:other"
     )) {
-      val db = app.injector.instanceOf[DBApi]
-      db.database("default").withConnection { c =>
-        c.getClass.getName must contain("hikari")
+      val db = app.injector.instanceOf[DBApi].database("default")
+      db must beLike {
+        case pdb: PooledDatabase => pdb.pool must haveClass[HikariCPConnectionPool]
+      }
+    }
+
+    "use custom class when default pool set to class name" in new WithApplication(_.configure(
+      "play.db.pool" -> classOf[CustomConnectionPool].getName,
+      "db.default.url" -> "jdbc:h2:mem:default",
+      "db.other.driver" -> "org.h2.Driver",
+      "db.other.url" -> "jdbc:h2:mem:other"
+    )) {
+      val db = app.injector.instanceOf[DBApi].database("default")
+      db must beLike {
+        case pdb: PooledDatabase => pdb.pool must haveClass[CustomConnectionPool]
+      }
+    }
+
+    "use custom class when database pool set to class name" in new WithApplication(_.configure(
+      "db.default.pool" -> classOf[CustomConnectionPool].getName,
+      "db.default.url" -> "jdbc:h2:mem:default",
+      "db.other.driver" -> "org.h2.Driver",
+      "db.other.url" -> "jdbc:h2:mem:other"
+    )) {
+      val db = app.injector.instanceOf[DBApi].database("default")
+      db must beLike {
+        case pdb: PooledDatabase => pdb.pool must haveClass[CustomConnectionPool]
       }
     }
 
@@ -52,3 +78,6 @@ class ConnectionPoolConfigSpec extends PlaySpecification {
   }
 
 }
+
+@Singleton
+class CustomConnectionPool @Inject() (environment: Environment) extends HikariCPConnectionPool(environment)
