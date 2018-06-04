cbe3fb0808c3e7af82abfbdbc7f616a3ccf14df3
Christian Schmitt <c.schmitt@briefdomain.de>
2018-06-04 10:07:43 +0200

    added whitespace

--- framework/src/play/src/main/java/play/libs/reflect/ConstructorUtils.java
+++ framework/src/play/src/main/java/play/libs/reflect/ConstructorUtils.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
  */
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
