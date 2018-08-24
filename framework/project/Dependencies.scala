6145213f36b57471c5051db1455516c862ee2a50
Marcos Pereira <marcos.silva@gmail.com>
2018-08-24 13:51:54 -0400

    Update Play JSON to 2.6.10

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
   val akkaVersion = "2.5.14"
   val akkaHttpVersion = "10.1.3"
-  val playJsonVersion = "2.6.9"
+  val playJsonVersion = "2.6.10"
 
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
