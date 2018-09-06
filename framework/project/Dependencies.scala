416de3b60223f3646e9b88c9c697ba9d65ab2952
Marcos Pereira <marcos.silva@gmail.com>
2018-09-06 14:23:14 -0400

    Update Akka HTTP to 10.0.14 (#8598)

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -9,7 +9,7 @@ import buildinfo.BuildInfo
 object Dependencies {
 
   val akkaVersion = "2.5.16"
-  val akkaHttpVersion = "10.0.13"
+  val akkaHttpVersion = "10.0.14"
   val playJsonVersion = "2.6.10"
 
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
