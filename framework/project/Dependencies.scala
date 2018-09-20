c41d55d69ad086db2a1040d749bd037566efa8df
Marcos Pereira <marcos.silva@gmail.com>
2018-09-20 18:15:44 -0400

    Update play-ws to version 2.0.0-M4

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -276,7 +276,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache" % caffeineVersion
   ) ++ jcacheApi
 
-  val playWsStandaloneVersion = "2.0.0-M3"
+  val playWsStandaloneVersion = "2.0.0-M4"
   val playWsDeps = Seq(
     "com.typesafe.play" %% "play-ws-standalone" % playWsStandaloneVersion,
     "com.typesafe.play" %% "play-ws-standalone-xml" % playWsStandaloneVersion,
