12440f3d01148ae6934053db94134586deb16e4a
Jules Ivanic <guizmaii@users.noreply.github.com>
2018-09-27 00:15:55 +0200

    Implement a `JsonHttpErrorHandler` (#8299)
    * Merge
    
    * Copy ExceptionUtils class from Apache commons.lang3 v3.7 and use it in the code
    
    * Copy ExceptionUtils class from Apache commons.lang3 v3.7
    
    * Typos
    
    * Typo
    
    * Fix javadoc
    
    * Add missing `override`
    
    * Implement the @gmethvin version
    
    * Implement the @gmethvin version, for Java
    
    * Clean
    
    * Clean
    
    * Add missing `override`
    
    * Review: improve naming
    
    * Simplify use of the Scala version of `JsonHttpErrorHandler`
    
    * Clean
    
    * Add minimal documentation

---
+++ framework/src/play/src/main/java/play/http/JsonHttpErrorHandler.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package play.http;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.Environment;
+import play.Logger;
+import play.api.OptionalSourceMapper;
+import play.api.UsefulException;
+import play.api.http.HttpErrorHandlerExceptions;
+import play.libs.Json;
+import play.libs.exception.ExceptionUtils;
+import play.mvc.Http.RequestHeader;
+import play.mvc.Result;
+import play.mvc.Results;
+
+import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+/**
+ * An alternative default HTTP error handler which will render errors as JSON messages instead of HTML pages.
+ *
+ * In Dev mode, exceptions thrown by the server code will be rendered in JSON messages.
+ * In Prod mode, they will not be rendered.
+ *
+ * You could override how exceptions are rendered in Dev mode by extending this class and overriding
+ * the [[formatDevServerErrorException]] method.
+ */
+public class JsonHttpErrorHandler implements HttpErrorHandler {
+
+    private final Environment environment;
+    private final OptionalSourceMapper sourceMapper;
+
+    @Inject
+    public JsonHttpErrorHandler(Environment environment, OptionalSourceMapper sourceMapper) {
+        this.environment = environment;
+        this.sourceMapper = sourceMapper;
+    }
+
+    @Override
+    public CompletionStage<Result> onClientError(RequestHeader request, int statusCode, String message) {
+        ObjectNode result = Json.newObject();
+        result.put("requestId", request.asScala().id());
+        result.put("message", message);
+
+        return CompletableFuture.completedFuture(Results.status(statusCode, error(result)));
+    }
+
+
+    @Override
+    public CompletionStage<Result> onServerError(RequestHeader request, Throwable exception) {
+        try {
+            UsefulException usefulException = throwableToUsefulException(exception);
+
+            logServerError(request, usefulException);
+
+            switch (environment.mode()) {
+                case PROD:
+                    return CompletableFuture.completedFuture(Results.internalServerError(prodServerError(request, usefulException)));
+                default:
+                    return CompletableFuture.completedFuture(Results.internalServerError(devServerError(request, usefulException)));
+            }
+        } catch (Exception e) {
+            Logger.error("Error while handling error", e);
+            return CompletableFuture.completedFuture(Results.internalServerError());
+        }
+    }
+
+    /**
+     * Convert the given exception to an exception that Play can report more information about.
+     * <p>
+     * This will generate an id for the exception, and in dev mode, will load the source code for the code that threw the
+     * exception, making it possible to report on the location that the exception was thrown from.
+     */
+    protected final UsefulException throwableToUsefulException(final Throwable throwable) {
+        return HttpErrorHandlerExceptions.throwableToUsefulException(sourceMapper.sourceMapper(), environment.isProd(), throwable);
+    }
+
+    /**
+     * Responsible for logging server errors.
+     * <p>
+     * The base implementation uses play.Logger.error to log, which uses the SLF4J "application" logger.  If a special annotation is desired for internal server errors, you may want to use SLF4J directly with the Marker API to distinguish server errors from application errors.
+     * <p>
+     * This can also be overridden to add additional logging information, eg. the id of the authenticated user.
+     *
+     * @param request         The request that triggered the server error.
+     * @param usefulException The server error.
+     */
+    protected void logServerError(RequestHeader request, UsefulException usefulException) {
+        Logger.error(String.format("\n\n! @%s - Internal server error, for (%s) [%s] ->\n",
+                usefulException.id, request.method(), request.uri()),
+                usefulException
+        );
+    }
+
+    /**
+     * Invoked in dev mode when a server error occurs.
+     *
+     * @param request The request that triggered the error.
+     * @param exception The exception.
+     */
+    protected JsonNode devServerError(RequestHeader request, UsefulException exception) {
+        ObjectNode exceptionJson = Json.newObject();
+        exceptionJson.put("title", exception.title);
+        exceptionJson.put("description", exception.description);
+        exceptionJson.set("stacktrace", formatDevServerErrorException(exception.cause));
+
+        ObjectNode result = Json.newObject();
+        result.put("id", exception.id);
+        result.put("requestId", request.asScala().id());
+        result.set("exception", exceptionJson);
+
+        return error(result);
+    }
+
+    /**
+     * Format a {@link Throwable} as a JSON value.
+     *
+     * Override this method if you want to change how exceptions are rendered in Dev mode.
+     *
+     * @param exception an exception
+     * @return a JSON representation of the passed exception
+     */
+    protected JsonNode formatDevServerErrorException(Throwable exception) {
+        ArrayNode res = Json.newArray();
+        for (String s : ExceptionUtils.getStackFrames(exception)) {
+            res.add(s.trim());
+        }
+        return res;
+    }
+
+    /**
+     * Invoked in prod mode when a server error occurs.
+     *
+     * Override this rather than {@link #onServerError(RequestHeader, Throwable)} if you don't want to change Play's debug output when logging errors
+     * in dev mode.
+     *
+     * @param request The request that triggered the error.
+     * @param exception The exception.
+     */
+    protected JsonNode prodServerError(RequestHeader request, UsefulException exception) {
+        ObjectNode result = Json.newObject();
+        result.put("id", exception.id);
+
+        return error(result);
+    }
+
+    private JsonNode error(JsonNode content) {
+        ObjectNode result = Json.newObject();
+        result.set("error", content);
+        return result;
+    }
+
+}
