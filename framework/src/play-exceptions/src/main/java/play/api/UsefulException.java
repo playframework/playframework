b18763c6fe6306f76fbc5ec7a5086d36545f0e1c
Yury Gribkov <yury.gribkov@gmail.com>
2018-10-04 15:51:42 -0400

    Output exception description for better diagnostics. Driven by the need to have more information for various runtime exceptions, e.g. `@79emlk42a: No valid constructors at ...` without any reference for a problem class that is passed as a description to this exception.

--- framework/src/play-exceptions/src/main/java/play/api/UsefulException.java
+++ framework/src/play-exceptions/src/main/java/play/api/UsefulException.java
@@ -37,7 +37,7 @@ public abstract class UsefulException extends RuntimeException {
     }
 
     public String toString() {
-        return "@" + id + ": " + title;
+        return "@" + id + ": " + getMessage();
     }
 
 } 
