0f783da4f686319e21cd42c79faad3e0074f9717
Matthias Kurz <m.kurz@irregular.at>
2018-10-04 13:57:37 +0200

    Lookup form helpers' _label and _name attributes in messages (#8656)
    * Lookup form helpers' _label and _name attributes in messages
    
    * Add test

--- framework/src/play/src/main/scala/views/helper/Helpers.scala
+++ framework/src/play/src/main/scala/views/helper/Helpers.scala
@@ -42,13 +42,13 @@ package views.html.helper {
     }
 
     def label: Any = {
-      args.get('_label).getOrElse(p.messages(field.label))
+      args.get('_label).map(l => p.messages(l.toString)).getOrElse(p.messages(field.label))
     }
 
     def hasName: Boolean = args.get('_name).isDefined
 
     def name: Any = {
-      args.get('_name).getOrElse(p.messages(field.label))
+      args.get('_name).map(n => p.messages(n.toString)).getOrElse(p.messages(field.label))
     }
 
     private def translateMsgArg(msgArg: Any) = msgArg match {
