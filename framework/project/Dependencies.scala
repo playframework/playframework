7e6f5dcb11f64975bd9a6a0ebce9f82aaad46337
sullis <github@seansullivan.com>
2018-10-29 20:19:58 -0700

    Netty 4.1.30.Final (#8742)

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -164,7 +164,7 @@ object Dependencies {
       sslConfig
     ) ++ scalaParserCombinators(scalaVersion) ++ specs2Deps.map(_ % Test) ++ javaTestDeps
 
-  val nettyVersion = "4.1.29.Final"
+  val nettyVersion = "4.1.30.Final"
 
   val netty = Seq(
     "com.typesafe.netty" % "netty-reactive-streams-http" % "2.0.0",
