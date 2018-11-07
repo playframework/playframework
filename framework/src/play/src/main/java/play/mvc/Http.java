f0ee406743cb572494b173afe06bd487fbaa3f22
Marcos Pereira <marcos.silva@gmail.com>
2018-11-07 13:58:00 -0500

    Use play.api.Configuration.load to consistently load the configuration

--- framework/src/play/src/main/java/play/mvc/Http.java
+++ framework/src/play/src/main/java/play/mvc/Http.java
@@ -9,6 +9,7 @@ import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import akka.util.ByteString;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
@@ -64,11 +65,24 @@ public class Http {
     @Deprecated
     public static class Context {
 
+        private static Config config() {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            Properties properties = System.getProperties();
+            scala.collection.immutable.Map<String, Object> directSettings = scala.collection.Map$.MODULE$.empty();
+
+            // We are allowing missing application conf because it can handle both cases.
+            boolean allowMissingApplicationConf = true;
+
+            // Using play.api.Configuration.load because it is more consistent with how the
+            // actual configuration is loaded for the application.
+            return play.api.Configuration.load(classLoader, properties, directSettings, allowMissingApplicationConf).underlying();
+        }
+
         /**
          * @deprecated Deprecated as of 2.7.0. <a href="https://www.playframework.com/documentation/latest/JavaHttpContextMigration27">Use a request instead</a>.
          */
         @Deprecated
-        public static ThreadLocal<Context> current = ConfigFactory.load().getBoolean("play.allowHttpContext") ? new ThreadLocal<>() : null;
+        public static ThreadLocal<Context> current = config().getBoolean("play.allowHttpContext") ? new ThreadLocal<>() : null;
 
         /**
          * Retrieves the current HTTP context, for the current thread.
@@ -98,7 +112,7 @@ public class Http {
          */
         @Deprecated
         public static Optional<Http.Context> safeCurrent() {
-            return Optional.ofNullable(Context.current).map(threadLocal -> threadLocal.get());
+            return Optional.ofNullable(Context.current).map(ThreadLocal::get);
         }
 
         /**
