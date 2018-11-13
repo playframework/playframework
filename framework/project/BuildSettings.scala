7fbaaaa764a64f3cb7fa35800082b1db8f15529d
Renato  Cavalcanti <renato@cavalcanti.be>
2018-11-13 14:48:21 +0100

    remove obsolete comments about 2.13.x versions

--- framework/project/BuildSettings.scala
+++ framework/project/BuildSettings.scala
@@ -721,15 +721,6 @@ object BuildSettings {
     // Argument for setting size of permgen space or meta space for all forked processes
     Docs.apiDocsInclude := true
   ) ++ Seq(
-    // TODO: Remove when updating to Scala 2.13.0-M4
-    // Interplay 2.0.3 adds Scala 2.13.0-M4 to crossScalaVersions, but we don't want
-    // that right because some dependencies don't have a build to M4 yet. As soon as
-    // we decide that we could release to M4, than we can remove this setting and use
-    // the one provided by interplay.
-    //
-    // See also:
-    // 1. the root project at build.sbt file.
-    // 2. RoutesCompilerProject project
     crossScalaVersions := Seq(ScalaVersions.scala211, ScalaVersions.scala212)
   )
 
