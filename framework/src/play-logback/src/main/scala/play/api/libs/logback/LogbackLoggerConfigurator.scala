668de94b28a88405b242a01c3cb770be6ba3a710
Matthias Kurz <m.kurz@irregular.at>
2018-11-16 23:25:18 +0100

    LoggerContext.reset should not removes just added listeners

--- framework/src/play-logback/src/main/scala/play/api/libs/logback/LogbackLoggerConfigurator.scala
+++ framework/src/play-logback/src/main/scala/play/api/libs/logback/LogbackLoggerConfigurator.scala
@@ -92,6 +92,8 @@ class LogbackLoggerConfigurator extends LoggerConfigurator {
       // Configure logback
       val ctx = loggerFactory.asInstanceOf[LoggerContext]
 
+      ctx.reset()
+
       // Set a level change propagator to minimize the overhead of JUL
       //
       // Please note that translating a java.util.logging event into SLF4J incurs the
@@ -110,8 +112,6 @@ class LogbackLoggerConfigurator extends LoggerConfigurator {
       levelChangePropagator.setResetJUL(true)
       ctx.addListener(levelChangePropagator)
       SLF4JBridgeHandler.install()
-
-      ctx.reset()
 
       // Ensure that play.Logger and play.api.Logger are ignored when detecting file name and line number for
       // logging
