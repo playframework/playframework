f4e7656c2745f90b02d90d42a4a86e0025f08fe2
Matthias Kurz <m.kurz@irregular.at>
2018-11-28 18:05:37 +0100

    Set up test routes with various combinations, this time with default value set in routes

--- framework/src/sbt-plugin/src/sbt-test/routes-compiler-plugin/routes-compilation/app/controllers/Application.scala
+++ framework/src/sbt-plugin/src/sbt-test/routes-compiler-plugin/routes-compilation/app/controllers/Application.scala
@@ -142,6 +142,114 @@ class Application @Inject() (c: ControllerComponents) extends AbstractController
   def takeJavaListIntegerOptional(x: java.util.Optional[java.util.List[Integer]]) = Action {
     Ok(x.asScala.map(_.asScala.mkString(",")).getOrElse("emptyOptional"))
   }
+  def takeStringWithDefault(x: String) = Action {
+    Ok(x)
+  }
+  def takeStringOptionWithDefault(x: Option[String]) = Action {
+    Ok(x.getOrElse("emptyOption"))
+  }
+  def takeStringOptionalWithDefault(x: java.util.Optional[String]) = Action {
+    Ok(x.asScala.getOrElse("emptyOptional"))
+  }
+  def takeCharWithDefault(x: Char) = Action {
+    Ok(x.toString)
+  }
+  def takeCharOptionWithDefault(x: Option[Char]) = Action {
+    Ok(x.map(_.toString).getOrElse("emptyOption"))
+  }
+//  def takeCharOptionalWithDefault(x: java.util.Optional[Char]) = Action {
+//    Ok(x.asScala.map(_.toString).getOrElse("emptyOptional"))
+//  }
+  def takeIntWithDefault(x: Int) = Action {
+    Ok(x.toString)
+  }
+  def takeIntOptionWithDefault(x: Option[Int]) = Action {
+    Ok(x.map(_.toString).getOrElse("emptyOption"))
+  }
+//  def takeIntOptionalWithDefault(x: java.util.Optional[Int]) = Action {
+//    Ok(x.asScala.map(_.toString).getOrElse("emptyOptional"))
+//  }
+  def takeIntegerWithDefault(x: Integer) = Action {
+    Ok(x.toString)
+  }
+//  def takeIntegerOptionWithDefault(x: Option[Integer]) = Action {
+//    Ok(x.map(_.toString).getOrElse("emptyOption"))
+//  }
+  def takeIntegerOptionalWithDefault(x: java.util.Optional[Integer]) = Action {
+    Ok(x.asScala.map(_.toString).getOrElse("emptyOptional"))
+  }
+  def takeListStringWithDefault(x: List[String]) = Action {
+    Ok(x.map(str => if(str.isEmpty){"emptyStringElement"}else{str}).mkString(","))
+  }
+  def takeListStringOptionWithDefault(x: Option[List[String]]) = Action {
+    Ok(x.map(_.map(str => if(str.isEmpty){"emptyStringElement"}else{str}).mkString(",")).getOrElse("emptyOption"))
+  }
+//  def takeListStringOptionalWithDefault(x: java.util.Optional[List[String]]) = Action {
+//    Ok(x.asScala.map(_.map(str => if(str.isEmpty){"emptyStringElement"}else{str}).mkString(",")).getOrElse("emptyOptional"))
+//  }
+  def takeListCharWithDefault(x: List[Char]) = Action {
+    Ok(x.mkString(","))
+  }
+  def takeListCharOptionWithDefault(x: Option[List[Char]]) = Action {
+    Ok(x.map(_.mkString(",")).getOrElse("emptyOption"))
+  }
+//  def takeListCharOptionalWithDefault(x: java.util.Optional[List[Char]]) = Action {
+//    Ok(x.asScala.map(_.mkString(",")).getOrElse("emptyOptional"))
+//  }
+  def takeJavaListStringWithDefault(x: java.util.List[String]) = Action {
+    Ok(x.asScala.map(str => if(str.isEmpty){"emptyStringElement"}else{str}).mkString(","))
+  }
+//  def takeJavaListStringOptionWithDefault(x: Option[java.util.List[String]]) = Action {
+//    Ok(x.map(_.asScala.map(str => if(str.isEmpty){"emptyStringElement"}else{str}).mkString(",")).getOrElse("emptyOption"))
+//  }
+  def takeJavaListStringOptionalWithDefault(x: java.util.Optional[java.util.List[String]]) = Action {
+    Ok(x.asScala.map(_.asScala.map(str => if(str.isEmpty){"emptyStringElement"}else{str}).mkString(",")).getOrElse("emptyOptional"))
+  }
+//  def takeJavaListCharWithDefault(x: java.util.List[Char]) = Action {
+//    Ok(x.asScala.mkString(","))
+//  }
+//  def takeJavaListCharOptionWithDefault(x: Option[java.util.List[Char]]) = Action {
+//    Ok(x.mkString(","))
+//  }
+//  def takeJavaListCharOptionalWithDefault(x: java.util.Optional[java.util.List[Char]]) = Action {
+//    Ok(x.asScala.mkString(","))
+//  }
+  def takeListIntWithDefault(x: List[Int]) = Action {
+    Ok(x.mkString(","))
+  }
+  def takeListIntOptionWithDefault(x: Option[List[Int]]) = Action {
+    Ok(x.map(_.mkString(",")).getOrElse("emptyOption"))
+  }
+//  def takeListIntOptionalWithDefault(x: java.util.Optional[List[Int]]) = Action {
+//    Ok(x.asScala.map(_.mkString(",")).getOrElse("emptyOptional"))
+//  }
+//  def takeJavaListIntWithDefault(x: java.util.List[Int]) = Action {
+//    Ok(x.asScala.mkString(","))
+//  }
+//  def takeJavaListIntOptionWithDefault(x: Option[java.util.List[Int]]) = Action {
+//    Ok(x.mkString(","))
+//  }
+//  def takeJavaListIntOptionalWithDefault(x: java.util.Optional[java.util.List[Int]]) = Action {
+//    Ok(x.asScala.mkString(","))
+//  }
+//  def takeListIntegerWithDefault(x: List[Integer]) = Action {
+//    Ok(x.mkString(","))
+//  }
+//  def takeListIntegerOptionWithDefault(x: Option[List[Integer]]) = Action {
+//    Ok(x.mkString(","))
+//  }
+//  def takeListIntegerOptionalWithDefault(x: java.util.Optional[List[Integer]]) = Action {
+//    Ok(x.asScala.mkString(","))
+//  }
+  def takeJavaListIntegerWithDefault(x: java.util.List[Integer]) = Action {
+    Ok(x.asScala.mkString(","))
+  }
+//  def takeJavaListIntegerOptionWithDefault(x: Option[java.util.List[Integer]]) = Action {
+//    Ok(x.map(_.asScala.mkString(",")).getOrElse("emptyOption"))
+//  }
+  def takeJavaListIntegerOptionalWithDefault(x: java.util.Optional[java.util.List[Integer]]) = Action {
+    Ok(x.asScala.map(_.asScala.mkString(",")).getOrElse("emptyOptional"))
+  }
   def urlcoding(dynamic: String, static: String, query: String) = Action {
     Ok(s"dynamic=$dynamic static=$static query=$query")
   }
