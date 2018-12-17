37b03f7dd8654701cb4f79ce52bb7852b227a89a
Marcos Pereira <marcos.silva@gmail.com>
2018-12-17 12:51:10 -0500

    Use Akka HTTP snapshots for our cron builds (#8884)
    ## Purpose
    
    The purpose is to discover Akka HTTP integration problems sooner. This enables us to configure a nightly build in Travis to test against Akka HTTP snapshots. Basically the same we did for Akka in #8601.

--- framework/project/AkkaSnapshotRepositories.scala
+++ framework/project/AkkaSnapshotRepositories.scala
@@ -12,7 +12,10 @@ object AkkaSnapshotRepositories extends AutoPlugin {
     // If this is a cron job in Travis:
     // https://docs.travis-ci.com/user/cron-jobs/#detecting-builds-triggered-by-cron
     resolvers ++= (sys.env.get("TRAVIS_EVENT_TYPE").filter(_.equalsIgnoreCase("cron")) match {
-      case Some(_) => Seq("akka-snapshot-repository" at "https://repo.akka.io/snapshots")
+      case Some(_) => Seq(
+        "akka-snapshot-repository" at "https://repo.akka.io/snapshots",
+        "akka-http-snapshot-repository" at "https://dl.bintray.com/akka/maven/"
+      )
       case None => Seq.empty
     })
   }
