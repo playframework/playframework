e3ed7145503135d89e84c2dba3540e5d9cbf11e0
Matthias Kurz <m.kurz@irregular.at>
2018-12-21 01:14:27 +0100

    Add tests

--- framework/src/play-java-forms/src/test/scala/play/data/DynamicFormSpec.scala
+++ framework/src/play-java-forms/src/test/scala/play/data/DynamicFormSpec.scala
@@ -5,6 +5,7 @@
 package play.data
 
 import com.typesafe.config.ConfigFactory
+import java.nio.file.Files
 
 import javax.validation.Validation
 
@@ -12,15 +13,18 @@ import org.specs2.mutable.Specification
 import play.api.i18n.DefaultMessagesApi
 import play.core.j.PlayFormsMagicForJava.javaFieldtoScalaField
 import play.data.format.Formatters
+import play.libs.Files.{ SingletonTemporaryFileCreator, TemporaryFile }
+import play.mvc.Http.MultipartFormData.FilePart
 import views.html.helper.FieldConstructor.defaultField
 import views.html.helper.inputText
 
 import scala.collection.JavaConverters._
+import scala.compat.java8.OptionConverters._
 
 /**
  * Specs for Java dynamic forms
  */
-class DynamicFormSpec extends Specification {
+class DynamicFormSpec extends CommonFormSpec {
 
   val messagesApi = new DefaultMessagesApi()
   implicit val messages = messagesApi.preferred(Seq.empty)
@@ -34,6 +38,48 @@ class DynamicFormSpec extends Specification {
       val form = new DynamicForm(jMessagesApi, new Formatters(jMessagesApi), validatorFactory, config).bindFromRequest(FormSpec.dummyRequest(Map("foo" -> Array("bar"))))
       form.get("foo") must_== "bar"
       form.value("foo").get must_== "bar"
+    }
+
+    "bind values from a multipart request containing files" in {
+      implicit val temporaryFileCreator = new SingletonTemporaryFileCreator()
+
+      val files = createThesisTemporaryFiles()
+
+      try {
+        val req = createThesisRequestWithFileParts(files)
+
+        val myForm = new DynamicForm(jMessagesApi, new Formatters(jMessagesApi), validatorFactory, config).bindFromRequest(req)
+
+        myForm.hasErrors() must beEqualTo(false)
+        myForm.hasGlobalErrors() must beEqualTo(false)
+
+        myForm.rawData().size() must beEqualTo(1)
+        myForm.files().size() must beEqualTo(5)
+
+        myForm.get("title") must beEqualTo("How Scala works")
+        myForm.field("title").value().asScala must beSome("How Scala works")
+        myForm.field("title").file().asScala must beNone
+
+        checkFileParts(Seq(myForm.file("document"), myForm.field("document").file().get()), "document", "application/pdf", "best_thesis.pdf", "by Lightbend founder Martin Odersky")
+        myForm.field("document").value().asScala must beNone
+
+        // TODO: This tests below are buggy. As you can see the closing bracket ] is missing everywhere. This is a bug in spring-beans.
+        // When parsing e.g. "data[attachments[0]]" spring just looks for the first occurrence of ] when and uses it for the the end position in substring
+        // See here, `keyEnd` is wrong:
+        // https://github.com/spring-projects/spring-framework/blob/v5.1.3.RELEASE/spring-beans/src/main/java/org/springframework/beans/AbstractNestablePropertyAccessor.java#L941
+
+        checkFileParts(Seq(myForm.file("attachments[0"), myForm.field("attachments[0").file().get()), "attachments[]", "application/x-tex", "final_draft.tex", "the final draft")
+        myForm.field("attachments[0").value().asScala must beNone
+        checkFileParts(Seq(myForm.file("attachments[1"), myForm.field("attachments[1").file().get()), "attachments[]", "text/x-scala-source", "examples.scala", "some code snippets")
+        myForm.field("attachments[1").value().asScala must beNone
+
+        checkFileParts(Seq(myForm.file("bibliography[0"), myForm.field("bibliography[0").file().get()), "bibliography[0]", "application/epub+zip", "Java_Concurrency_in_Practice.epub", "Java Concurrency in Practice")
+        myForm.field("bibliography[0").value().asScala must beNone
+        checkFileParts(Seq(myForm.file("bibliography[1"), myForm.field("bibliography[1").file().get()), "bibliography[1]", "application/x-mobipocket-ebook", "The-Java-Programming-Language.mobi", "The Java Programming Language")
+        myForm.field("bibliography[1").value().asScala must beNone
+      } finally {
+        files.values.foreach(temporaryFileCreator.delete(_))
+      }
     }
 
     "allow access to raw data values from request" in {
