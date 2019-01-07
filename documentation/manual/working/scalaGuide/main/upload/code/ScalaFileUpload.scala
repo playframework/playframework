e235f959acc60625c4a3b3dcb682c72f079a01b1
Matthias Kurz <m.kurz@irregular.at>
2019-01-07 01:40:31 +0100

    File upload docs should also show fileSize/getFileSize()

--- documentation/manual/working/scalaGuide/main/upload/code/ScalaFileUpload.scala
+++ documentation/manual/working/scalaGuide/main/upload/code/ScalaFileUpload.scala
@@ -50,8 +50,10 @@ package scalaguide.upload.fileupload {
             // only get the last part of the filename
             // otherwise someone can send a path like ../../home/foo/bar.txt to write to other files on the system
             val filename = Paths.get(picture.filename).getFileName
+            val fileSize = picture.fileSize
+            val contentType = picture.contentType
 
-            picture.ref.moveTo(Paths.get(s"/tmp/picture/$filename"), replace = true)
+            picture.ref.copyTo(Paths.get(s"/tmp/picture/$filename"), replace = true)
             Ok("File uploaded")
           }.getOrElse {
             Redirect(routes.HomeController.index).flashing("error" -> "Missing file")
