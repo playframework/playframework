cf42bedd31042d69b68c41fb83672a5d36b8efef
Marcos Pereira <marcos.silva@gmail.com>
2019-01-07 19:02:28 -0500

    Update guava to version 23.6.1

--- framework/project/Dependencies.scala
+++ framework/project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   val slf4j = Seq("slf4j-api", "jul-to-slf4j", "jcl-over-slf4j").map("org.slf4j" % _ % slf4jVersion)
   val slf4jSimple = "org.slf4j" % "slf4j-simple" % slf4jVersion
 
-  val guava = "com.google.guava" % "guava" % "22.0"
+  val guava = "com.google.guava" % "guava" % "23.6.1-jre"
   val findBugs = "com.google.code.findbugs" % "jsr305" % "3.0.2" // Needed by guava
   val mockitoAll = "org.mockito" % "mockito-all" % "1.10.19"
 
