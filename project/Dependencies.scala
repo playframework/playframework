9a1f5c460d0683c4e8c5bfef67a73042a7fe0e6e
Scala steward <me@scala-steward.org>
2019-04-03 22:26:54 +0200

    Update akka-actor, akka-slf4j, akka-stream, ... to 2.5.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ import buildinfo.BuildInfo
 
 object Dependencies {
 
-  val akkaVersion: String     = sys.props.getOrElse("akka.version", "2.5.21")
+  val akkaVersion: String     = sys.props.getOrElse("akka.version", "2.5.22")
   val akkaHttpVersion: String = sys.props.getOrElse("akka.http.version", "10.1.8")
 
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.3.7"
