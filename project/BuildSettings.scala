a2e1bb3ec9fe7e0cb8d11a6304b16d1edcc445a5
Roman Parshikov <promansew@gmail.com>
2019-04-03 23:53:17 +0800

    Fix MiMa filter

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -222,7 +222,7 @@ object BuildSettings {
       ProblemFilters.exclude[MissingTypesProblem]("play.core.j.PlayMagicForJava$"),
       // Add fileName param (with default value) to Scala's sendResource(...) method
       ProblemFilters.exclude[DirectMissingMethodProblem]("play.api.mvc.Results#Status.sendResource"),
-      ProblemFilters.exclude[DirectMissingMethodProblem]("play.api.test.Helpers.TOO_MANY_REQUEST")
+      ProblemFilters.exclude[DirectMissingMethodProblem]("play.api.http.Status.TOO_MANY_REQUEST")
     ),
     unmanagedSourceDirectories in Compile += {
       (sourceDirectory in Compile).value / s"scala-${scalaBinaryVersion.value}"
