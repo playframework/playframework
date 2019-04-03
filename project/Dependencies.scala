526ae558facee46460b2432c05090ea18352e700
Marcos Pereira <marcos.silva@gmail.com>
2019-04-03 17:36:06 -0400

    Update Play-WS to version 2.0.3 (#9164)
    * Update Play-WS to version 2.0.3
    
    This version has an new configuration that enables/disables an
    internal cookie store that is persistent accross multiple requests
    to the same hosts. By default, it is disabled.
    
    * Update ServerReloadingSpec.scala
    
    * Update core/play-integration-test/src/it/scala/play/it/server/ServerReloadingSpec.scala
    
    Co-Authored-By: marcospereira <marcos.silva@gmail.com>
    
    * Add migration guide notes to cookie store

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -274,7 +274,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi
 
-  val playWsStandaloneVersion = "2.0.1"
+  val playWsStandaloneVersion = "2.0.3"
   val playWsDeps = Seq(
     "com.typesafe.play"                        %% "play-ws-standalone" % playWsStandaloneVersion,
     "com.typesafe.play"                        %% "play-ws-standalone-xml" % playWsStandaloneVersion,
