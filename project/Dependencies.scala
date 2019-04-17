c32f8dac635bf7a831070c2b645a8f8fdd49cb9a
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-04-17 11:27:46 +0200

    Update okhttp to 3.14.1 (#9181)
    Updates com.squareup.okhttp3:okhttp from 3.14.0 to 3.14.1.
    
    I'll automatically update this PR to resolve conflicts as long as you don't change it yourself.
    
    If you'd like to skip this version, you can just close this PR. If you have any feedback, just mention @scala-steward in the comments below.
    
    Have a nice day!
    
    <details>
    <summary>Ignore future updates</summary>
    
    Add this to your `.scala-steward.conf` file to ignore future updates of this dependency:
    ```
    updates.ignore = [{ groupId = "com.squareup.okhttp3", artifactId = "okhttp" }]
    ```
    </details>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -165,7 +165,7 @@ object Dependencies {
 
   val jimfs = "com.google.jimfs" % "jimfs" % "1.1"
 
-  val okHttp = "com.squareup.okhttp3" % "okhttp" % "3.14.0"
+  val okHttp = "com.squareup.okhttp3" % "okhttp" % "3.14.1"
 
   def routesCompilerDependencies(scalaVersion: String) = {
     val deps = CrossVersion.partialVersion(scalaVersion) match {
