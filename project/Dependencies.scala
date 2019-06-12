05eb01abc29a33af04dd985da4eed048d0d0eb49
Scala Steward <me@scala-steward.org>
2019-06-12 19:35:57 +0200

    Update play-doc to 2.1.0-M2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -227,7 +227,7 @@ object Dependencies {
     "org.webjars" % "prettify" % "4-Mar-2013-1" % "webjars"
   )
 
-  val playDocVersion = "2.1.0-M1"
+  val playDocVersion = "2.1.0-M2"
   val playDocsDependencies = Seq(
     "com.typesafe.play" %% "play-doc" % playDocVersion
   ) ++ playdocWebjarDependencies
