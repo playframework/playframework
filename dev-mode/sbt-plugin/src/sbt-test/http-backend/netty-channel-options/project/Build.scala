b11d14e94bcbfbcfee67a45472aff5b9a6dd3778
Dale Wijnand <dale.wijnand@gmail.com>
2019-08-14 18:26:54 +0100

    Cleanup DevModeBuild in sbt-test/http-backend/netty-channel-options

--- dev-mode/sbt-plugin/src/sbt-test/http-backend/netty-channel-options/project/Build.scala
+++ dev-mode/sbt-plugin/src/sbt-test/http-backend/netty-channel-options/project/Build.scala
@@ -4,10 +4,6 @@
 import sbt._
 
 object DevModeBuild {
-
-  val ConnectTimeout = 10000
-  val ReadTimeout    = 10000
-
   def callIndex(): Unit = callUrl("/")
 
   def checkLines(source: String, target: String): Unit = {
@@ -21,37 +17,24 @@ object DevModeBuild {
     println("-------")
     println(targetLines.mkString("\n"))
 
-    sourceLines.foreach(sl => {
+    sourceLines.foreach { sl =>
       if (!targetLines.contains(sl)) {
         throw new RuntimeException(s"File $target didn't contain line:\n$sl")
       }
-    })
+    }
   }
 
   private def callUrl(path: String, headers: (String, String)*): (Int, String) = {
-    val url  = new java.net.URL("http://localhost:9000" + path)
+    val url  = new java.net.URL(s"http://localhost:9000$path")
     val conn = url.openConnection().asInstanceOf[java.net.HttpURLConnection]
-    conn.setConnectTimeout(ConnectTimeout)
-    conn.setReadTimeout(ReadTimeout)
-
-    headers.foreach(h => conn.setRequestProperty(h._1, h._2))
-
-    val status = conn.getResponseCode
-
-    val is = if (conn.getResponseCode >= 400) {
-      conn.getErrorStream
-    } else {
-      conn.getInputStream
-    }
-
-    // The input stream may be null if there's no body
-    val contents = if (is != null) {
-      val c = IO.readStream(is)
-      is.close()
-      c
-    } else ""
-    conn.disconnect()
-
-    (status, contents)
+    conn.setConnectTimeout(10000)
+    conn.setReadTimeout(10000)
+    headers.foreach { case (k, v) => conn.setRequestProperty(k, v) }
+    try {
+      val status   = conn.getResponseCode
+      val in       = if (status < 400) conn.getInputStream else conn.getErrorStream
+      val contents = if (in == null) "" else try IO.readStream(in) finally in.close()
+      (status, contents)
+    } finally conn.disconnect()
   }
 }
