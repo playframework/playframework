e486c64a40af8565e70f01829f01081580399d26
Dale Wijnand <dale.wijnand@gmail.com>
2019-08-14 18:24:44 +0100

    Cleanup ProdServerStart

--- transport/server/play-server/src/main/scala/play/core/server/ProdServerStart.scala
+++ transport/server/play-server/src/main/scala/play/core/server/ProdServerStart.scala
@@ -9,12 +9,13 @@ import java.nio.file.FileAlreadyExistsException
 import java.nio.file.Files
 import java.nio.file.StandardOpenOption
 
-import akka.Done
-import akka.actor.CoordinatedShutdown
-import play.api._
-
 import scala.concurrent.Future
 import scala.util.control.NonFatal
+
+import akka.Done
+import akka.actor.CoordinatedShutdown
+
+import play.api._
 
 /**
  * Used to start servers in 'prod' mode, the mode that is
@@ -26,10 +27,7 @@ object ProdServerStart {
   /**
    * Start a prod mode server from the command line.
    */
-  def main(args: Array[String]): Unit = {
-    val process = new RealServerProcess(args)
-    start(process)
-  }
+  def main(args: Array[String]): Unit = start(new RealServerProcess(args))
 
   /**
    * Starts a Play server and application for the given process. The settings
@@ -37,13 +35,10 @@ object ProdServerStart {
    * various system properties. Crash out by exiting the given process if there
    * are any problems.
    *
-   * @param process The process (real or abstract) to use for starting the
-   *                server.
+   * @param process The process (real or abstract) to use for starting the server.
    */
   def start(process: ServerProcess): ReloadableServer = {
-
     try {
-
       // Read settings
       val config: ServerConfig = readServerConfigSettings(process)
 
@@ -61,17 +56,17 @@ object ProdServerStart {
         Play.start(application)
 
         // Start the server
-        val serverProvider: ServerProvider = ServerProvider.fromConfiguration(process.classLoader, config.configuration)
-        val server                         = serverProvider.createServer(config, application)
+        val serverProvider = ServerProvider.fromConfiguration(process.classLoader, config.configuration)
+        val server         = serverProvider.createServer(config, application)
 
-        application.coordinatedShutdown
-          .addTask(CoordinatedShutdown.PhaseBeforeActorSystemTerminate, "remove-pid-file") { () =>
-            // Must delete the PID file after stopping the server not before...
-            // In case of unclean shutdown or failure, leave the PID file there!
-            pidFile.foreach(_.delete())
-            assert(!pidFile.exists(_.exists), "PID file should not exist!")
-            Future.successful(Done)
-          }
+        val phase = CoordinatedShutdown.PhaseBeforeActorSystemTerminate
+        application.coordinatedShutdown.addTask(phase, "remove-pid-file") { () =>
+          // Must delete the PID file after stopping the server not before...
+          // In case of unclean shutdown or failure, leave the PID file there!
+          pidFile.foreach(_.delete())
+          assert(pidFile.forall(!_.exists), "PID file should not exist!")
+          Future.successful(Done)
+        }
 
         process.addShutdownHook {
           // Only run server stop if the shutdown reason is not defined. That means the
@@ -93,21 +88,18 @@ object ProdServerStart {
           throw e
       }
     } catch {
-      case ServerStartException(message, cause) =>
-        process.exit(message, cause)
-      case NonFatal(e) =>
-        process.exit("Oops, cannot start the server.", cause = Some(e))
+      case ServerStartException(message, cause) => process.exit(message, cause)
+      case NonFatal(e)                          => process.exit("Oops, cannot start the server.", Some(e))
     }
   }
 
   /**
-   * Read the server config from the current process's command
-   * line args and system properties.
+   * Read the server config from the current process's command line args and system properties.
    */
   def readServerConfigSettings(process: ServerProcess): ServerConfig = {
     val configuration: Configuration = {
-      val rootDirArg: Option[File] = process.args.headOption.map(new File(_))
-      val rootDirConfig            = rootDirArg.fold(Map.empty[String, String])(dir => ServerConfig.rootDirConfig(dir))
+      val rootDirArg    = process.args.headOption.map(new File(_))
+      val rootDirConfig = rootDirArg.fold(Map.empty[String, String])(ServerConfig.rootDirConfig(_))
       Configuration.load(process.classLoader, process.properties, rootDirConfig, true)
     }
 
@@ -116,39 +108,29 @@ object ProdServerStart {
         .getOptional[String]("play.server.dir")
         .getOrElse(throw ServerStartException("No root server path supplied"))
       val file = new File(path)
-      if (!(file.exists && file.isDirectory)) {
+      if (!file.isDirectory)
         throw ServerStartException(s"Bad root server path: $path")
-      }
       file
     }
 
     def parsePort(portType: String): Option[Int] = {
-      configuration.getOptional[String](s"play.server.${portType}.port").flatMap {
-        case "disabled" => None
-        case str =>
-          val i = try Integer.parseInt(str)
-          catch {
-            case _: NumberFormatException => throw ServerStartException(s"Invalid ${portType.toUpperCase} port: $str")
-          }
-          Some(i)
+      configuration.getOptional[String](s"play.server.$portType.port").filter(_ != "disabled").map { str =>
+        try Integer.parseInt(str)
+        catch {
+          case _: NumberFormatException =>
+            throw ServerStartException(s"Invalid ${portType.toUpperCase} port: $str")
+        }
       }
     }
 
     val httpPort  = parsePort("http")
     val httpsPort = parsePort("https")
-    if (httpPort.orElse(httpsPort).isEmpty) throw ServerStartException("Must provide either an HTTP or HTTPS port")
+    val address   = configuration.getOptional[String]("play.server.http.address").getOrElse("0.0.0.0")
 
-    val address = configuration.getOptional[String]("play.server.http.address").getOrElse("0.0.0.0")
+    if (httpPort.orElse(httpsPort).isEmpty)
+      throw ServerStartException("Must provide either an HTTP or HTTPS port")
 
-    ServerConfig(
-      rootDir = rootDir,
-      port = httpPort,
-      sslPort = httpsPort,
-      address = address,
-      mode = Mode.Prod,
-      properties = process.properties,
-      configuration = configuration
-    )
+    ServerConfig(rootDir, httpPort, httpsPort, address, Mode.Prod, process.properties, configuration)
   }
 
   /**
@@ -165,11 +147,10 @@ object ProdServerStart {
       val out = try Files.newOutputStream(pidFile.toPath, StandardOpenOption.CREATE_NEW)
       catch {
         case _: FileAlreadyExistsException =>
-          throw ServerStartException(s"This application is already running (Or delete ${pidFile.getPath} file).")
+          throw ServerStartException(s"This application is already running (or delete ${pidFile.getPath} file).")
       }
       try out.write(pid.getBytes)
       finally out.close()
-
       Some(pidFile)
     }
   }
