2078afac5a823e8ebffe65d00eafa2004490a969
Scala Steward <me@scala-steward.org>
2019-09-21 07:24:15 +0200

    Update akka-http-core, akka-http2-support to 10.1.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ import buildinfo.BuildInfo
 object Dependencies {
 
   val akkaVersion: String = sys.props.getOrElse("akka.version", "2.6.0-M7")
-  val akkaHttpVersion     = "10.1.9"
+  val akkaHttpVersion     = "10.1.10"
 
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.4.0"
 
