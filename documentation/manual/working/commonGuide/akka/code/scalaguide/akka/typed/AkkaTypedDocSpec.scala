2a0b6671bb75f4c33f3141a251af4e1784a86f2d
Dale Wijnand <dale.wijnand@gmail.com>
2019-10-09 14:33:10 +0100

    Un-nest AkkaTypedDocSpec, now that support code is moved

--- documentation/manual/working/commonGuide/akka/code/scalaguide/akka/typed/AkkaTypedDocSpec.scala
+++ documentation/manual/working/commonGuide/akka/code/scalaguide/akka/typed/AkkaTypedDocSpec.scala
@@ -4,79 +4,60 @@
 
 package scalaguide.akka.typed
 
-import org.specs2.matcher.MatchResult
 import org.specs2.mutable.Specification
 import play.api.inject.Injector
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.inject.guice.GuiceableModule
 
 final class AkkaTypedDocSpec extends Specification {
-  import AkkaTypedDocSpec._
+  "Runtime DI support for FP-style" in fpStyle
+  "Runtime DI support for OO-style" in ooStyle
 
-  "Runtime DI support for FP-style typed actors" in (fpStyle.mainIsInjected)
-  "Runtime DI support for OO-style typed actors" in (ooStyle.mainIsInjected)
-  "Runtime DI support for multi-instance FP-style typed actors" in (fpStyleMulti.mainIsInjected)
-  "Runtime DI support for multi-instance OO-style typed actors" in (ooStyleMulti.mainIsInjected)
-  "Compile-time DI without support works" in (compileTimeDI.works)
+  "Runtime DI support for multi-instance FP-style" in fpStyleMulti
+  "Runtime DI support for multi-instance OO-style" in ooStyleMulti
 
-}
+  "Compile-time DI without support works" in compileTimeDI
 
-object AkkaTypedDocSpec {
-
-  private object fpStyle { // functional programming style
+  private def fpStyle = { // functional programming style
     import fp._
-
-    def mainIsInjected(): MatchResult[_] = {
-      val main = newInjector(AppModule).instanceOf[Main]
-      (main.helloActor !== null).and(main.configuredActor !== null)
-    }
+    val main = newInjector(AppModule).instanceOf[Main]
+    (main.helloActor !== null).and(main.configuredActor !== null)
   }
 
-  private object ooStyle { // object-oriented style
+  private def ooStyle = { // object-oriented style
     import oo._
-
-    def mainIsInjected(): MatchResult[_] = {
-      val main = newInjector(AppModule).instanceOf[Main]
-      (main.helloActor !== null).and(main.configuredActor !== null)
-    }
+    val main = newInjector(AppModule).instanceOf[Main]
+    (main.helloActor !== null).and(main.configuredActor !== null)
   }
 
-  private object fpStyleMulti { // with multiple spawned ActorRef[T]
+  private def fpStyleMulti = { // with multiple spawned ActorRef[T]
     import fp.multi._
-
-    def mainIsInjected(): MatchResult[_] = {
-      val main = newInjector(AppModule).instanceOf[Main]
-      (main.helloActor1 !== null)
-        .and(main.helloActor2 !== null)
-        .and(main.configuredActor1 !== null)
-        .and(main.configuredActor2 !== null)
-    }
+    val main = newInjector(AppModule).instanceOf[Main]
+    (main.helloActor1 !== null)
+      .and(main.helloActor2 !== null)
+      .and(main.configuredActor1 !== null)
+      .and(main.configuredActor2 !== null)
   }
 
-  private object ooStyleMulti { // with multiple spawned ActorRef[T]
+  private def ooStyleMulti = { // with multiple spawned ActorRef[T]
     import oo.multi._
-
-    def mainIsInjected(): MatchResult[_] = {
-      val main = newInjector(AppModule).instanceOf[Main]
-      (main.helloActor1 !== null)
-        .and(main.helloActor2 !== null)
-        .and(main.configuredActor1 !== null)
-        .and(main.configuredActor2 !== null)
-    }
+    val main = newInjector(AppModule).instanceOf[Main]
+    (main.helloActor1 !== null)
+      .and(main.helloActor2 !== null)
+      .and(main.configuredActor1 !== null)
+      .and(main.configuredActor2 !== null)
   }
 
-  private object compileTimeDI { // a sanity-check of what compile-time DI looks like
+  private def compileTimeDI = { // a sanity-check of what compile-time DI looks like
     import java.io.File
     import play.api._
     import fp._
 
-    def works(): MatchResult[_] = {
-      val environment   = Environment(new File("."), getClass.getClassLoader, Mode.Test)
-      val context       = ApplicationLoader.Context.create(environment, Map("my.config" -> "foo"))
-      val appComponents = new AppComponents(context)
-      val main          = appComponents.main
-      (main.helloActor !== null).and(main.configuredActor !== null)
-    }
+    val environment   = Environment(new File("."), getClass.getClassLoader, Mode.Test)
+    val context       = ApplicationLoader.Context.create(environment, Map("my.config" -> "foo"))
+    val appComponents = new AppComponents(context)
+    val main          = appComponents.main
+    (main.helloActor !== null).and(main.configuredActor !== null)
   }
 
   private def newInjector(bindModules: GuiceableModule): Injector = {
