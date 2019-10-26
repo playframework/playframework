6d00ab15290a99c4215f18d899362b1b144d36de
Scala Steward <me@scala-steward.org>
2019-10-26 06:25:55 +0200

    Update hibernate-validator to 6.1.0.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -106,7 +106,7 @@ object Dependencies {
   )
 
   val javaFormsDeps = Seq(
-    "org.hibernate.validator" % "hibernate-validator" % "6.0.18.Final",
+    "org.hibernate.validator" % "hibernate-validator" % "6.1.0.Final",
     ("org.springframework" % "spring-context" % springFrameworkVersion)
       .exclude("org.springframework", "spring-aop")
       .exclude("org.springframework", "spring-beans")
