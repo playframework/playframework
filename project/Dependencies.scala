2173f84bae5ab62d9c08c6ee2dc9bc98f6eafab0
Scala Steward <me@scala-steward.org>
2019-10-29 06:50:32 +0100

    Update play-ahc-ws-standalone, ... to 2.1.0-RC2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -282,7 +282,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi
 
-  val playWsStandaloneVersion = "2.1.0-M7"
+  val playWsStandaloneVersion = "2.1.0-RC2"
   val playWsDeps = Seq(
     "com.typesafe.play"                        %% "play-ws-standalone" % playWsStandaloneVersion,
     "com.typesafe.play"                        %% "play-ws-standalone-xml" % playWsStandaloneVersion,
