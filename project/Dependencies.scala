b19259943ad664c7af060660f62a7724d4ad8a5e
Scala Steward <me@scala-steward.org>
2019-11-19 08:18:15 +0100

    Update netty-reactive-streams-http to 2.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -160,7 +160,7 @@ object Dependencies {
   val nettyVersion = "4.1.43.Final"
 
   val netty = Seq(
-    "com.typesafe.netty" % "netty-reactive-streams-http" % "2.0.3",
+    "com.typesafe.netty" % "netty-reactive-streams-http" % "2.0.4",
     ("io.netty" % "netty-transport-native-epoll" % nettyVersion).classifier("linux-x86_64")
   ) ++ specs2Deps.map(_ % Test)
 
