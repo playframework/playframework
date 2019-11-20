52c5e108cf383ee91a403faa23ea23bc68d56c8b
Scala Steward <me@scala-steward.org>
2019-11-20 05:09:08 +0100

    Update play-file-watch to 1.1.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -186,7 +186,7 @@ object Dependencies {
     )
   }
 
-  val playFileWatch = "com.lightbend.play" %% "play-file-watch" % "1.1.8"
+  val playFileWatch = "com.lightbend.play" %% "play-file-watch" % "1.1.9"
 
   def runSupportDependencies(sbtVersion: String): Seq[ModuleID] = {
     (CrossVersion.binarySbtVersion(sbtVersion) match {
