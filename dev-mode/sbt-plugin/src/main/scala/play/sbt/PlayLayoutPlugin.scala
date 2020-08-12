deafaed33d5d43f7cec8e14e61ea1465bdcb53b0
Thibault Duplessis <t@lichess.org>
2020-08-12 17:10:17 +0200

    need this to compile routes

--- dev-mode/sbt-plugin/src/main/scala/play/sbt/PlayLayoutPlugin.scala
+++ dev-mode/sbt-plugin/src/main/scala/play/sbt/PlayLayoutPlugin.scala
@@ -23,6 +23,7 @@ object PlayLayoutPlugin extends AutoPlugin {
     target := baseDirectory.value / "target",
     sourceDirectory in Compile := baseDirectory.value / "app",
     sourceDirectory in Test := baseDirectory.value / "test",
+    resourceDirectory in Compile := baseDirectory.value / "conf",
     scalaSource in Compile := baseDirectory.value / "app",
     scalaSource in Test := baseDirectory.value / "test",
     javaSource in Compile := baseDirectory.value / "app",
