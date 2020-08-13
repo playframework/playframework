65494d0054a44c442e430d1510d00863978a0b44
Thibault Duplessis <t@lichess.org>
2020-08-13 11:46:51 +0200

    remove PID file (use systemd!)

--- transport/server/play-server/src/main/scala/play/core/server/ProdServerStart.scala
+++ transport/server/play-server/src/main/scala/play/core/server/ProdServerStart.scala
@@ -41,52 +41,32 @@ object ProdServerStart {
     try {
       // Read settings
       val config: ServerConfig = readServerConfigSettings(process)
-
-      // Create a PID file before we do any real work
-      val pidFile = createPidFile(process, config.configuration)
-
-      try {
-        // Start the application
-        val application: Application = {
-          val environment = Environment(config.rootDir, process.classLoader, Mode.Prod)
-          val context     = ApplicationLoader.Context.create(environment)
-          val loader      = ApplicationLoader(context)
-          loader.load(context)
-        }
-        Play.start(application)
-
-        // Start the server
-        val serverProvider = ServerProvider.fromConfiguration(process.classLoader, config.configuration)
-        val server         = serverProvider.createServer(config, application)
-
-        val phase = CoordinatedShutdown.PhaseBeforeActorSystemTerminate
-        application.coordinatedShutdown.addTask(phase, "remove-pid-file") { () =>
-          // Must delete the PID file after stopping the server not before...
-          // In case of unclean shutdown or failure, leave the PID file there!
-          pidFile.foreach(_.delete())
-          assert(pidFile.forall(!_.exists), "PID file should not exist!")
-          Future.successful(Done)
-        }
-
-        process.addShutdownHook {
-          // Only run server stop if the shutdown reason is not defined. That means the
-          // process received a SIGTERM (or other acceptable signal) instead of being
-          // stopped because of CoordinatedShutdown, for example when downing a cluster.
-          // The reason for that is we want to avoid calling coordinated shutdown from
-          // inside a JVM shutdown hook if the trigger of the JVM shutdown hook was
-          // coordinated shutdown.
-          if (application.coordinatedShutdown.shutdownReason().isEmpty) {
-            server.stop()
-          }
-        }
-
-        server
-      } catch {
-        case NonFatal(e) =>
-          // Clean up pidfile when the server fails to start
-          pidFile.foreach(_.delete())
-          throw e
+      // Start the application
+      val application: Application = {
+        val environment = Environment(config.rootDir, process.classLoader, Mode.Prod)
+        val context     = ApplicationLoader.Context.create(environment)
+        val loader      = ApplicationLoader(context)
+        loader.load(context)
       }
+      Play.start(application)
+
+      // Start the server
+      val serverProvider = ServerProvider.fromConfiguration(process.classLoader, config.configuration)
+      val server         = serverProvider.createServer(config, application)
+
+      process.addShutdownHook {
+        // Only run server stop if the shutdown reason is not defined. That means the
+        // process received a SIGTERM (or other acceptable signal) instead of being
+        // stopped because of CoordinatedShutdown, for example when downing a cluster.
+        // The reason for that is we want to avoid calling coordinated shutdown from
+        // inside a JVM shutdown hook if the trigger of the JVM shutdown hook was
+        // coordinated shutdown.
+        if (application.coordinatedShutdown.shutdownReason().isEmpty) {
+          server.stop()
+        }
+      }
+
+      server
     } catch {
       case ServerStartException(message, cause) => process.exit(message, cause)
       case NonFatal(e)                          => process.exit("Oops, cannot start the server.", Some(e))
@@ -131,28 +111,5 @@ object ProdServerStart {
       throw ServerStartException("Must provide either an HTTP or HTTPS port")
 
     ServerConfig(rootDir, httpPort, httpsPort, address, Mode.Prod, process.properties, configuration)
-  }
-
-  /**
-   * Create a pid file for the current process.
-   */
-  def createPidFile(process: ServerProcess, configuration: Configuration): Option[File] = {
-    val pidFilePath = configuration
-      .getOptional[String]("play.server.pidfile.path")
-      .getOrElse(throw ServerStartException("Pid file path not configured"))
-    if (pidFilePath == "/dev/null") None
-    else {
-      val pidFile = new File(pidFilePath).getAbsoluteFile
-      val pid     = process.pid.getOrElse(throw ServerStartException("Couldn't determine current process's pid"))
-      val out =
-        try Files.newOutputStream(pidFile.toPath, StandardOpenOption.CREATE_NEW)
-        catch {
-          case _: FileAlreadyExistsException =>
-            throw ServerStartException(s"This application is already running (or delete ${pidFile.getPath} file).")
-        }
-      try out.write(pid.getBytes)
-      finally out.close()
-      Some(pidFile)
-    }
   }
 }
