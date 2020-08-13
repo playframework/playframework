ab20ff2ab45f5b85bd6c4a09250df9942482841c
Thibault Duplessis <t@lichess.org>
2020-08-13 11:45:50 +0200

    remove build-link

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -22,7 +22,7 @@ import scala.util.control.NonFatal
 
 object BuildSettings {
 
-  val playVersion = "2.8.2-lila_0.6"
+  val playVersion = "2.8.2-lila_0.7"
 
   /** File header settings.  */
   private def fileUriRegexFilter(pattern: String): FileFilter = new FileFilter {
