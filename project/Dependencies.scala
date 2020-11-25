d7d41bee7c19cbcc24e1d003d5331df58002bb47
Scala Steward <me@scala-steward.org>
2020-11-25 07:13:52 +0100

    Update mockito-core to 3.6.28

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -48,7 +48,7 @@ object Dependencies {
 
   val guava      = "com.google.guava"         % "guava"        % "30.0-jre"
   val findBugs   = "com.google.code.findbugs" % "jsr305"       % "3.0.2" // Needed by guava
-  val mockitoAll = "org.mockito"              % "mockito-core" % "3.6.0"
+  val mockitoAll = "org.mockito"              % "mockito-core" % "3.6.28"
 
   val h2database    = "com.h2database"   % "h2"    % "1.4.200"
   val derbyDatabase = "org.apache.derby" % "derby" % "10.14.2.0"
