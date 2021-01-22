860651df71faa8458991b509536e79fb04003633
Matthias Kurz <m.kurz@irregular.at>
2021-01-22 10:30:35 +0100

    Scripted tests

---
+++ dev-mode/sbt-plugin/src/sbt-test/play-sbt-plugin/dev-mode-runtime-error-source/app/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class HomeController extends Controller {
+    public Result controllerFail() {
+        throw new RuntimeException("Exception thrown in controller");
+    }
+
+    public Result subProjectInsideFail() {
+        inside.Foo.fail();
+        return ok("should not reach this line");
+    }
+
+    public Result subProjectOutsideFail() {
+        outside.Bar.fail();
+        return ok("should not reach this line");
+    }
+}
