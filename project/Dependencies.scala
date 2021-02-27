f3c221d7cb3489f91b5baa8d56963bfae5868f83
Scala Steward <me@scala-steward.org>
2021-02-27 01:40:37 +0100

    Update guice-assistedinject to 5.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -123,7 +123,7 @@ object Dependencies {
     logback
   ).map(_ % Test)
 
-  val guiceVersion = "5.0.0"
+  val guiceVersion = "5.0.1"
   val guiceDeps = Seq(
     "com.google.inject"            % "guice"                % guiceVersion,
     "com.google.inject.extensions" % "guice-assistedinject" % guiceVersion
