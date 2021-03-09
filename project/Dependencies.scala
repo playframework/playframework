ea27fe27c26d036d94cb345c0890cbaefb105e89
Scala Steward <me@scala-steward.org>
2021-03-09 12:41:12 +0100

    Update netty-transport-native-epoll to 4.1.60.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -147,7 +147,7 @@ object Dependencies {
         sslConfig
       ) ++ scalaParserCombinators ++ specs2Deps.map(_ % Test) ++ javaTestDeps
 
-  val nettyVersion = "4.1.59.Final"
+  val nettyVersion = "4.1.60.Final"
 
   val netty = Seq(
     "com.typesafe.netty" % "netty-reactive-streams-http" % "2.0.5",
