5c33a4b543e3361277fc08d64b5903f1c9502b74
Scala Steward <me@scala-steward.org>
2021-03-16 18:42:36 +0100

    Update play-file-watch to 1.1.16
    (cherry picked from commit 08ec02315b402763ad02889a8f80218658b412dd)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -185,7 +185,7 @@ object Dependencies {
     )
   }
 
-  val playFileWatch = "com.lightbend.play" %% "play-file-watch" % "1.1.15"
+  val playFileWatch = "com.lightbend.play" %% "play-file-watch" % "1.1.16"
 
   def runSupportDependencies(sbtVersion: String): Seq[ModuleID] = {
     (CrossVersion.binarySbtVersion(sbtVersion) match {
