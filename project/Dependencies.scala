1a87308db900d3d7754024bf501215d2e6ccbbf6
Ignasi Marimon-Clos <ignasi@lightbend.com>
2021-03-24 10:27:57 +0100

    Bump Jackson to 2.11.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,8 +30,8 @@ object Dependencies {
     "org.scalacheck" %% "scalacheck"        % "1.15.3"      % Test
   )
 
-  val jacksonVersion         = "2.10.5"
-  val jacksonDatabindVersion = "2.10.5.1"
+  val jacksonVersion         = "2.11.4"
+  val jacksonDatabindVersion = jacksonVersion
   val jacksonDatabind        = Seq("com.fasterxml.jackson.core" % "jackson-databind" % jacksonDatabindVersion)
   val jacksons = Seq(
     "com.fasterxml.jackson.core"     % "jackson-core",
