810ed0b7475d813e734ae05ed76bf49ffac206ed
Sam Snarr <38705645+arid-mangoss@users.noreply.github.com>
2021-04-09 20:49:02 -0400

    moved comment left
    Hopefully, the comment has been moved left enough that it won't be split by the formatter. Let's see what the build says
    
    (cherry picked from commit 6a56b9b225c1c1e5d14e9484594cb0c68b4cf891)
    
    # Conflicts:
    #	documentation/manual/tutorial/code/javaguide/hello/HelloController.java

--- documentation/manual/tutorial/code/javaguide/hello/HelloController.java
+++ documentation/manual/tutorial/code/javaguide/hello/HelloController.java
@@ -17,11 +17,15 @@ public class HelloController extends Controller {
   // #hello-world-index-action
   public Result index() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     // ###replace:        return ok(views.html.index.render("Your new application is
     // ready."));
     return ok(javaguide.hello.html.index.render("Your new application is ready."));
 =======
     // ###replace: return ok(views.html.index.render("Your new application is ready.", assetsFinder));
+=======
+// ###replace: return ok(views.html.index.render("Your new application is ready.", assetsFinder));
+>>>>>>> 6a56b9b225 (moved comment left)
     return ok(javaguide.hello.html.index.render("Your new application is ready.", assetsFinder));
 >>>>>>> e3c37f9ed1 (Fixed `###replace:` tag that led to incorrect docs)
   }
