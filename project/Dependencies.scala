cb29163593aaf7fda418c0ec59a070d60e53960e
Matthias Kurz <m.kurz@irregular.at>
2021-08-26 13:16:21 +0200

    Fix cron jobs (repo.akka.io not used anymore)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ import buildinfo.BuildInfo
 
 object Dependencies {
   val akkaVersion: String = sys.props.getOrElse("akka.version", "2.6.14")
-  val akkaHttpVersion     = "10.1.14"
+  val akkaHttpVersion     = sys.props.getOrElse("akka.http.version", "10.1.14")
 
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.4.3"
 
