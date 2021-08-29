70ae74f196ce92ea8a18da09bcc9dfa41ba57338
Sam Snarr <38705645+arid-mangoss@users.noreply.github.com>
2021-08-29 14:01:46 -0400

    tried changing the wording so it was shorter
    (cherry picked from commit 0f38f39e66b6f217e408e2bcace80821163f314c)
    
    # Conflicts:
    #	documentation/manual/tutorial/code/javaguide/hello/HelloController.java

--- documentation/manual/tutorial/code/javaguide/hello/HelloController.java
+++ documentation/manual/tutorial/code/javaguide/hello/HelloController.java
@@ -18,6 +18,7 @@ public class HelloController extends Controller {
   public Result index() {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     // ###replace:        return ok(views.html.index.render("Your new application is
     // ready."));
     return ok(javaguide.hello.html.index.render("Your new application is ready."));
@@ -28,6 +29,10 @@ public class HelloController extends Controller {
 >>>>>>> 6a56b9b225 (moved comment left)
     return ok(javaguide.hello.html.index.render("Your new application is ready.", assetsFinder));
 >>>>>>> e3c37f9ed1 (Fixed `###replace:` tag that led to incorrect docs)
+=======
+    // ###replace: return ok(views.html.index.render("Your app is ready.", assetsFinder));
+    return ok(javaguide.hello.html.index.render("Your app is ready.", assetsFinder));
+>>>>>>> 0f38f39e66 (tried changing the wording so it was shorter)
   }
   // #hello-world-index-action
 
