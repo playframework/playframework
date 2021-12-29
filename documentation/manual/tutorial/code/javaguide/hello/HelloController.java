920f6b4590999b367b5d6611e6b5e9376d00f125
Matthias Kurz <m.kurz@irregular.at>
2021-12-30 00:16:46 +0100

    Fix conflicts

--- documentation/manual/tutorial/code/javaguide/hello/HelloController.java
+++ documentation/manual/tutorial/code/javaguide/hello/HelloController.java
@@ -16,23 +16,8 @@ public class HelloController extends Controller {
 
   // #hello-world-index-action
   public Result index() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // ###replace:        return ok(views.html.index.render("Your new application is
-    // ready."));
-    return ok(javaguide.hello.html.index.render("Your new application is ready."));
-=======
-    // ###replace: return ok(views.html.index.render("Your new application is ready.", assetsFinder));
-=======
-// ###replace: return ok(views.html.index.render("Your new application is ready.", assetsFinder));
->>>>>>> 6a56b9b225 (moved comment left)
-    return ok(javaguide.hello.html.index.render("Your new application is ready.", assetsFinder));
->>>>>>> e3c37f9ed1 (Fixed `###replace:` tag that led to incorrect docs)
-=======
-    // ###replace: return ok(views.html.index.render("Your app is ready.", assetsFinder));
-    return ok(javaguide.hello.html.index.render("Your app is ready.", assetsFinder));
->>>>>>> 0f38f39e66 (tried changing the wording so it was shorter)
+    // ###replace: return ok(views.html.index.render("Your app is ready."));
+    return ok(javaguide.hello.html.index.render("Your app is ready."));
   }
   // #hello-world-index-action
 
