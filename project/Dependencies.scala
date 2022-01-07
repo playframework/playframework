7f251a8a1be92bc288d229f1a08e64dff23a6966
Matthias Kurz <m.kurz@irregular.at>
2022-01-07 13:12:29 +0100

    Use akka snapshot for now until 2.6.19 is released

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ import Keys._
 import buildinfo.BuildInfo
 
 object Dependencies {
-  val akkaVersion: String = sys.props.getOrElse("akka.version", "2.6.18")
+  val akkaVersion: String = sys.props.getOrElse("akka.version", "2.6.18+21-4fb7bd9b-SNAPSHOT")
   val akkaHttpVersion     = sys.props.getOrElse("akka.http.version", "10.2.7")
 
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.6.0"
