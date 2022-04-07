f2077b8501ee6760612d91f9298515935a5b26ed
Matthias Kurz <m.kurz@irregular.at>
2022-04-07 14:34:11 +0200

    Spring 5.3.x dropped the RELEASE version suffix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -96,7 +96,7 @@ object Dependencies {
     case _                               => Nil
   }
 
-  val springFrameworkVersion = "5.3.18.RELEASE"
+  val springFrameworkVersion = "5.3.18"
 
   def javaDeps(scalaVersion: String) =
     Seq(
