c9a558d9d4d6a473381570d62d322b1cd478d85f
Scala Steward <me@scala-steward.org>
2022-04-09 14:30:44 +0200

    specs2-core, specs2-junit, ... 4.15.0 (was 4.13.3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.11"
 
-  val specs2Version = "4.13.3"
+  val specs2Version = "4.15.0"
   val specs2Deps = Seq(
     "specs2-core",
     "specs2-junit",
