ca6a2939c423483f5f1e4917d02ce86ff734e804
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-04-16 22:22:38 +0900

    Changed the method name of PathBindable

--- core/play/src/main/java/play/mvc/PathBindable.java
+++ core/play/src/main/java/play/mvc/PathBindable.java
@@ -18,7 +18,7 @@ package play.mvc;
  *     public String email;
  *     public String name;
  *
- *     public User bind(String key, String email) {
+ *     public User bindPath(String key, String email) {
  *         User user = findByEmail(email);
  *         if (user != null) {
  *             user;
@@ -27,11 +27,11 @@ package play.mvc;
  *         }
  *     }
  *
- *     public String unbind(String key) {
+ *     public String unbindPath(String key) {
  *         return email;
  *     }
  *
- *     public String javascriptUnbind() {
+ *     public String javascriptUnbindPath() {
  *         return "function(k,v) {\n" +
  *             "    return v.email;" +
  *             "}";
@@ -50,14 +50,14 @@ package play.mvc;
 public interface PathBindable<T extends PathBindable<T>> {
 
   /**
-   * Bind an URL path parameter.
+   * Bind a URL path parameter.
    *
    * @param key Parameter key
    * @param txt The value as String (extracted from the URL path)
    * @return The object, may be this object
    * @throws RuntimeException if this object could not be bound
    */
-  T bind(String key, String txt);
+  T bindPath(String key, String txt);
 
   /**
    * Unbind a URL path parameter.
@@ -65,12 +65,12 @@ public interface PathBindable<T extends PathBindable<T>> {
    * @param key Parameter key
    * @return a suitable string representation of T for use in constructing a new URL path
    */
-  String unbind(String key);
+  String unbindPath(String key);
 
   /**
    * Javascript function to unbind in the Javascript router.
    *
    * @return The javascript function, or null if you want to use the default implementation.
    */
-  String javascriptUnbind();
+  String javascriptUnbindPath();
 }
