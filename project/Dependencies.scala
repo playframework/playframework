27b249dca6ae203996f66cd870a7bacb15df805a
Scala Steward <scala_steward@virtuslab.com>
2022-08-11 02:05:32 +0000

    hibernate-validator 6.2.4.Final (was 6.2.3.Final)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -111,7 +111,7 @@ object Dependencies {
   )
 
   val javaFormsDeps = Seq(
-    "org.hibernate.validator" % "hibernate-validator" % "6.2.3.Final",
+    "org.hibernate.validator" % "hibernate-validator" % "6.2.4.Final",
     ("org.springframework" % "spring-context" % springFrameworkVersion)
       .exclude("org.springframework", "spring-aop")
       .exclude("org.springframework", "spring-beans")
