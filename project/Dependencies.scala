52deb3e9b8419a17aef8953da9f777b43e39bf01
Scala Steward <scala_steward@virtuslab.com>
2022-10-15 04:38:08 +0000

    caffeine, jcache 3.0.6 (was 3.0.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -276,7 +276,7 @@ object Dependencies {
     "org.ehcache"    % "jcache"  % "1.0.1"
   ) ++ jcacheApi
 
-  val caffeineVersion = "3.0.1"
+  val caffeineVersion = "3.0.6"
   val playCaffeineDeps = Seq(
     "com.github.ben-manes.caffeine" % "caffeine" % caffeineVersion,
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
