fe97191b5c5a27a0704e725adc3254a851114793
Matthias Kurz <m.kurz@irregular.at>
2022-10-17 11:25:46 +0200

    Upgrade joda time as well

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -106,7 +106,7 @@ object Dependencies {
     ) ++ specs2Deps.map(_ % Test)
 
   val joda = Seq(
-    "joda-time" % "joda-time"    % "2.10.13",
+    "joda-time" % "joda-time"    % "2.10.14",
     "org.joda"  % "joda-convert" % "2.2.2"
   )
 
