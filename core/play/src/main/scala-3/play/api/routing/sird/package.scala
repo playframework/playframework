3153e46be2386f8b407620591ed5ef2c019c7a03
João Ferreira <jtjeferreira@gmail.com>
2022-11-19 23:16:32 +0000

    fix mima. is more duplication worth it?

--- core/play/src/main/scala-3/play/api/routing/sird/package.scala
+++ core/play/src/main/scala-3/play/api/routing/sird/package.scala
@@ -2,12 +2,51 @@
  * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
 
-package play.api.routing.sird
+package play.api.routing
 
-import scala.quoted._
+import scala.language.experimental.macros
 
-trait SirdMacros {
-  extension (inline sc: StringContext) {
+/**
+ * The Play "String Interpolating Routing DSL", sird for short.
+ *
+ * This provides:
+ * - Extractors for requests that extract requests by method, eg GET, POST etc.
+ * - A string interpolating path extractor
+ * - Extractors for binding parameters from paths to various types, eg int, long, double, bool.
+ *
+ * The request method extractors return the original request for further extraction.
+ *
+ * The path extractor supports three kinds of extracted values:
+ * - Path segment values. This is the default, eg `p"/foo/\$id"`. The value will be URI decoded, and may not traverse /'s.
+ * - Full path values. This can be indicated by post fixing the value with a *, eg `p"/assets/\$path*"`. The value will
+ * not be URI decoded, as that will make it impossible to distinguish between / and %2F.
+ * - Regex values. This can be indicated by post fixing the value with a regular expression enclosed in angle brackets.
+ * For example, `p"/foo/\$id<[0-9]+>`. The value will not be URI decoded.
+ *
+ * The extractors for primitive types are merely provided for convenience, for example, `p"/foo/\${int(id)}"` will
+ * extract `id` as an integer.  If `id` is not an integer, the match will simply fail.
+ *
+ * Example usage:
+ *
+ * {{{
+ *  import play.api.routing.sird._
+ *  import play.api.routing._
+ *  import play.api.mvc._
+ *
+ *  Router.from {
+ *    case GET(p"/hello/\$to") => Action {
+ *      Results.Ok(s"Hello \$to")
+ *    }
+ *    case PUT(p"/api/items/\${int(id)}") => Action.async { req =>
+ *      Items.save(id, req.body.json.as[Item]).map { _ =>
+ *        Results.Ok(s"Saved item \$id")
+ *      }
+ *    }
+ *  }
+ * }}}
+ */
+package object sird extends RequestMethodExtractors with PathBindableExtractors {
+  extension(inline sc: StringContext) {
 
     /**
      * String interpolator for required query parameters out of query strings.
@@ -62,4 +101,22 @@ trait SirdMacros {
      */
     val p: PathExtractor = PathExtractor.cached(sc.parts)
   }
+
+  /**
+   * Allow multiple parameters to be extracted
+   */
+  object & {
+    def unapply[A](a: A): Option[(A, A)] =
+      Some((a, a))
+  }
+
+  /**
+   * Same as &, but for convenience to make the dsl look nicer when extracting query strings
+   */
+  val ? = &
+
+  /**
+   * The query string type
+   */
+  type QueryString = Map[String, Seq[String]]
 }
