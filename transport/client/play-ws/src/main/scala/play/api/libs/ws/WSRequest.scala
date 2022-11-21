cfa7fa3c9abcc923c9d2eab18adda5fae20d8f05
João Ferreira <jtjeferreira@gmail.com>
2022-11-21 18:36:01 +0000

    scala2/scala3 difference https://github.com/lampepfl/dotty/issues/4770#issuecomment-403090584
    make mima happy

--- transport/client/play-ws/src/main/scala/play/api/libs/ws/WSRequest.scala
+++ transport/client/play-ws/src/main/scala/play/api/libs/ws/WSRequest.scala
@@ -36,46 +36,6 @@ trait WSRequest extends StandaloneWSRequest with WSBodyWritables {
   override def withHttpHeaders(headers: (String, String)*): Self
 
   /**
-   * Returns this request with the given headers, preserving the existing ones.
-   *
-   * @param hdrs the headers to be added
-   */
-  //TODO scala3 compiler bug?
-  /*[error] 21 |case class AhcWSRequest(underlying: StandaloneAhcWSRequest) extends WSRequest with WSBodyWritables {
-  [error]    |           ^
-  [error]    |class AhcWSRequest needs to be abstract, since:
-  [error]    |it has 4 unimplemented members.
-  [error]    |/** As seen from class AhcWSRequest, the missing signatures are as follows.
-  [error]    | *  For convenience, these are usable as stub implementations.
-  [error]    | */
-  [error]    |  override def addHttpHeaders(hdrs: Seq[(String, String)]): AhcWSRequest.this.Self = ???
-  [error]    |  override def addQueryStringParameters
-  [error]    |  (parameters: Seq[(String, String)]): AhcWSRequest.this.Self = ???
-  [error]    |  override def header(name: String): Option[String] = ???
-  [error]    |  override def headerValues(name: String): Seq[String] = ???
-   */
-  //override def addHttpHeaders(hdrs: (String, String)*): Self
-
-  /**
-   * Get the value of the header with the specified name. If there are more than one values
-   * for this header, the first value is returned. If there are no values, than a None is
-   * returned.
-   *
-   * @param name the header name
-   * @return the header value
-   */
-  //override def header(name: String): Option[String]
-
-  /**
-   * Get all the values of header with the specified name. If there are no values for
-   * the header with the specified name, than an empty sequence is returned.
-   *
-   * @param name the header name.
-   * @return all the values for this header name.
-   */
-  //override def headerValues(name: String): Seq[String]
-
-  /**
    * Returns this request with the given query string parameters, adding to the existing ones.
    *
    * @param parameters the query string parameters
@@ -89,13 +49,6 @@ trait WSRequest extends StandaloneWSRequest with WSBodyWritables {
    * @param parameters the query string parameters
    */
   override def withQueryStringParameters(parameters: (String, String)*): Self
-
-  /**
-   * Returns this request with the given query string parameters, preserving the existing ones.
-   *
-   * @param parameters the query string parameters
-   */
-  //override def addQueryStringParameters(parameters: (String, String)*): Self
 
   /**
    * Returns this request with the given cookies, preserving the existing ones.
