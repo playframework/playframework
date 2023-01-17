5c3858c038c0504d376124942162db7d47673ab8
Matthias Kurz <m.kurz@irregular.at>
2023-01-17 17:30:20 +0100

    Last touch: fixing comments + use constant for status code

--- web/play-filters-helpers/src/main/scala/play/filters/ip/IPFilter.scala
+++ web/play-filters-helpers/src/main/scala/play/filters/ip/IPFilter.scala
@@ -71,7 +71,7 @@ object IPFilterConfig {
    */
   def fromConfiguration(conf: Configuration): IPFilterConfig = {
     val ipConfig                   = conf.get[Configuration]("play.filters.ip")
-    val accessDeniedHttpStatusCode = ipConfig.getOptional[Int]("accessDeniedHttpStatusCode").getOrElse(403)
+    val accessDeniedHttpStatusCode = ipConfig.getOptional[Int]("accessDeniedHttpStatusCode").getOrElse(Status.FORBIDDEN)
     val whiteList =
       ipConfig.getOptional[Seq[String]]("whiteList").getOrElse(Seq.empty).map(InetAddress.getByName(_).getAddress())
     val blackList =
