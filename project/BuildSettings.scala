c7572265138f5a7a266dc0b1e38b4bdfe3c22e95
João Ferreira <joao.ferreira@zego.com>
2023-01-30 17:47:52 +0000

    remove scala 3.3.0 workaround after the interplay upgrade

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -433,18 +433,6 @@ object BuildSettings {
       .settings(
         scalacOptions += "-release:11"
       )
-      .settings(
-        // FIXME this was copied from interplay PlayLibraryBase plugin
-        // remove when interplay is updated to a version using scala 3.3.0
-        // format: off
-        scalaVersion := (Seq(ScalaVersions.scala213, "3.3.0-RC1")
-          .filter(v => SemanticSelector(sys.props.get("scala.version").getOrElse(ScalaVersions.scala213)).matches(VersionNumber(v))) match {
-          case Nil => sys.error("Unable to detect scalaVersion!")
-          case Seq(version) => version
-          case multiple => sys.error(s"Multiple crossScalaVersions matched query '${sys.props("scala.version")}': ${multiple.mkString(", ")}")
-        }),
-        // format: on
-      )
   }
 
   def omnidocSettings: Seq[Setting[_]] = Def.settings(
