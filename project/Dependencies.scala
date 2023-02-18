a50bb159d97fff89300e7a0d212aa6920972c12c
Scala Steward <scala_steward@virtuslab.com>
2023-02-18 18:40:57 +0000

    spring-beans, spring-context, ... 6.0.17 (was 5.3.25)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -106,7 +106,7 @@ object Dependencies {
       }
     })
 
-  val springFrameworkVersion = "5.3.32"
+  val springFrameworkVersion = "6.0.17"
 
   val javaDeps = Seq(
     // Used by the Java routing DSL
