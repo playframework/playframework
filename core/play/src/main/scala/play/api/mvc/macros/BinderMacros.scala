b74d7c60b2845dc3ee479f123d7d92743b76e3c5
Matthias Kurz <m.kurz@irregular.at>
2023-03-15 23:06:42 +0100

    anyValPathBindable should make use transform method
    (cherry picked from commit b3170d37f5a49b99f375ceb43a859086005fb944)
    
    # Conflicts:
    #	core/play/src/main/scala/play/api/mvc/macros/BinderMacros.scala

--- core/play/src/main/scala/play/api/mvc/macros/BinderMacros.scala
+++ core/play/src/main/scala/play/api/mvc/macros/BinderMacros.scala
@@ -14,6 +14,7 @@ class BinderMacros(val c: MacroContext) {
       // currently we do not need to invoke `import _root_.play.api.mvc.PathBindable._`
       // since we are in the same package
       q"""
+<<<<<<< HEAD
          new _root_.play.api.mvc.PathBindable[${t.tpe}] {
            private val binder = _root_.scala.Predef.implicitly[_root_.play.api.mvc.PathBindable[${param.typeSignature}]]
            override def bind(key: String, value: String): Either[String, ${t.tpe}] = {
@@ -25,6 +26,10 @@ class BinderMacros(val c: MacroContext) {
            }
 
          }
+=======
+         private val binder = _root_.scala.Predef.implicitly[_root_.play.api.mvc.PathBindable[${param.typeSignature}]]
+         binder.transform((p: ${param.typeSignature}) => new ${t.tpe}(p), (p: ${t.tpe}) => p.${param.name.toTermName})
+>>>>>>> b3170d37f5 (anyValPathBindable should make use transform method)
        """
     }.getOrElse(fail("PathBindable", t.tpe))
   }
