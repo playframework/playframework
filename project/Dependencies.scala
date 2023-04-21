b885ba29eb78839b66dca0614fdafa398c2dd925
Scala Steward <scala_steward@virtuslab.com>
2023-04-21 18:20:28 +0000

    joda-time 2.12.5 (was 2.12.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -131,7 +131,7 @@ object Dependencies {
   ) ++ specs2Deps.map(_ % Test)
 
   val joda = Seq(
-    "joda-time" % "joda-time"    % "2.12.2",
+    "joda-time" % "joda-time"    % "2.12.5",
     "org.joda"  % "joda-convert" % "2.2.3"
   )
 
