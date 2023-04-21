2d64d4ba95aa9daf1f6896fde6dc506d93913d59
Scala Steward <scala_steward@virtuslab.com>
2023-04-21 18:24:56 +0000

    selenium-api 4.8.3 (was 4.8.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -276,7 +276,7 @@ object Dependencies {
   val fluentleniumVersion = "5.0.4"
   // This is the selenium version compatible with the FluentLenium version declared above.
   // See http://mvnrepository.com/artifact/org.fluentlenium/fluentlenium-core/5.0.4
-  val seleniumVersion = "4.8.1"
+  val seleniumVersion = "4.8.3"
 
   val testDependencies = Seq(junit, junitInterface, guava, findBugs, logback) ++ Seq(
     ("org.fluentlenium" % "fluentlenium-core" % fluentleniumVersion).exclude("org.jboss.netty", "netty"),
