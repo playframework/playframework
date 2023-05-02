70f89e49ef03f4705cf82e5afb22662f1300da91
Matthias Kurz <m.kurz@irregular.at>
2023-05-02 11:55:31 +0200

    Guice 6.0.0-rc1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -162,7 +162,7 @@ object Dependencies {
     logback
   ).map(_ % Test)
 
-  val guiceVersion = "5.1.0"
+  val guiceVersion = "6.0.0-rc1"
   val guiceDeps = Seq(
     "com.google.inject"            % "guice"                % guiceVersion,
     "com.google.inject.extensions" % "guice-assistedinject" % guiceVersion
