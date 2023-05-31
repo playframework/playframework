d56eb953d852eff17961d24425cb34b3d07e4a27
Alexey <samtnt@yandex.ru>
2023-05-31 19:17:59 +0300

    Fix class field dependency injection example java files

--- documentation/manual/working/javaGuide/main/dependencyinjection/code/javaguide/di/guice/classfield/NoopCounter.java
+++ documentation/manual/working/javaGuide/main/dependencyinjection/code/javaguide/di/guice/classfield/NoopCounter.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
 package javaguide.di.guice.classfield;
 
 //#class-field-dependency-injection
