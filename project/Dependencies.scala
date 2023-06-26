c7a349b15c50a558b73db2c32a1b0758240cfbde
xuwei-k <6b656e6a69@gmail.com>
2023-06-26 14:25:01 +0900

    Update akka to 2.6.21
    - https://github.com/akka/akka/compare/v2.6.20...v2.6.21
    - https://github.com/akka/akka/releases/tag/v2.6.21
    
    (cherry picked from commit 7956fa42c5a1ce89e045bc398b3ff607a786f4df)
    
    # Conflicts:
    #	project/Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ import Keys._
 import buildinfo.BuildInfo
 
 object Dependencies {
-  val akkaVersion: String = sys.props.getOrElse("akka.version", "2.6.20")
+  val akkaVersion: String = sys.props.getOrElse("akka.version", "2.6.21")
   val akkaHttpVersion     = sys.props.getOrElse("akka.http.version", "10.1.15")
 
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.4.3"
