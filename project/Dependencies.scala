77da5618282dd4c1ed956c26062b98f865ac534a
Scala Steward <m.kurz@irregular.at>
2023-07-11 19:25:27 +0200

    Update hibernate-core to 5.4.33 in 2.8.x

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -82,7 +82,7 @@ object Dependencies {
 
   val jpaDeps = Seq(
     "org.hibernate.javax.persistence" % "hibernate-jpa-2.1-api" % "1.0.2.Final",
-    "org.hibernate"                   % "hibernate-core"        % "5.4.32.Final" % "test"
+    "org.hibernate"                   % "hibernate-core"        % "5.4.33.Final" % "test"
   )
 
   def scalaReflect(scalaVersion: String) = "org.scala-lang" % "scala-reflect" % scalaVersion % "provided"
