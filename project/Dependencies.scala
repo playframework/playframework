78f3ca0454a0be74c087dab754a2f253d43c3e19
Matthias Kurz <m.kurz@irregular.at>
2023-07-11 20:38:53 +0200

    Upgrade netty-reactive-streams-http to 2.0.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -165,7 +165,7 @@ object Dependencies {
   val nettyVersion = "4.1.94.Final"
 
   val netty = Seq(
-    "com.typesafe.netty" % "netty-reactive-streams-http"  % "2.0.8",
+    "com.typesafe.netty" % "netty-reactive-streams-http"  % "2.0.9",
     ("io.netty"          % "netty-transport-native-epoll" % nettyVersion).classifier("linux-x86_64")
   ) ++ specs2Deps.map(_ % Test)
 
