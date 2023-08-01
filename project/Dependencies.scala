bf6a877dbfc02dc521d7280372cb651bcf6432f1
Scala Steward <scala_steward@virtuslab.com>
2023-08-01 17:59:16 +0000

    selenium-api 4.11.0 (was 4.10.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -275,7 +275,7 @@ object Dependencies {
   val fluentleniumVersion = "6.0.0"
   // This is the selenium version compatible with the FluentLenium version declared above.
   // See https://repo1.maven.org/maven2/io/fluentlenium/fluentlenium-parent/6.0.0/fluentlenium-parent-6.0.0.pom
-  val seleniumVersion = "4.10.0"
+  val seleniumVersion = "4.11.0"
 
   val testDependencies = Seq(junit, junitInterface, guava, logback) ++ Seq(
     ("io.fluentlenium" % "fluentlenium-core" % fluentleniumVersion).exclude("org.jboss.netty", "netty"),
