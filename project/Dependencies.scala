9461e06596f45b01dba5d662360aa90dee7a827f
Matthias Kurz <m.kurz@irregular.at>
2023-10-07 12:32:23 +0200

    play-ws milestone which switches everything to org.playframework

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -284,7 +284,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi
 
-  val playWsStandaloneVersion = "3.0.0-M2"
+  val playWsStandaloneVersion = "3.0.0-M3"
   val playWsDeps = Seq(
     "org.playframework" %% "play-ws-standalone"      % playWsStandaloneVersion,
     "org.playframework" %% "play-ws-standalone-xml"  % playWsStandaloneVersion,
