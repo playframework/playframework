5c477efa17c7ee67151a709f83d2598d5c92cadd
Scala Steward <scala_steward@virtuslab.com>
2023-10-25 00:23:49 +0000

    htmlunit-driver 2.70.0 (was 2.36.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -261,7 +261,7 @@ object Dependencies {
     // slowing down the build. So the open range deps were removed and we can re-add
     // them using a specific version. Using an open range is also not good for the
     // local cache.
-    ("org.seleniumhq.selenium" % "htmlunit-driver" % "2.36.0").excludeAll(
+    ("org.seleniumhq.selenium" % "htmlunit-driver" % "2.70.0").excludeAll(
       ExclusionRule("org.seleniumhq.selenium", "selenium-api"),
       ExclusionRule("org.seleniumhq.selenium", "selenium-support")
     ),
