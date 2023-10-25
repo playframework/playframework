082be8764e448d06e24ea1ab04c3a5dcef6e167b
Scala Steward <scala_steward@virtuslab.com>
2023-10-25 00:14:51 +0000

    guice 7.0.0 (was 4.2.3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -137,7 +137,7 @@ object Dependencies {
     logback
   ).map(_ % Test)
 
-  val guiceVersion = "4.2.3"
+  val guiceVersion = "7.0.0"
   val guiceDeps = Seq(
     "com.google.inject"            % "guice"                % guiceVersion,
     "com.google.inject.extensions" % "guice-assistedinject" % guiceVersion
