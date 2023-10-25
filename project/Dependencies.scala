072465cd1ca29af031fc439bfc9b73c57ee652c7
Scala Steward <scala_steward@virtuslab.com>
2023-10-25 00:14:34 +0000

    guava 32.1.3-jre (was 30.1.1-jre)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
   val slf4j        = Seq("slf4j-api", "jul-to-slf4j", "jcl-over-slf4j").map("org.slf4j" % _ % slf4jVersion)
   val slf4jSimple  = "org.slf4j" % "slf4j-simple" % slf4jVersion
 
-  val guava      = "com.google.guava"         % "guava"        % "30.1.1-jre"
+  val guava      = "com.google.guava"         % "guava"        % "32.1.3-jre"
   val findBugs   = "com.google.code.findbugs" % "jsr305"       % "3.0.2" // Needed by guava
   val mockitoAll = "org.mockito"              % "mockito-core" % "3.2.4"
 
