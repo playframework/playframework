e4872d4f024ce48c3d86294381c45e4f4df98041
Alex Boxall <alexboxall3@gmail.com>
2023-11-07 16:39:08 +1100

    Changed are now formatted correctly

--- core/play/src/main/java/play/mvc/Http.java
+++ core/play/src/main/java/play/mvc/Http.java
@@ -2364,7 +2364,9 @@ public class Http {
         return key;
       }
 
-      /** @return the sanitized version of the file name (i.e. only the filename, no path components) */
+      /**
+       * @return the sanitized version of the file name (i.e. only the filename, no path components)
+       */
       public String getSanitizedFilename() {
         try {
           // Will throw InvalidPathException on invalid filepaths
