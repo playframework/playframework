cbc2c98afa381305e46a37c225ec7be6510b1a9b
Matthias Kurz <m.kurz@irregular.at>
2023-12-07 12:37:36 +0100

    latest netty-reactive-streams with latest netty

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -171,7 +171,7 @@ object Dependencies {
   val nettyVersion = "4.1.101.Final"
 
   val netty = Seq(
-    "org.playframework.netty" % "netty-reactive-streams-http"  % "3.0.0",
+    "org.playframework.netty" % "netty-reactive-streams-http"  % "3.0.1",
     ("io.netty"               % "netty-transport-native-epoll" % nettyVersion).classifier("linux-x86_64")
   ) ++ specs2Deps.map(_ % Test)
 
