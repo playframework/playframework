e5cccf4583a1f10d112950999e820ff8d819641d
Scala Steward <scala_steward@virtuslab.com>
2024-02-22 00:58:18 +0000

    sbt-web 1.5.4 (was 1.5.3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -214,7 +214,7 @@ object Dependencies {
       playFileWatch,
       sbtDep("org.playframework.twirl" % "sbt-twirl"           % BuildInfo.sbtTwirlVersion),
       sbtDep("com.github.sbt"          % "sbt-native-packager" % BuildInfo.sbtNativePackagerVersion),
-      sbtDep("com.github.sbt"          % "sbt-web"             % "1.5.3"),
+      sbtDep("com.github.sbt"          % "sbt-web"             % "1.5.4"),
       sbtDep("com.github.sbt"          % "sbt-js-engine"       % "1.3.5"),
       logback % Test
     ) ++ specs2Deps.map(_ % Test) ++ scalaReflect(scalaVersion)
