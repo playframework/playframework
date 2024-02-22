6dc95c6e1c42d52d13669bf6f7b60e83b433e73f
Scala Steward <scala_steward@virtuslab.com>
2024-02-22 00:54:34 +0000

    hibernate-core 6.4.4.Final (was 6.4.1.Final)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -91,7 +91,7 @@ object Dependencies {
 
   val jpaDeps = Seq(
     "jakarta.persistence" % "jakarta.persistence-api" % "3.1.0",
-    "org.hibernate"       % "hibernate-core"          % "6.4.1.Final" % "test"
+    "org.hibernate"       % "hibernate-core"          % "6.4.4.Final" % "test"
   )
 
   def scalaReflect(scalaVersion: String) = CrossVersion.partialVersion(scalaVersion) match {
