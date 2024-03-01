1efa80b8f292512d84208bc052cebbdb4e155cad
Matthias Kurz <m.kurz@irregular.at>
2024-03-01 14:01:37 +0100

    Upgrade sbt-js-engine, sbt-web, webjars-locator

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -221,8 +221,8 @@ object Dependencies {
       playFileWatch,
       sbtDep("org.playframework.twirl" % "sbt-twirl"           % BuildInfo.sbtTwirlVersion),
       sbtDep("com.github.sbt"          % "sbt-native-packager" % BuildInfo.sbtNativePackagerVersion),
-      sbtDep("com.github.sbt"          % "sbt-web"             % "1.5.4"),
-      sbtDep("com.github.sbt"          % "sbt-js-engine"       % "1.3.5"),
+      sbtDep("com.github.sbt"          % "sbt-web"             % "1.5.5"),
+      sbtDep("com.github.sbt"          % "sbt-js-engine"       % "1.3.6"),
       logback % Test
     ) ++ specs2Deps.map(_ % Test) ++ scalaReflect(scalaVersion)
   }
