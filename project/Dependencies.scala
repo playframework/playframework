c84b0f467e601bd214f5addc44aa11e7e93892f9
Scala Steward <scala_steward@virtuslab.com>
2024-03-24 17:39:54 +0000

    [2.9.x] mockito-core 5.11.0 (was 5.6.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
   val slf4jSimple  = "org.slf4j" % "slf4j-simple" % slf4jVersion
 
   val guava      = "com.google.guava" % "guava"        % "32.1.3-jre"
-  val mockitoAll = "org.mockito"      % "mockito-core" % "5.6.0"
+  val mockitoAll = "org.mockito"      % "mockito-core" % "5.11.0"
 
   val javaxInject = "javax.inject" % "javax.inject" % "1"
 
