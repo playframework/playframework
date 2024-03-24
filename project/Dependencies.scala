c8c1540fe6f05de645807a6f441360fdc1669de5
Scala Steward <scala_steward@virtuslab.com>
2024-03-24 17:40:40 +0000

    [2.9.x] play-ahc-ws-standalone 2.2.6 (was 2.2.5)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -317,7 +317,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi
 
-  val playWsStandaloneVersion = "2.2.5"
+  val playWsStandaloneVersion = "2.2.6"
   val playWsDeps = Seq(
     ("com.typesafe.play" %% "play-ws-standalone"      % playWsStandaloneVersion).forScala3TestsExcludeAkkaOrganization(),
     ("com.typesafe.play" %% "play-ws-standalone-xml"  % playWsStandaloneVersion).forScala3TestsExcludeAkkaOrganization(),
