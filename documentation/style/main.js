1d1cbbf0509252c8bea433ab28680102cbd9391f
pckrishnadas88 <pckrishnadas88@gmail.com>
2024-04-14 15:00:28 +0530

    button scroll issue fix for code snippets.

--- documentation/style/main.js
+++ documentation/style/main.js
@@ -2,24 +2,25 @@
  * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
 
-$(function() {
-
+$(function () {
   // Tabbed code samples
 
-  $("dl").has("dd > pre").each(function() {
-    var dl = $(this);
-    dl.addClass("tabbed");
-    dl.find("dt").each(function(i) {
-      var dt = $(this);
-      dt.html("<a href=\"#tab" + i + "\">" + dt.text() + "</a>");
+  $("dl")
+    .has("dd > pre")
+    .each(function () {
+      var dl = $(this);
+      dl.addClass("tabbed");
+      dl.find("dt").each(function (i) {
+        var dt = $(this);
+        dt.html('<a href="#tab' + i + '">' + dt.text() + "</a>");
+      });
+      dl.find("dd").hide();
+      var current = dl.find("dt:first").addClass("current");
+      var currentContent = current.next("dd").show();
+      dl.css("height", current.height() + currentContent.height());
     });
-    dl.find("dd").hide();
-    var current = dl.find("dt:first").addClass("current");
-    var currentContent = current.next("dd").show();
-    dl.css("height", current.height() + currentContent.height());
-  });
 
-  $("dl.tabbed dt a").click(function(e){
+  $("dl.tabbed dt a").click(function (e) {
     e.preventDefault();
     var current = $(this).parent("dt");
     var dl = current.parent("dl");
@@ -28,5 +29,4 @@ $(function() {
     var currentContent = current.next("dd").show();
     dl.css("height", current.height() + currentContent.height());
   });
-
 });
