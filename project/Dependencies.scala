59d794752a357c8e09ec69db375fd126b68b2738
Scala Steward <m.kurz@irregular.at>
2024-04-30 11:52:27 +0200

    [3.0.x] spring-core 5.3.34 (was 5.3.33)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -106,7 +106,7 @@ object Dependencies {
       }
     })
 
-  val springFrameworkVersion = "5.3.33"
+  val springFrameworkVersion = "5.3.34"
 
   val javaDeps = Seq(
     // Used by the Java routing DSL
