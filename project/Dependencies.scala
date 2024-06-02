3d295f12cfe030bcc2c0d132b8b5e399fe1a4ccb
Scala Steward <m.kurz@irregular.at>
2024-06-02 23:42:39 +0200

    [2.8.x] junit-interface 0.13.3 (was 0.11)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -126,7 +126,7 @@ object Dependencies {
       .exclude("org.springframework", "spring-core")
   ) ++ specs2Deps.map(_ % Test)
 
-  val junitInterface = "com.novocode" % "junit-interface" % "0.11"
+  val junitInterface = "com.github.sbt" % "junit-interface" % "0.13.3"
   val junit          = "junit"        % "junit"           % "4.13.2"
 
   val javaTestDeps = Seq(
