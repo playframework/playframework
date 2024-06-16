59b237abe1dfc48e8a53a9d73e990511220a65bc
Scala Steward <m.kurz@irregular.at>
2024-06-16 17:37:45 +0200

    [2.9.x] sbt-web 1.5.8 (was 1.5.6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -243,7 +243,7 @@ object Dependencies {
       playFileWatch,
       sbtDep("com.typesafe.play" % "sbt-twirl"           % BuildInfo.sbtTwirlVersion),
       sbtDep("com.github.sbt"    % "sbt-native-packager" % BuildInfo.sbtNativePackagerVersion),
-      sbtDep("com.github.sbt"    % "sbt-web"             % "1.5.6"),
+      sbtDep("com.github.sbt"    % "sbt-web"             % "1.5.8"),
       sbtDep("com.github.sbt"    % "sbt-js-engine"       % "1.3.9"),
       logback % Test
     ) ++ specs2Deps.map(_ % Test) ++ scalaReflect(scalaVersion)
