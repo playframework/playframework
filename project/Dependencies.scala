fdef499cd8fdd81c5d1bed57b6467f8e369ac466
Scala Steward <scala_steward@virtuslab.com>
2024-09-14 17:00:52 +0000

    [3.0.x] logback-classic 1.5.8 (was 1.5.6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   val playJsonVersion = "3.0.4"
 
-  val logback = "ch.qos.logback" % "logback-classic" % "1.5.6"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.5.8"
 
   val specs2Version = "4.20.8"
   val specs2Deps = Seq(
