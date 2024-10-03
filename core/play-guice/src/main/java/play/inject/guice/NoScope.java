c5170f052bb54e57e7c6c57997939f4002a07b6e
Rommel Rico <rommel@chatmeter.com>
2024-10-03 14:57:53 -0700

    Adds NoScope Java annotation

---
+++ core/play-guice/src/main/java/play/inject/guice/NoScope.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package play.inject.guice;
+
+import jakarta.inject.Scope;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Custom scope annotation to represent the absence of any specific scope.
+ *
+ * This annotation is created because Play Framework's GuiceInjectorBuilder
+ * requires a scope to be defined with an annotation. Since Guice's NO_SCOPE
+ * is not bound to an annotation by default, we define @NoScope to represent
+ * a "no-scope" condition. It will be detected by the builder to apply the
+ * desired behavior for unscoped bindings.
+ */
+@Scope
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoScope {}
