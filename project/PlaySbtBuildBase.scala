b1334a5798573bf9cdd379bcec9bccba73c3ebbd
Billy Autrey <40704452+BillyAutrey@users.noreply.github.com>
2024-10-09 14:35:40 -0500

    Add crossbuild support

--- project/PlaySbtBuildBase.scala
+++ project/PlaySbtBuildBase.scala
@@ -12,8 +12,16 @@ object PlaySbtBuildBase extends AutoPlugin {
 
   override def projectSettings = Seq(
     scalaVersion                  := ScalaVersions.scala212,
-    crossScalaVersions            := Seq(ScalaVersions.scala212),
-    pluginCrossBuild / sbtVersion := SbtVersions.sbt111,
+    crossScalaVersions            := Seq(ScalaVersions.scala212, ScalaVersions.scala3),
+    pluginCrossBuild / sbtVersion := {
+      //SbtVersions.sbt110
+      scalaBinaryVersion.value match {
+        case "2.12" =>
+          SbtVersions.sbt111
+        case _ =>
+          SbtVersions.sbt2
+      }
+    },
     compile / javacOptions ++= Seq("--release", "17"),
     doc / javacOptions := Seq("-source", "17")
   )
