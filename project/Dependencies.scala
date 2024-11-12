ee792807b36e48efff65329d6808cf224ec2b402
Scala Steward <m.kurz@irregular.at>
2024-11-12 11:05:30 +0100

    [2.9.x] joda-convert 3.0.1 (was 2.2.4)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -131,7 +131,7 @@ object Dependencies {
 
   val joda = Seq(
     "joda-time" % "joda-time"    % "2.12.7",
-    "org.joda"  % "joda-convert" % "2.2.4"
+    "org.joda"  % "joda-convert" % "3.0.1"
   )
 
   val javaFormsDeps = Seq(
