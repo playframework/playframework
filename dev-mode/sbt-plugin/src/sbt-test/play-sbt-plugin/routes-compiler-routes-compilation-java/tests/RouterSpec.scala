b0e40128958d8bc103415bb4cd0cef25ab73761b
Sergey Morgunov <hostage@me.com>
2024-11-18 16:00:00 +0300

    Drop routes, related with Scala types

--- dev-mode/sbt-plugin/src/sbt-test/play-sbt-plugin/routes-compiler-routes-compilation-java/tests/RouterSpec.scala
+++ dev-mode/sbt-plugin/src/sbt-test/play-sbt-plugin/routes-compiler-routes-compilation-java/tests/RouterSpec.scala
@@ -70,19 +70,19 @@ object RouterSpec extends PlaySpecification {
     "from a list of numbers" in new WithApplication() {
       override def running() = {
         val result =
-          route(implicitApp, FakeRequest(GET, controllers.routes.Application.takeListInt(List(1, 2, 3)).url)).get
+          route(implicitApp, FakeRequest(GET, controllers.routes.Application.takeJavaListInteger(java.util.List.of(1, 2, 3)).url)).get
         contentAsString(result) must equalTo("1,2,3")
       }
     }
     "from a list of numbers and letters" in new WithApplication() {
       override def running() = {
-        val result = route(implicitApp, FakeRequest(GET, "/take-slist-int?x=1&x=a&x=2")).get
+        val result = route(implicitApp, FakeRequest(GET, "/take-jlist-jint?x=1&x=a&x=2")).get
         status(result) must equalTo(BAD_REQUEST)
       }
     }
     "when there is no parameter at all" in new WithApplication() {
       override def running() = {
-        val result = route(implicitApp, FakeRequest(GET, "/take-slist-int")).get
+        val result = route(implicitApp, FakeRequest(GET, "/take-jlist-jint")).get
         contentAsString(result) must equalTo("")
       }
     }
@@ -166,20 +166,6 @@ object RouterSpec extends PlaySpecification {
     }
   )
   testQueryParamBinding(
-    "Option[String]",
-    "take-str-opt",
-    "x=xyz",
-    "xyz", // calls takeStringOption(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("emptyOption")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
     "java.util.Optional[String]",
     "take-str-jopt",
     "x=xyz",
@@ -190,34 +176,6 @@ object RouterSpec extends PlaySpecification {
     },
     whenNoParam = result => {
       contentAsString(result) must equalTo("emptyOptional")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "Char",
-    "take-char",
-    "x=z",
-    "z", // calls takeChar(...)
-    whenNoValue = result => {
-      contentAsString(result) must contain("Missing parameter: x")
-      status(result) must equalTo(BAD_REQUEST)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must contain("Missing parameter: x")
-      status(result) must equalTo(BAD_REQUEST)
-    }
-  )
-  testQueryParamBinding(
-    "Option[Char]",
-    "take-char-opt",
-    "x=z",
-    "z", // calls takeCharOption(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("emptyOption")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("emptyOption")
       status(result) must equalTo(OK)
     }
   )
@@ -240,34 +198,6 @@ object RouterSpec extends PlaySpecification {
     "take-jchar-jopt",
     "x=z",
     "z", // calls takeCharacterOptional(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("emptyOption")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("emptyOption")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "Short",
-    "take-short",
-    "x=789",
-    "789", // calls takeShort(...)
-    whenNoValue = result => {
-      contentAsString(result) must contain("Missing parameter: x")
-      status(result) must equalTo(BAD_REQUEST)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must contain("Missing parameter: x")
-      status(result) must equalTo(BAD_REQUEST)
-    }
-  )
-  testQueryParamBinding(
-    "Option[Short]",
-    "take-short-opt",
-    "x=789",
-    "789", // calls takeShortOption(...)
     whenNoValue = result => {
       contentAsString(result) must equalTo("emptyOption")
       status(result) must equalTo(OK)
@@ -302,34 +232,6 @@ object RouterSpec extends PlaySpecification {
     },
     whenNoParam = result => {
       contentAsString(result) must equalTo("emptyOptional")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "Int",
-    "take-int",
-    "x=789",
-    "789", // calls takeInt(...)
-    whenNoValue = result => {
-      contentAsString(result) must contain("Missing parameter: x")
-      status(result) must equalTo(BAD_REQUEST)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must contain("Missing parameter: x")
-      status(result) must equalTo(BAD_REQUEST)
-    }
-  )
-  testQueryParamBinding(
-    "Option[Int]",
-    "take-int-opt",
-    "x=789",
-    "789", // calls takeIntOption(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("emptyOption")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("emptyOption")
       status(result) must equalTo(OK)
     }
   )
@@ -404,64 +306,6 @@ object RouterSpec extends PlaySpecification {
     }
   )
   testQueryParamBinding(
-    "List[String]",
-    "take-slist-str",
-    "x=x&x=y&x=z",
-    "x,y,z", // calls takeListString(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("emptyStringElement") // means non-empty List("") was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty List() was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "Option[List[String]]",
-    "take-slist-str-opt",
-    "x=x&x=y&x=z",
-    "x,y,z", // calls takeListStringOption(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo(
-        "emptyStringElement"
-      ) // means non-empty list Some(List("")) was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty list Some(List()) was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "List[Char]",
-    "take-slist-char",
-    "x=z",
-    "z", // calls takeListChar(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("") // means empty List() was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty List() was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "Option[List[Char]]",
-    "take-slist-char-opt",
-    "x=z",
-    "z", // calls takeListCharOption(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("") // means empty Some(List()) was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty Some(List()) was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
     "java.util.List[Character]",
     "take-jlist-jchar",
     "x=z",
@@ -520,34 +364,6 @@ object RouterSpec extends PlaySpecification {
     }
   )
   testQueryParamBinding(
-    "List[Short]",
-    "take-slist-short",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListShort(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("") // means empty List() was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty List() was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "Option[List[Short]]",
-    "take-slist-short-opt",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListShortOption(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("") // means empty list Some(List()) was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty list Some(List()) was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
     "java.util.List[java.lang.Short]",
     "take-jlist-jshort",
     "x=7&x=8&x=9",
@@ -572,34 +388,6 @@ object RouterSpec extends PlaySpecification {
     },
     whenNoParam = result => {
       contentAsString(result) must equalTo("") // means empty list Optional.of(List()) was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "List[Int]",
-    "take-slist-int",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListInt(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("") // means empty List() was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty List() was passed to action
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBinding(
-    "Option[List[Int]]",
-    "take-slist-int-opt",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListIntOption(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("") // means empty list Some(List()) was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("") // means empty list Some(List()) was passed to action
       status(result) must equalTo(OK)
     }
   )
@@ -646,20 +434,6 @@ object RouterSpec extends PlaySpecification {
     }
   )
   testQueryParamBindingWithDefault(
-    "Option[String]",
-    "take-str-opt",
-    "x=xyz",
-    "xyz", // calls takeStringOptionWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("abc")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
     "java.util.Optional[String]",
     "take-str-jopt",
     "x=xyz",
@@ -670,34 +444,6 @@ object RouterSpec extends PlaySpecification {
     },
     whenNoParam = result => {
       contentAsString(result) must equalTo("abc")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Char",
-    "take-char",
-    "x=z",
-    "z", // calls takeCharWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("a")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("a")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Option[Char]",
-    "take-char-opt",
-    "x=z",
-    "z", // calls takeCharOptionWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("a")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("a")
       status(result) must equalTo(OK)
     }
   )
@@ -730,34 +476,6 @@ object RouterSpec extends PlaySpecification {
     }
   )
   testQueryParamBindingWithDefault(
-    "Short",
-    "take-short",
-    "x=789",
-    "789", // calls takeShortWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Option[Short]",
-    "take-short-opt",
-    "x=789",
-    "789", // calls takeShortOptionWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
     "java.lang.Short",
     "take-jshort",
     "x=789",
@@ -776,34 +494,6 @@ object RouterSpec extends PlaySpecification {
     "take-jshort-jopt",
     "x=789",
     "789", // calls takeJavaShortOptionalWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Int",
-    "take-int",
-    "x=789",
-    "789", // calls takeIntWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("123")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Option[Int]",
-    "take-int-opt",
-    "x=789",
-    "789", // calls takeIntOptionWithDefault(...)
     whenNoValue = result => {
       contentAsString(result) must equalTo("123")
       status(result) must equalTo(OK)
@@ -884,64 +574,6 @@ object RouterSpec extends PlaySpecification {
     }
   )
   testQueryParamBindingWithDefault(
-    "List[String]",
-    "take-slist-str",
-    "x=x&x=y&x=z",
-    "x,y,z", // calls takeListStringWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("emptyStringElement") // means non-empty List("") was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("abc,def,ghi")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Option[List[String]]",
-    "take-slist-str-opt",
-    "x=x&x=y&x=z",
-    "x,y,z", // calls takeListStringOptionWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo(
-        "emptyStringElement"
-      ) // means non-empty list Some(List("")) was passed to action
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("abc,def,ghi")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "List[Char]",
-    "take-slist-char",
-    "x=z",
-    "z", // calls takeListCharWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("a,b,c")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("a,b,c")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Option[List[Char]]",
-    "take-slist-char-opt",
-    "x=z",
-    "z", // calls takeListCharOptionWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("a,b,c")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("a,b,c")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
     "java.util.List[Character]",
     "take-jlist-jchar",
     "x=z",
@@ -1000,34 +632,6 @@ object RouterSpec extends PlaySpecification {
     }
   )
   testQueryParamBindingWithDefault(
-    "List[Short]",
-    "take-slist-short",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListShortWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Option[List[Short]]",
-    "take-slist-short-opt",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListShortOptionWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
     "java.util.List[java.lang.Short]",
     "take-jlist-jshort",
     "x=7&x=8&x=9",
@@ -1046,34 +650,6 @@ object RouterSpec extends PlaySpecification {
     "take-jlist-jshort-jopt",
     "x=7&x=8&x=9",
     "7,8,9", // calls takeJavaListShortOptionalWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "List[Int]",
-    "take-slist-int",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListIntWithDefault(...)
-    whenNoValue = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    },
-    whenNoParam = result => {
-      contentAsString(result) must equalTo("1,2,3")
-      status(result) must equalTo(OK)
-    }
-  )
-  testQueryParamBindingWithDefault(
-    "Option[List[Int]]",
-    "take-slist-int-opt",
-    "x=7&x=8&x=9",
-    "7,8,9", // calls takeListIntOptionWithDefault(...)
     whenNoValue = result => {
       contentAsString(result) must equalTo("1,2,3")
       status(result) must equalTo(OK)
