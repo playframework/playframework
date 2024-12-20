2f498abb70e8fcaaf3e987167d8780640f603bcb
Matthias Kurz <m.kurz@irregular.at>
2024-12-20 10:24:32 +0100

    Test Scala 3.8.0-RC2

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,8 +5,8 @@
 object ScalaVersions {
   val scala212 = "2.12.20"
   val scala213 = "2.13.18"
-  val scala3   = "3.3.7"
   val scala37  = "3.7.4"
+  val scala38  = "3.8.0-RC2"
 }
 
 object SbtVersions {
