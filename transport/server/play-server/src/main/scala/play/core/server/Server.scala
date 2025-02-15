bb03fade6f10b49dfcc8cce36b9a2e1782b31a58
Matthias Kurz <m.kurz@irregular.at>
2025-02-15 10:19:46 +0100

    DIRTY DIRTY DIRTY WIP

--- transport/server/play-server/src/main/scala/play/core/server/Server.scala
+++ transport/server/play-server/src/main/scala/play/core/server/Server.scala
@@ -35,6 +35,7 @@ import play.api.mvc.request.RequestTarget
 import play.api.routing.Router
 import play.api.ApplicationLoader.Context
 import play.core._
+import play.core.parsers.FormUrlEncodedParser
 import play.routing.{ Router => JRouter }
 
 trait WebSocketable {
@@ -168,7 +169,10 @@ object Server {
       val withoutQueryString = withoutHost.split('?').head
       if (withoutQueryString.isEmpty) "/" else withoutQueryString
     }
+    // TODO_QUERY
     override lazy val queryMap: Map[String, Seq[String]] = {
+      FormUrlEncodedParser.parse(requestUri)
+      /*
       // Very rough parse of query string that doesn't decode
       if (requestUri.contains("?")) {
         requestUri
@@ -187,6 +191,7 @@ object Server {
       } else {
         Map.empty
       }
+      */
     }
   }
 
