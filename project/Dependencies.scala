569e7951c6ab00829f43a72c9e643f6ef8504240
Scala Steward <m.kurz@irregular.at>
2025-02-24 00:57:53 +0100

    [3.0.x] play-ahc-ws-standalone 3.0.7 (was 3.0.6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -291,7 +291,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi
 
-  val playWsStandaloneVersion = "3.0.6"
+  val playWsStandaloneVersion = "3.0.7"
   val playWsDeps = Seq(
     "org.playframework" %% "play-ws-standalone"      % playWsStandaloneVersion,
     "org.playframework" %% "play-ws-standalone-xml"  % playWsStandaloneVersion,
