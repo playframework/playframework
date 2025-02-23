3fa413e5f4b4f5a420b5bbf956e329993942da43
Scala Steward <m.kurz@irregular.at>
2025-02-24 00:57:57 +0100

    [3.0.x] netty-reactive-streams-http 3.0.4 (was 3.0.3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -171,7 +171,7 @@ object Dependencies {
   val nettyVersion = "4.1.118.Final"
 
   val netty = Seq(
-    "org.playframework.netty" % "netty-reactive-streams-http" % "3.0.3",
+    "org.playframework.netty" % "netty-reactive-streams-http" % "3.0.4",
     "io.netty"                % "netty-codec-http"            % nettyVersion, // increases transitive Netty dependency version ...
     "io.netty"                % "netty-handler"               % nettyVersion, // ... pulled in by netty-reactive-streams-http
     // Provide various Linux native transport dependencies. Netty automatically loads the correct native library
