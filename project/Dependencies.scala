f894e1de13ef408f780bcb8b29516418817ddee6
Matthias Kurz <m.kurz@irregular.at>
2025-02-23 16:18:12 +0100

    Latest play-ws milestone

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -289,7 +289,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi ++ Seq(assertj % Test)
 
-  val playWsStandaloneVersion = "3.1.0-M4"
+  val playWsStandaloneVersion = "3.1.0-M5"
   val playWsDeps = Seq(
     "org.playframework" %% "play-ws-standalone"      % playWsStandaloneVersion,
     "org.playframework" %% "play-ws-standalone-xml"  % playWsStandaloneVersion,
