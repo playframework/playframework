aba21a028f1d89638b72c99e515a596d149674d1
Scala Steward <m.kurz@irregular.at>
2025-02-28 11:11:36 +0100

    [2.9.x] jdbc-driver 1.2.10 (was 1.2.9)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -85,7 +85,7 @@ object Dependencies {
     "org.apache.derby" % "derbytools"
   ).map(_ % derbyVersion)
 
-  val acolyteVersion = "1.2.9"
+  val acolyteVersion = "1.2.10"
   val acolyte        = "org.eu.acolyte" % "jdbc-driver" % acolyteVersion
 
   val jjwtVersion = "0.11.5"
