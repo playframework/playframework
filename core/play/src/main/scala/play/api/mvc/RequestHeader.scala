101d6b15110666727763ada090826d7c1dccce9a
Matthias Kurz <m.kurz@irregular.at>
2025-03-18 11:19:52 +0100

    TODO: add creation time of the request (?)

--- core/play/src/main/scala/play/api/mvc/RequestHeader.scala
+++ core/play/src/main/scala/play/api/mvc/RequestHeader.scala
@@ -19,6 +19,8 @@ import play.api.libs.typedmap.TypedKey
 import play.api.libs.typedmap.TypedMap
 import play.api.mvc.request._
 
+import java.time.Instant
+
 /**
  * The HTTP request header. Note that it doesn't contain the request body yet.
  */
@@ -165,6 +167,12 @@ trait RequestHeader {
    * (because JDK itself does not support UUIDv7 yet)
    */
   final def id: UUID = attrs(RequestAttrKey.Id)
+
+  /**
+   * TODO ?
+   * @return
+   */
+  final def createdAt: Instant = Instant.now() // TODO: read microsecond timestamp from id UUIDv7
 
   /**
    * The HTTP method.
