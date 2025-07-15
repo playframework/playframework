9135347f55de0b55e1ad7ebfafa762f277fd5c12
Matthias Kurz <m.kurz@irregular.at>
2025-07-15 14:03:58 +0200

    Make sure Java's Session and Flash always exists even if empty req attrs
    (cherry picked from commit aa3e91969af9696d098d0e7ab9255fd5d0803834)

--- core/play/src/main/java/play/mvc/Http.java
+++ core/play/src/main/java/play/mvc/Http.java
@@ -393,7 +393,10 @@ public class Http {
      * {@link Cell} to store the session cookie, to allow it to be evaluated on-demand.
      */
     default Session session() {
-      return attrs().get(RequestAttrKey.Session().asJava()).value().asJava();
+      return attrs()
+          .getOptional(RequestAttrKey.Session().asJava())
+          .map(cell -> cell.value().asJava())
+          .orElseGet(() -> new Session());
     }
 
     /**
@@ -402,7 +405,10 @@ public class Http {
      * store the flash, to allow it to be evaluated on-demand.
      */
     default Flash flash() {
-      return attrs().get(RequestAttrKey.Flash().asJava()).value().asJava();
+      return attrs()
+          .getOptional(RequestAttrKey.Flash().asJava())
+          .map(cell -> cell.value().asJava())
+          .orElseGet(() -> new Flash());
     }
 
     /**
