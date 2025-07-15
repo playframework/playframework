5518f1b7dc11c008a2e6ddc6c82de72f1dc01256
Matthias Kurz <m.kurz@irregular.at>
2025-07-16 00:48:02 +0200

    Tests for request-id generation
    (cherry picked from commit 1d3ec771361527ddc00c703e1af116f8557fde83)

--- core/play/src/test/scala/play/api/mvc/RequestHeaderSpec.scala
+++ core/play/src/test/scala/play/api/mvc/RequestHeaderSpec.scala
@@ -16,6 +16,7 @@ import play.api.libs.typedmap.TypedKey
 import play.api.libs.typedmap.TypedMap
 import play.api.mvc.request.DefaultRequestFactory
 import play.api.mvc.request.RemoteConnection
+import play.api.mvc.request.RequestAttrKey
 import play.api.mvc.request.RequestTarget
 
 class RequestHeaderSpec extends Specification {
@@ -206,6 +207,19 @@ class RequestHeaderSpec extends Specification {
         accept("en-US;q=0.7, es") must contain(exactly(Lang("es"), Lang("en-US")).inOrder)
       }
     }
+
+    "have request id" in {
+      "generated if it does not exist yet" in {
+        val rh = dummyRequestHeader()
+        // The request id will likely be somewhere from 1 to 10000 in the tests
+        rh.id must beBetween(1L, 10000L)
+        rh.attrs(RequestAttrKey.Id) must beBetween(1L, 10000L)
+      }
+
+      "not generated if one exists in the attrs already" in {
+        dummyRequestHeader(attrs = TypedMap(RequestAttrKey.Id -> 987656789)).id must_== 987656789
+      }
+    }
   }
 
   private def accept(value: String) =
@@ -216,7 +230,8 @@ class RequestHeaderSpec extends Specification {
   private def dummyRequestHeader(
       requestMethod: String = "GET",
       requestUri: String = "/",
-      headers: Headers = Headers()
+      headers: Headers = Headers(),
+      attrs: TypedMap = TypedMap.empty
   ): RequestHeader = {
     new DefaultRequestFactory(HttpConfiguration()).createRequestHeader(
       connection = RemoteConnection("", false, None),
@@ -224,7 +239,7 @@ class RequestHeaderSpec extends Specification {
       target = RequestTarget(requestUri, "", Map.empty),
       version = "",
       headers = headers,
-      attrs = TypedMap.empty
+      attrs = attrs
     )
   }
 
