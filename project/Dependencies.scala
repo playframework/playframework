38fa4312a28c9898200a2ba03e8f88a99832d23a
Matthias Kurz <m.kurz@irregular.at>
2025-10-23 14:29:54 +0200

    [3.0.x] play-json 3.0.6 (was 3.0.5)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
 
   val sslConfig = "com.typesafe" %% "ssl-config-core" % "0.6.1"
 
-  val playJsonVersion = "3.0.5"
+  val playJsonVersion = "3.0.6"
 
   val logback = "ch.qos.logback" % "logback-classic" % "1.5.20"
 
