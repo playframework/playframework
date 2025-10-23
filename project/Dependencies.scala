100293702b980a3be8db3851aef0ee8543c6d79c
Matthias Kurz <m.kurz@irregular.at>
2025-10-23 14:36:43 +0200

    play-ahc-ws-standalone, ... 3.1.0-M7 (was 3.1.0-M6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -304,7 +304,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi ++ Seq(assertj % Test)
 
-  val playWsStandaloneVersion = "3.1.0-M6"
+  val playWsStandaloneVersion = "3.1.0-M7"
   val playWsDeps              = Seq(
     "org.playframework" %% "play-ws-standalone"      % playWsStandaloneVersion,
     "org.playframework" %% "play-ws-standalone-xml"  % playWsStandaloneVersion,
