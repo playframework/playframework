c4f58e183e67a47b21a09021fd4232e533840eb2
Matthias Kurz <m.kurz@irregular.at>
2025-10-23 14:34:57 +0200

    okhttp-jvm 5.2.1 (was 5.1.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -194,7 +194,7 @@ object Dependencies {
 
   val jimfs = "com.google.jimfs" % "jimfs" % "1.3.1"
 
-  val okHttp = "com.squareup.okhttp3" % "okhttp-jvm" % "5.1.0"
+  val okHttp = "com.squareup.okhttp3" % "okhttp-jvm" % "5.2.1"
 
   def routesCompilerDependencies(scalaVersion: String) = {
     specs2Deps.map(_ % Test) ++ Seq(specsMatcherExtra % Test) ++ scalaParserCombinators(
