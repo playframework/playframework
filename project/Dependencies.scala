9e8e8540d1cbd75f105335f2d56e7f10e25068dd
Matthias Kurz <m.kurz@irregular.at>
2025-10-23 14:41:15 +0200

    logback-classic 1.5.20 (was 1.5.18)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   val playJsonVersion = "3.1.0-M1"
 
-  val logback = "ch.qos.logback" % "logback-classic" % "1.5.18"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.5.20"
 
   val specs2Version = "4.22.0"
   val specs2Deps    = Seq(
