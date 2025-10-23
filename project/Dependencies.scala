dd04427e6dc07cefdbad0b17fc18cb35bfd55287
Matthias Kurz <m.kurz@irregular.at>
2025-10-23 14:24:13 +0200

    [2.9.x] netty-reactive-streams-http 2.0.16 (was 2.0.15)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -201,7 +201,7 @@ object Dependencies {
   val nettyVersion = "4.1.128.Final"
 
   val netty = Seq(
-    "com.typesafe.netty" % "netty-reactive-streams-http" % "2.0.15",
+    "com.typesafe.netty" % "netty-reactive-streams-http" % "2.0.16",
     "io.netty"           % "netty-codec-http"            % nettyVersion, // increases transitive Netty dependency version ...
     "io.netty"           % "netty-handler"               % nettyVersion, // ... pulled in by netty-reactive-streams-http
     // Provide various Linux native transport dependencies. Netty automatically loads the correct native library
