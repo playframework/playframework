ee26ac715b376741f9de8096432ce0c25c2efcb7
Scala Steward <scala_steward@virtuslab.com>
2026-01-28 04:19:15 +0000

    [3.0.x] logback-classic 1.5.26 (was 1.5.25)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   val playJsonVersion = "3.0.6"
 
-  val logback = "ch.qos.logback" % "logback-classic" % "1.5.25"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.5.26"
 
   val bytebuddyVersion = "1.17.8"
   val bytebuddy        = "net.bytebuddy" % "byte-buddy"       % bytebuddyVersion
