699ec8cc0b28c7984058d8c7e3b2b65d86f4fd32
Scala Steward <scala_steward@virtuslab.com>
2026-03-26 22:08:28 +0000

    [2.9.x] typesafe:config 1.4.6 (was 1.4.5)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -245,7 +245,7 @@ object Dependencies {
     Seq(playFileWatch, logback % Test) ++ specs2Deps.map(_ % Test)
   }
 
-  val typesafeConfig = "com.typesafe" % "config" % "1.4.5"
+  val typesafeConfig = "com.typesafe" % "config" % "1.4.6"
 
   def sbtDependencies(sbtVersion: String, scalaVersion: String) = {
     def sbtDep(moduleId: ModuleID) = sbtPluginDep(moduleId, sbtVersion, scalaVersion)
