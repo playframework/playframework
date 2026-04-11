9bc32d814cdfa31afa49c2cdc6bba40fb9f5befc
Matthias Kurz <m.kurz@irregular.at>
2026-04-11 19:20:56 +0200

    jackson-databind 2.21.2 (was 2.20.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     "org.scalacheck" %% "scalacheck"        % "1.19.0"      % Test
   )
 
-  val jacksonVersion  = "2.20.2"
+  val jacksonVersion  = "2.21.2"
   val jacksonDatabind = Seq("com.fasterxml.jackson.core" % "jackson-databind" % jacksonVersion)
   val jacksons        = Seq(
     "com.fasterxml.jackson.core"     % "jackson-core",
