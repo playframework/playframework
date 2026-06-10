fa65eccff4303f33313f38420f03cd7c567f4768
Matthias Kurz <m.kurz@irregular.at>
2026-06-10 13:59:28 +0200

    Add package-info.java to mark packages as internal

---
+++ web/play-java-forms/src/main/java/play/data/internal/binding/format/support/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+/**
+ * Internal formatting support for Play Java form binding.
+ *
+ * <p>These classes are not public API and may change or be removed without notice.
+ */
+@org.apache.pekko.annotation.InternalApi
+package play.data.internal.binding.format.support;
