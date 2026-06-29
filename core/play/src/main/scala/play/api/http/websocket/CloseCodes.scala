b7727f8ba181de41e28544128621ebe6551f8d45
Matthias Kurz <m.kurz@irregular.at>
2026-06-29 21:38:20 +0200

    Document WebSocket close frame limits

--- core/play/src/main/scala/play/api/http/websocket/CloseCodes.scala
+++ core/play/src/main/scala/play/api/http/websocket/CloseCodes.scala
@@ -5,19 +5,40 @@
 package play.api.http.websocket
 
 /**
- * WebSocket close codes
+ * WebSocket close codes.
+ *
+ * RFC 6455 limits the reason in a Close frame with a status code to 123 UTF-8 bytes. Play truncates longer
+ * Close reasons before sending them.
  */
 object CloseCodes {
-  val Regular                = 1000
-  val GoingAway              = 1001
-  val ProtocolError          = 1002
-  val Unacceptable           = 1003
-  val NoStatus               = 1005
+  val Regular       = 1000
+  val GoingAway     = 1001
+  val ProtocolError = 1002
+  val Unacceptable  = 1003
+
+  /**
+   * Indicates that no close status code was present.
+   *
+   * This value is reserved by RFC 6455 and must not be sent as the status code in a WebSocket Close frame.
+   */
+  val NoStatus = 1005
+
+  /**
+   * Indicates that the connection was closed abnormally without receiving a Close frame.
+   *
+   * This value is reserved by RFC 6455 and must not be sent as the status code in a WebSocket Close frame.
+   */
   val ConnectionAbort        = 1006
   val InconsistentData       = 1007
   val PolicyViolated         = 1008
   val TooBig                 = 1009
   val ClientRejectsExtension = 1010
   val UnexpectedCondition    = 1011
-  val TLSHandshakeFailure    = 1015
+
+  /**
+   * Indicates that the connection was closed because the TLS handshake failed.
+   *
+   * This value is reserved by RFC 6455 and must not be sent as the status code in a WebSocket Close frame.
+   */
+  val TLSHandshakeFailure = 1015
 }
