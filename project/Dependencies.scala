257699860e6e7d0e2505dc2d95808001ce62327a
Matthias Kurz <m.kurz@irregular.at>
2026-07-01 18:00:20 +0200

    Update playWsStandaloneVersion to 3.1.0-M13
    Comes with Pekko 2.0.0-M3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -289,7 +289,7 @@ object Dependencies {
     "com.github.ben-manes.caffeine" % "jcache"   % caffeineVersion
   ) ++ jcacheApi ++ Seq(assertj % Test)
 
-  val playWsStandaloneVersion = "3.1.0-M12"
+  val playWsStandaloneVersion = "3.1.0-M13"
   val playWsDeps              = Seq(
     "org.playframework" %% "play-ws-standalone"      % playWsStandaloneVersion,
     "org.playframework" %% "play-ws-standalone-xml"  % playWsStandaloneVersion,
