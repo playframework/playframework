53931ea00ab60b4325c9014923285a6ffde7edd4
Scala Steward <scala_steward@virtuslab.com>
2026-08-29 20:42:59 +0000

    scalacheck 1.20.0 (was 1.19.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
 
   val scalacheckDependencies = Seq(
     "org.specs2"     %% "specs2-scalacheck" % specs2Version % Test,
-    "org.scalacheck" %% "scalacheck"        % "1.19.0"      % Test
+    "org.scalacheck" %% "scalacheck"        % "1.20.0"      % Test
   )
 
   val jacksonVersion  = "2.21.5"
