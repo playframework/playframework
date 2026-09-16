aea2c5ba8daa65936db0725edf09b8ad36d46af6
Vlad Chesnokov <vchesnokov@gradle.com>
2026-09-16 10:28:38 +0200

    Gradle plugin: get aggregated routes via dedicated configurations to make the plugin IP-safe

--- dev-mode/gradle-plugin/src/main/java/play/gradle/plugin/PlayRoutesPlugin.java
+++ dev-mode/gradle-plugin/src/main/java/play/gradle/plugin/PlayRoutesPlugin.java
@@ -3,27 +3,29 @@
  */
 package play.gradle.plugin;
 
+import static java.util.stream.Collectors.joining;
 import static org.gradle.api.attributes.Category.CATEGORY_ATTRIBUTE;
 import static org.gradle.api.attributes.Usage.USAGE_ATTRIBUTE;
-import static org.gradle.api.plugins.JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME;
-import static play.gradle.internal.Utils.filterProjectComponents;
 import static play.gradle.internal.Utils.getDefaultPlayVersion;
 import static play.gradle.internal.Utils.isGradleVersionLessThan;
 import static play.gradle.internal.Utils.isPlayJava;
-import static play.gradle.internal.Utils.isPlayProject;
 import static play.gradle.internal.Utils.javaPluginExtension;
+import static play.gradle.internal.Utils.mainSourceSet;
 import static play.gradle.internal.Utils.playExtension;
 import static play.gradle.internal.Utils.scalaSourceDirectorySet;
 import static play.gradle.plugin.PlayPlugin.DEFAULT_SCALA_VERSION;
 import static play.gradle.plugin.PlayPlugin.PLAY_GROUP_ID;
 
 import java.util.List;
+import java.util.Objects;
 import javax.inject.Inject;
 import org.gradle.api.Incubating;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.result.ResolutionResult;
+import org.gradle.api.artifacts.result.ResolvedDependencyResult;
 import org.gradle.api.attributes.Attribute;
 import org.gradle.api.attributes.Category;
 import org.gradle.api.attributes.Usage;
@@ -31,6 +33,7 @@ import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.internal.tasks.DefaultSourceSet;
 import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskProvider;
 import org.jetbrains.annotations.NotNull;
@@ -47,6 +50,9 @@ public class PlayRoutesPlugin implements Plugin<Project> {
 
   public static final Attribute<String> SOURCE_SET_ATTRIBUTE =
       Attribute.of("org.playframework.routes.sourceset", String.class);
+
+  public static final Attribute<String> AGGREGATED_PROJECTS_ATTRIBUTE =
+      Attribute.of("org.playframework.routes.aggregated", String.class);
 
   private static final List<String> ROUTES_SOURCE_INCLUDES = List.of("routes", "*.routes");
 
@@ -69,30 +75,43 @@ public class PlayRoutesPlugin implements Plugin<Project> {
     configureSourceSetDefaults(project, routesConfiguration);
   }
 
-  /**
-   * @param project Current project
-   * @return {@code false} if this project is aggregated by one of his dependencies
-   */
-  private boolean isAggregated(final Project project) {
-    var runtime = project.getConfigurations().getByName(RUNTIME_CLASSPATH_CONFIGURATION_NAME);
-    return filterProjectComponents(runtime).stream()
-        .map(project::project)
-        .anyMatch(
-            child ->
-                isPlayProject(child)
-                    && playExtension(child)
-                        .getRoutes()
-                        .getAggregateReverseRoutes()
-                        .get()
-                        .contains(project));
+  private Provider<Boolean> shouldGenerateReverseRouter(final Project project) {
+    Configuration graph = createRoutesGraphConfiguration(project);
+    String projectPath = project.getPath();
+    return project.provider(
+        () -> !isAggregatedBy(graph.getIncoming().getResolutionResult(), projectPath));
+  }
+
+  private static boolean isAggregatedBy(ResolutionResult dependencies, String projectPath) {
+    return dependencies.getAllDependencies().stream()
+        .filter(ResolvedDependencyResult.class::isInstance)
+        .map(dependency -> ((ResolvedDependencyResult) dependency).getResolvedVariant())
+        .map(variant -> variant.getAttributes().getAttribute(AGGREGATED_PROJECTS_ATTRIBUTE))
+        .filter(Objects::nonNull)
+        .anyMatch(aggregated -> List.of(aggregated.split(",")).contains(projectPath));
+  }
+
+  private Configuration createRoutesGraphConfiguration(final Project project) {
+    SourceSet mainSourceSet = mainSourceSet(project);
+    Configuration conf = project.getConfigurations().create("playRoutesGraph");
+    conf.setDescription("The dependency graph used to detect aggregation of the reverse routes.");
+    conf.setVisible(false);
+    conf.setCanBeConsumed(false);
+    conf.setCanBeResolved(true);
+    conf.extendsFrom(
+        project.getConfigurations().getByName(mainSourceSet.getImplementationConfigurationName()),
+        project.getConfigurations().getByName(mainSourceSet.getRuntimeOnlyConfigurationName()));
+    conf.getAttributes().attribute(SOURCE_SET_ATTRIBUTE, mainSourceSet.getName());
+    return conf;
   }
 
   private void configureDefaultSettings(final Project project) {
     var routes = playExtension(project).getRoutes();
     routes.getNamespaceReverseRouter().convention(false);
-    routes.getGenerateReverseRouter().convention(project.provider(() -> !isAggregated(project)));
-    routes.getGenerateJsReverseRouter().convention(project.provider(() -> !isAggregated(project)));
     routes.getAggregateReverseRoutes().convention(project.getObjects().listProperty(Project.class));
+    Provider<Boolean> generateReverseRouter = shouldGenerateReverseRouter(project);
+    routes.getGenerateReverseRouter().convention(generateReverseRouter);
+    routes.getGenerateJsReverseRouter().convention(generateReverseRouter);
     routes
         .getImports()
         .addAll(
@@ -155,6 +174,10 @@ public class PlayRoutesPlugin implements Plugin<Project> {
         .attribute(USAGE_ATTRIBUTE, objectFactory.named(Usage.class, PLAY_ROUTES_USAGE))
         .attribute(CATEGORY_ATTRIBUTE, objectFactory.named(Category.class, Category.LIBRARY))
         .attribute(SOURCE_SET_ATTRIBUTE, sourceSet.getName());
+    conf.extendsFrom(
+        project.getConfigurations().getByName(sourceSet.getImplementationConfigurationName()),
+        project.getConfigurations().getByName(sourceSet.getRuntimeOnlyConfigurationName()));
+    declareAggregatedProjects(project, conf);
     // Source directories can still be added while the build script is evaluated
     if (isGradleVersionLessThan("7.4")) {
       // Publishing a lazily computed set of artifacts is not supported in Gradle < 7.4
@@ -164,6 +187,27 @@ public class PlayRoutesPlugin implements Plugin<Project> {
     } else {
       conf.getOutgoing().artifacts(project.provider(routesSource::getSrcDirs));
     }
+  }
+
+  private void declareAggregatedProjects(final Project project, final Configuration conf) {
+    // The aggregation is only known once the build script has configured the extension
+    Provider<String> aggregatedProjects = aggregatedProjectPaths(project);
+    if (isGradleVersionLessThan("7.4")) {
+      // Declaring a lazily computed attribute is not supported yet
+      project.afterEvaluate(
+          evaluated ->
+              conf.getAttributes()
+                  .attribute(AGGREGATED_PROJECTS_ATTRIBUTE, aggregatedProjects.get()));
+    } else {
+      conf.getAttributes().attributeProvider(AGGREGATED_PROJECTS_ATTRIBUTE, aggregatedProjects);
+    }
+  }
+
+  private Provider<String> aggregatedProjectPaths(final Project project) {
+    return playExtension(project)
+        .getRoutes()
+        .getAggregateReverseRoutes()
+        .map(projects -> projects.stream().map(Project::getPath).collect(joining(",")));
   }
 
   private Configuration createAggregateRoutesConfiguration(
