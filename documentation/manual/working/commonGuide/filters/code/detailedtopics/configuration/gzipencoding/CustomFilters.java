8cf09a2caa3d2a4c5aa7978c1f0b9671f1d3aa75
Matthias Kurz <m.kurz@irregular.at>
2026-09-22 12:28:13 +0200

    Keep gzip docs compatible across Scala 3 versions

--- documentation/manual/working/commonGuide/filters/code/detailedtopics/configuration/gzipencoding/CustomFilters.java
+++ documentation/manual/working/commonGuide/filters/code/detailedtopics/configuration/gzipencoding/CustomFilters.java
@@ -21,14 +21,16 @@ public class CustomFilters implements HttpFilters {
   private List<EssentialFilter> filters;
 
   @Inject
+  @SuppressWarnings({"rawtypes", "unchecked"})
   public CustomFilters(Materializer materializer) {
     // #gzip-filter
     GzipFilterConfig gzipFilterConfig = new GzipFilterConfig();
     GzipFilter gzipFilter =
         new GzipFilter(
             gzipFilterConfig.withShouldGzip(
-                (BiFunction<Http.RequestHeader, Result, Object>)
-                    (req, res) -> res.body().contentType().orElse("").startsWith("text/html")),
+                (BiFunction)
+                    (BiFunction<Http.RequestHeader, Result, Boolean>)
+                        (req, res) -> res.body().contentType().orElse("").startsWith("text/html")),
             materializer);
     // #gzip-filter
     filters = Collections.singletonList(gzipFilter.asJava());
