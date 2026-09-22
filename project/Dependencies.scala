7f9138b926db0da3c8c3432fcacb0ccfcbefd622
Scala Steward <scala_steward@virtuslab.com>
2026-09-22 20:30:31 +0000

    jimfs 1.3.2 (was 1.3.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -177,7 +177,7 @@ object Dependencies {
 
   val cookieEncodingDependencies = slf4j
 
-  val jimfs = "com.google.jimfs" % "jimfs" % "1.3.1"
+  val jimfs = "com.google.jimfs" % "jimfs" % "1.3.2"
 
   val okHttp = "com.squareup.okhttp3" % "okhttp-jvm" % "5.5.0"
 
