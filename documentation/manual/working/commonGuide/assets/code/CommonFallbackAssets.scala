6044beb3a1a7436d5c991e97275b898fe45f1534
Matthias Kurz <m.kurz@irregular.at>
2026-09-23 17:58:56 +0200

    Add standard headers to fallback asset examples

--- documentation/manual/working/commonGuide/assets/code/CommonFallbackAssets.scala
+++ documentation/manual/working/commonGuide/assets/code/CommonFallbackAssets.scala
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) from 2022 The Play Framework Contributors <https://playframework.com/>, 2011-2021 Lightbend Inc. <https://www.lightbend.com/>
+ * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package common.fallback.assets
 
 package object controllers {
