9ca6f07380c65aa8a9b17b6424bd242277d8b527
Matthias Kurz <m.kurz@irregular.at>
2026-09-23 06:03:22 +0200

    Test Java WebSocket action composition

--- core/play-integration-test/src/test/java/play/it/http/websocket/WebSocketSpecJavaActions.java
+++ core/play-integration-test/src/test/java/play/it/http/websocket/WebSocketSpecJavaActions.java
@@ -4,16 +4,25 @@
 
 package play.it.http.websocket;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import org.apache.pekko.stream.javadsl.Flow;
 import org.apache.pekko.stream.javadsl.Sink;
 import org.apache.pekko.stream.javadsl.Source;
+import play.http.ActionCreator;
 import play.libs.F;
+import play.libs.typedmap.TypedKey;
+import play.mvc.Action;
 import play.mvc.Http;
+import play.mvc.Result;
 import play.mvc.Results;
+import play.mvc.Security;
 import play.mvc.WebSocket;
 import scala.concurrent.Promise;
 
@@ -21,6 +30,64 @@ import scala.concurrent.Promise;
 public class WebSocketSpecJavaActions {
   public static class JavaJsonMessage {
     public int count;
+  }
+
+  private static final TypedKey<String> ACTION_CREATOR_ATTRIBUTE =
+      TypedKey.create("websocket-action-creator");
+
+  public static class AllowAuthenticator extends Security.Authenticator {
+    @Override
+    public Optional<String> getUsername(Http.Request request) {
+      return Optional.of("controller");
+    }
+  }
+
+  public static class RejectAuthenticator extends Security.Authenticator {
+    @Override
+    public Optional<String> getUsername(Http.Request request) {
+      return Optional.empty();
+    }
+  }
+
+  public static class WebSocketActionCreator implements ActionCreator {
+    @Override
+    public Action createAction(Http.Request request, Method actionMethod) {
+      return new Action.Simple() {
+        @Override
+        public CompletionStage<Result> call(Http.Request request) {
+          return delegate.call(request.addAttr(ACTION_CREATOR_ATTRIBUTE, "creator"));
+        }
+      };
+    }
+  }
+
+  @Security.Authenticated(AllowAuthenticator.class)
+  public static class ActionCompositionController {
+    private final AtomicBoolean rejectedInvoked = new AtomicBoolean();
+
+    public WebSocket accepted() {
+      return WebSocket.Text.accept(
+          request ->
+              Flow.fromSinkAndSource(
+                  Sink.ignore(),
+                  Source.single(
+                      request.attrs().getOptional(Security.USERNAME).orElse("missing")
+                          + ":"
+                          + request
+                              .attrs()
+                              .getOptional(ACTION_CREATOR_ATTRIBUTE)
+                              .orElse("missing"))));
+    }
+
+    @Security.Authenticated(RejectAuthenticator.class)
+    public WebSocket rejected() {
+      rejectedInvoked.set(true);
+      return accepted();
+    }
+
+    public boolean wasRejectedInvoked() {
+      return rejectedInvoked.get();
+    }
   }
 
   private static <A> Sink<A, ?> getChunks(Consumer<List<A>> onDone) {
